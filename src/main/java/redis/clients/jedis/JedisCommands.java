ef1cbfafc91e924f648dfc7f28b20c3fdd7dc7a0
Yaourt <yaourt@yaourtprod.net>
2010-11-04 14:08:18 +0100

    JedisCommands DOES NOT inherit from BinaryJedisCommand

--- src/main/java/redis/clients/jedis/JedisCommands.java
+++ src/main/java/redis/clients/jedis/JedisCommands.java
@@ -7,7 +7,7 @@ import java.util.Set;
 /**
  * Common interface for sharded and non-sharded Jedis
  */
-public interface JedisCommands extends BinaryJedisCommands {
+public interface JedisCommands {
     String set(String key, String value);
 
     String get(String key);

202d68a21c9179b77ec0208acdffbc9a888fe666
Dario Guzik <dario.guzik@mercadolibre.com>
2011-05-15 18:34:15 -0300

    optimize

--- src/main/java/redis/clients/util/Sharded.java
+++ src/main/java/redis/clients/util/Sharded.java
@@ -55,12 +55,14 @@ public class Sharded<R, S extends ShardInfo<R>> {
 
         for (int i = 0; i != shards.size(); ++i) {
             final S shardInfo = shards.get(i);
-            for (int n = 0; n < 160 * shardInfo.getWeight(); n++) {
-            	if (shardInfo.getName() == null)
+            if (shardInfo.getName() == null)
+            	for (int n = 0; n < 160 * shardInfo.getWeight(); n++) {
             		nodes.put(this.algo.hash("SHARD-" + i + "-NODE-" + n), shardInfo);
-            	else
+            	}
+            else
+            	for (int n = 0; n < 160 * shardInfo.getWeight(); n++) {
             		nodes.put(this.algo.hash(shardInfo.getName() + "*" + shardInfo.getWeight() + n), shardInfo);
-            }
+            	}
             resources.put(shardInfo, shardInfo.createResource());
         }
     }

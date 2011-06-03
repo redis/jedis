8465720d921a8d628d70bc126b0903d2b9ae3781
Jonathan Leibiusky <ionathan@gmail.com>
2011-06-03 10:52:49 -0300

    remove strange license header

--- src/main/java/redis/clients/jedis/JedisShardInfo.java
+++ src/main/java/redis/clients/jedis/JedisShardInfo.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2009-2010 MBTE Sweden AB.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package redis.clients.jedis;
 
 import redis.clients.util.ShardInfo;
@@ -22,7 +7,7 @@ public class JedisShardInfo extends ShardInfo<Jedis> {
     public String toString() {
         return host + ":" + port + "*" + getWeight();
     }
-    
+
     private int timeout;
     private String host;
     private int port;
@@ -40,27 +25,28 @@ public class JedisShardInfo extends ShardInfo<Jedis> {
     public JedisShardInfo(String host) {
         this(host, Protocol.DEFAULT_PORT);
     }
+
     public JedisShardInfo(String host, String name) {
-    	this(host, Protocol.DEFAULT_PORT, name);
+        this(host, Protocol.DEFAULT_PORT, name);
     }
-    	 
+
     public JedisShardInfo(String host, int port) {
         this(host, port, 2000);
     }
-    
+
     public JedisShardInfo(String host, int port, String name) {
-    	this(host, port, 2000, name);
+        this(host, port, 2000, name);
     }
-    
+
     public JedisShardInfo(String host, int port, int timeout) {
         this(host, port, timeout, Sharded.DEFAULT_WEIGHT);
     }
-    
+
     public JedisShardInfo(String host, int port, int timeout, String name) {
-    	this(host, port, timeout, Sharded.DEFAULT_WEIGHT);
-    	this.name = name;
+        this(host, port, timeout, Sharded.DEFAULT_WEIGHT);
+        this.name = name;
     }
-    
+
     public JedisShardInfo(String host, int port, int timeout, int weight) {
         super(weight);
         this.host = host;
@@ -83,9 +69,9 @@ public class JedisShardInfo extends ShardInfo<Jedis> {
     public void setTimeout(int timeout) {
         this.timeout = timeout;
     }
-    
+
     public String getName() {
-    	return name ;
+        return name;
     }
 
     @Override

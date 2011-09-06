7d8cfa5a975110e155fd32414841d741d2ba6e72
Yaourt <yaourt@yaourtprod.net>
2011-09-06 17:42:52 +0200

    Use (optionnaly) JedisDynamicShardProvider is ShardedJedisPool.

--- src/main/java/redis/clients/jedis/ShardedJedisPool.java
+++ src/main/java/redis/clients/jedis/ShardedJedisPool.java
@@ -7,10 +7,23 @@ import org.apache.commons.pool.BasePoolableObjectFactory;
 import org.apache.commons.pool.impl.GenericObjectPool;
 
 import redis.clients.util.Hashing;
+import redis.clients.util.JedisDynamicShardsProvider;
 import redis.clients.util.Pool;
 
 public class ShardedJedisPool extends Pool<ShardedJedis> {
-    public ShardedJedisPool(final GenericObjectPool.Config poolConfig,
+	public ShardedJedisPool (final GenericObjectPool.Config poolConfig, final JedisDynamicShardsProvider shardProvider) {
+		this(poolConfig, shardProvider, Hashing.MURMUR_HASH, null);
+	}
+
+	public ShardedJedisPool (final GenericObjectPool.Config poolConfig, final JedisDynamicShardsProvider shardProvider, final Pattern keyTagPattern) {
+		this(poolConfig, shardProvider, Hashing.MURMUR_HASH, keyTagPattern);
+	}
+
+	public ShardedJedisPool (final GenericObjectPool.Config poolConfig, final JedisDynamicShardsProvider shardProvider, final Hashing algo, final Pattern keyTagPattern) {
+		super(poolConfig, new ShardedJedisFactory(shardProvider, algo, keyTagPattern));
+	}
+
+	public ShardedJedisPool(final GenericObjectPool.Config poolConfig,
             List<JedisShardInfo> shards) {
         this(poolConfig, shards, Hashing.MURMUR_HASH);
     }
@@ -37,22 +50,42 @@ public class ShardedJedisPool extends Pool<ShardedJedis> {
         private List<JedisShardInfo> shards;
         private Hashing algo;
         private Pattern keyTagPattern;
+        
+        private final JedisDynamicShardsProvider provider;
+        private final boolean isDynamic;
+        
+        public ShardedJedisFactory(final JedisDynamicShardsProvider provider, final Hashing algo, final Pattern keyTagPattern) {
+        	this.isDynamic = true;
+        	this.provider = provider;
+        	this.shards = null;
+        	this.keyTagPattern = keyTagPattern;
+        }
 
         public ShardedJedisFactory(List<JedisShardInfo> shards, Hashing algo,
                 Pattern keyTagPattern) {
+        	this.isDynamic = false;
+        	this.provider = null;
             this.shards = shards;
             this.algo = algo;
             this.keyTagPattern = keyTagPattern;
         }
 
         public Object makeObject() throws Exception {
-            ShardedJedis jedis = new ShardedJedis(shards, algo, keyTagPattern);
-            return jedis;
+        	if(isDynamic) {
+	            ShardedJedis jedis = new ShardedJedis(provider, algo, keyTagPattern);
+	            return jedis;
+        	} else {
+	            ShardedJedis jedis = new ShardedJedis(shards, algo, keyTagPattern);
+	            return jedis;
+        	}
         }
 
         public void destroyObject(final Object obj) throws Exception {
             if ((obj != null) && (obj instanceof ShardedJedis)) {
                 ShardedJedis shardedJedis = (ShardedJedis) obj;
+                if(isDynamic) {
+                	provider.unregister(shardedJedis);
+                }
                 for (Jedis jedis : shardedJedis.getAllShards()) {
                     try {
                    		try {

5a6d98ac56f8dec608c79c242c81389149fc3def
Eric Hauser <ewhauser@gmail.com>
2012-04-19 16:48:18 -0400

    fix broken instance reference to protocol

--- src/main/java/redis/clients/jedis/Connection.java
+++ src/main/java/redis/clients/jedis/Connection.java
@@ -209,7 +209,7 @@ public class Connection {
     public List<Long> getIntegerMultiBulkReply() {
         flush();
         pipelinedCommands--;
-        return (List<Long>) protocol.read(inputStream);
+        return (List<Long>) Protocol.read(inputStream);
     }
 
     public List<Object> getAll() {

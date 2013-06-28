75087b0d3ab7a06873cdef242a037304211f7bfe
Vlastimil Menčík <vlastimil.mencik@etnetera.cz>
2013-06-28 14:03:55 +0200

    JedisFactory selects database to the one in its configuration before the instance is borrowed from the pool.

--- src/main/java/redis/clients/jedis/JedisPool.java
+++ src/main/java/redis/clients/jedis/JedisPool.java
@@ -103,6 +103,17 @@ public class JedisPool extends Pool<Jedis> {
             
             return jedis;
         }
+        
+        @Override
+        public void activateObject(Object obj) throws Exception {
+            if (!(obj instanceof Jedis)) {
+        	throw new IllegalArgumentException("activated object is not a Jedis instance");
+            }
+            final Jedis jedis = (Jedis)obj;
+            if (jedis.getDB() != database) {
+        	jedis.select(database);
+            }
+        }
 
         public void destroyObject(final Object obj) throws Exception {
             if (obj instanceof Jedis) {

4ac34cd777f32ad54f06d5ede7e5edf3fb2b7ec8
ffratoni <f.fratoni@wazzamba.com>
2013-07-19 19:09:08 +0200

    zadd fix for Jedis. zadd modified to accepr a Map<String,Double> to allow adding many members with the same score

--- src/main/java/redis/clients/jedis/JedisCommands.java
+++ src/main/java/redis/clients/jedis/JedisCommands.java
@@ -115,6 +115,7 @@ public interface JedisCommands {
     Long zadd(String key, double score, String member);
     
     Long zadd(String key, Map<Double, String> scoreMembers);
+    Long zaddFixed(String key, Map<String,Double > scoreMembers);
 
     Set<String> zrange(String key, long start, long end);
 

d0e580d1ad77ef870cb4e8aced2b2829279107a0
Jungtaek Lim <kabhwan@gmail.com>
2014-01-27 13:41:41 +0900

    Fix Pool's race condition by synchronize all inner pool usages
    * Pool
    ** synchronize all inner pool usages (to avoid timing issue)
    * JedisSentinelPool
    ** when master changed, change current master information after reinitialize inner pool
    * JedisSentinelPoolTest
    ** fix resource leak (jedis instance not returned to pool)

--- src/main/java/redis/clients/util/Pool.java
+++ src/main/java/redis/clients/util/Pool.java
@@ -9,6 +9,7 @@ import redis.clients.jedis.exceptions.JedisException;
 
 public abstract class Pool<T> {
     protected GenericObjectPool<T> internalPool;
+    protected Object lock = new Object();
 
     /**
      * Using this constructor means you have to set and initialize the
@@ -25,32 +26,38 @@ public abstract class Pool<T> {
     public void initPool(final GenericObjectPoolConfig poolConfig,
 	    PooledObjectFactory<T> factory) {
 
-	if (this.internalPool != null) {
-	    try {
-		closeInternalPool();
-	    } catch (Exception e) {
-	    }
+    synchronized (lock) {
+		if (this.internalPool != null) {
+		    try {
+			closeInternalPool();
+		    } catch (Exception e) {
+		    }
+		}
+		
+		this.internalPool = new GenericObjectPool<T>(factory, poolConfig);
 	}
-
-	this.internalPool = new GenericObjectPool<T>(factory, poolConfig);
     }
 
     public T getResource() {
-	try {
-	    return internalPool.borrowObject();
-	} catch (Exception e) {
-	    throw new JedisConnectionException(
-		    "Could not get a resource from the pool", e);
+    synchronized (lock) {
+		try {
+		    return internalPool.borrowObject();
+		} catch (Exception e) {
+		    throw new JedisConnectionException(
+			    "Could not get a resource from the pool", e);
+		}
 	}
     }
 
     public void returnResourceObject(final T resource) {
-	try {
-	    internalPool.returnObject(resource);
-	} catch (Exception e) {
-	    throw new JedisException(
-		    "Could not return the resource to the pool", e);
-	}
+    synchronized (lock) {
+		try {
+		    internalPool.returnObject(resource);
+		} catch (Exception e) {
+		    throw new JedisException(
+			    "Could not return the resource to the pool", e);
+		}
+    }
     }
 
     public void returnBrokenResource(final T resource) {
@@ -66,12 +73,14 @@ public abstract class Pool<T> {
     }
 
     protected void returnBrokenResourceObject(final T resource) {
-	try {
-	    internalPool.invalidateObject(resource);
-	} catch (Exception e) {
-	    throw new JedisException(
-		    "Could not return the resource to the pool", e);
-	}
+    synchronized (lock) {
+		try {
+		    internalPool.invalidateObject(resource);
+		} catch (Exception e) {
+		    throw new JedisException(
+			    "Could not return the resource to the pool", e);
+		}
+    }
     }
 
     protected void closeInternalPool() {

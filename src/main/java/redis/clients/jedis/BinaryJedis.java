a0f28717c351cb031df938788cca8b27fa7a27e7
Aniket Schneider <aschneider@visiblemeasures.com>
2014-03-05 16:56:51 -0500

    Deprecate pexpire(key, int) methods

--- src/main/java/redis/clients/jedis/BinaryJedis.java
+++ src/main/java/redis/clients/jedis/BinaryJedis.java
@@ -3314,6 +3314,11 @@ public class BinaryJedis implements BasicCommands, BinaryJedisCommands,
 	return client.getStatusCodeReply();
     }
 
+    @Deprecated
+    public Long pexpire(final byte[] key, final int milliseconds) {
+	return pexpire(key, (long) milliseconds);
+    }
+
     public Long pexpire(final byte[] key, final long milliseconds) {
 	checkIsInMulti();
 	client.pexpire(key, milliseconds);

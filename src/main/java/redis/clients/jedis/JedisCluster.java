1517110c920110e70a06344fd2bbbf250a3b8f8d
Ming <albert.einstain@gmail.com>
2014-08-14 10:37:28 +0800

    add blpop/brpop with timeout parameter interface to JedisCommands

--- src/main/java/redis/clients/jedis/JedisCluster.java
+++ src/main/java/redis/clients/jedis/JedisCluster.java
@@ -1587,4 +1587,26 @@ public class JedisCluster implements JedisCommands, BasicCommands, Closeable {
 	    }
 	}.run(key);
     }
+
+    @Override
+    public List<String> blpop(final int timeout, final String key) {
+	return new JedisClusterCommand<List<String>>(connectionHandler,
+		timeout, maxRedirections) {
+	    @Override
+	    public List<String> execute(Jedis connection) {
+		return connection.blpop(timeout,key);
+	    }
+	}.run(null);
+    }
+
+    @Override
+    public List<String> brpop(final int timeout, final String key) {
+	return new JedisClusterCommand<List<String>>(connectionHandler,
+		timeout, maxRedirections) {
+	    @Override
+	    public List<String> execute(Jedis connection) {
+		return connection.brpop(timeout,key);
+	    }
+	}.run(null);
+    }
 }

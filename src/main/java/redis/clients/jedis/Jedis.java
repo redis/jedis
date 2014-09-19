f065b80ab0e16b059d49308f6743b24ef3c9d0cc
Nykolas Lima <nykolas.lima@gmail.com>
2014-09-19 17:09:27 -0300

    we can't pass null because it will throw an exception. Using localhost instead

--- src/main/java/redis/clients/jedis/Jedis.java
+++ src/main/java/redis/clients/jedis/Jedis.java
@@ -27,7 +27,7 @@ public class Jedis extends BinaryJedis implements JedisCommands,
      * @deprecated CDI eyes only
      */
     public Jedis() {
-	super((String)null);
+	super("localhost");
     }
     
     public Jedis(final String host) {

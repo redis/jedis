f2ef1515d8d3712fc8e9165993b74244958a4bcb
Zayeem <zayeeem@gmail.com>
2014-12-02 16:27:54 -0800

    Used Java Convention to format the code

--- src/main/java/redis/clients/util/Pair.java
+++ src/main/java/redis/clients/util/Pair.java
@@ -6,28 +6,30 @@ public class Pair<A, B> {
     private final B right;
 
     public Pair(final A left, final B right) {
-        this.left = left;
-        this.right = right;
+	this.left = left;
+	this.right = right;
     }
 
     public A getLeft() {
-        return left;
+	return left;
     }
 
     public B getRight() {
-        return right;
+	return right;
     }
 
     @Override
     public boolean equals(final Object object) {
-        return (object.getClass().equals(Pair.class) && ((Pair) object).getLeft().equals(this.left) && ((Pair) object).getRight().equals(this.right));
+	return (object.getClass().equals(Pair.class)
+		&& ((Pair) object).getLeft().equals(this.left) && ((Pair) object)
+		.getRight().equals(this.right));
     }
 
     @Override
     public int hashCode() {
-        int hash = 5;
-        hash = 97 * hash + (this.left != null ? this.left.hashCode() : 0);
-        hash = 97 * hash + (this.right != null ? this.right.hashCode() : 0);
-        return hash;
+	int hash = 5;
+	hash = 97 * hash + (this.left != null ? this.left.hashCode() : 0);
+	hash = 97 * hash + (this.right != null ? this.right.hashCode() : 0);
+	return hash;
     }
 }

4de7095031109d5fddc611d5e8158c43562e57f6
Nykolas Lima <nykolas.lima@gmail.com>
2015-03-31 00:51:45 -0300

    fixing wrong test

--- src/test/java/redis/clients/jedis/tests/commands/BinaryValuesCommandsTest.java
+++ src/test/java/redis/clients/jedis/tests/commands/BinaryValuesCommandsTest.java
@@ -74,7 +74,7 @@ public class BinaryValuesCommandsTest extends JedisCommandTestBase {
     String status = jedis.set(bfoo, binaryValue);
     assertTrue(Keyword.OK.name().equalsIgnoreCase(status));
     // nx should fail if value exists
-    String statusSuccess = jedis.set(bfoo, binaryValue, setParams().nx().ex(expireSeconds));
+    String statusSuccess = jedis.set(bfoo, binaryValue, setParams().xx().ex(expireSeconds));
     assertTrue(Keyword.OK.name().equalsIgnoreCase(statusSuccess));
 
     byte[] value = jedis.get(bfoo);

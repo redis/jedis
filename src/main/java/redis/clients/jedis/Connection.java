f9c6b887b13e541b1cd7f9040fd620c45f1a2759
Marcos Lilljedahl <marcosnils@gmail.com>
2015-04-28 01:37:50 -0300

    Replace Command with ProtocolCommand for missing methods

--- src/main/java/redis/clients/jedis/Connection.java
+++ src/main/java/redis/clients/jedis/Connection.java
@@ -81,7 +81,7 @@ public class Connection implements Closeable {
     }
   }
 
-  protected Connection sendCommand(final Command cmd, final String... args) {
+  protected Connection sendCommand(final ProtocolCommand cmd, final String... args) {
     final byte[][] bargs = new byte[args.length][];
     for (int i = 0; i < args.length; i++) {
       bargs[i] = SafeEncoder.encode(args[i]);
@@ -102,7 +102,7 @@ public class Connection implements Closeable {
     }
   }
 
-  protected Connection sendCommand(final Command cmd) {
+  protected Connection sendCommand(final ProtocolCommand cmd) {
     try {
       connect();
       Protocol.sendCommand(outputStream, cmd, new byte[0][]);

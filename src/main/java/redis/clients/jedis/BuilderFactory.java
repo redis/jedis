42d66829a12d28b7063fc960fb5bd43f9298a67c
Fevzi Ozgul <fevzi.ozgul@devfactory.com>
2016-06-02 00:32:41 +0300

    Fixing squid S1168 Empty arrays and collections should be returned instead of null- part 1 (#1299)
    * Fixing squid S1168 Empty arrays and collections should be returned instead of null part 1
    
    * individual collection namespaces
    
    * ListCommandTest class modified

--- src/main/java/redis/clients/jedis/BuilderFactory.java
+++ src/main/java/redis/clients/jedis/BuilderFactory.java
@@ -1,6 +1,7 @@
 package redis.clients.jedis;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -72,7 +73,7 @@ public final class BuilderFactory {
     @SuppressWarnings("unchecked")
     public List<String> build(Object data) {
       if (null == data) {
-        return null;
+        return Collections.emptyList();
       }
       List<byte[]> l = (List<byte[]>) data;
       final ArrayList<String> result = new ArrayList<String>(l.size());
@@ -137,7 +138,7 @@ public final class BuilderFactory {
     @SuppressWarnings("unchecked")
     public Set<String> build(Object data) {
       if (null == data) {
-        return null;
+        return Collections.emptySet();
       }
       List<byte[]> l = (List<byte[]>) data;
       final Set<String> result = new HashSet<String>(l.size());
@@ -162,7 +163,7 @@ public final class BuilderFactory {
     @SuppressWarnings("unchecked")
     public List<byte[]> build(Object data) {
       if (null == data) {
-        return null;
+        return Collections.emptyList();
       }
       List<byte[]> l = (List<byte[]>) data;
 
@@ -179,7 +180,7 @@ public final class BuilderFactory {
     @SuppressWarnings("unchecked")
     public Set<byte[]> build(Object data) {
       if (null == data) {
-        return null;
+        return Collections.emptySet();
       }
       List<byte[]> l = (List<byte[]>) data;
       final Set<byte[]> result = new LinkedHashSet<byte[]>(l);
@@ -222,7 +223,7 @@ public final class BuilderFactory {
     @SuppressWarnings("unchecked")
     public Set<String> build(Object data) {
       if (null == data) {
-        return null;
+        return Collections.emptySet();
       }
       List<byte[]> l = (List<byte[]>) data;
       final Set<String> result = new LinkedHashSet<String>(l.size());
@@ -247,7 +248,7 @@ public final class BuilderFactory {
     @SuppressWarnings("unchecked")
     public Set<Tuple> build(Object data) {
       if (null == data) {
-        return null;
+        return Collections.emptySet();
       }
       List<byte[]> l = (List<byte[]>) data;
       final Set<Tuple> result = new LinkedHashSet<Tuple>(l.size());
@@ -270,7 +271,7 @@ public final class BuilderFactory {
     @SuppressWarnings("unchecked")
     public Set<Tuple> build(Object data) {
       if (null == data) {
-        return null;
+        return Collections.emptySet();
       }
       List<byte[]> l = (List<byte[]>) data;
       final Set<Tuple> result = new LinkedHashSet<Tuple>(l.size());
@@ -348,7 +349,7 @@ public final class BuilderFactory {
     @Override
     public List<GeoCoordinate> build(Object data) {
       if (null == data) {
-        return null;
+        return Collections.emptyList();
       }
       return interpretGeoposResult((List<Object>) data);
     }
@@ -378,7 +379,7 @@ public final class BuilderFactory {
     @Override
     public List<GeoRadiusResponse> build(Object data) {
       if (data == null) {
-        return null;
+        return Collections.emptyList();
       } else {
         List<Object> objectList = (List<Object>) data;
 
@@ -438,7 +439,7 @@ public final class BuilderFactory {
     @Override
     public List<Module> build(Object data) {
       if (data == null) {
-        return null;
+        return Collections.emptyList();
       } else {
         List<List<Object>> objectList = (List<List<Object>>) data;
 

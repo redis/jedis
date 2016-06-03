c81e3d1cbcec07cf2fb32a8f1a5b4064805de4b1
Fevzi Ozgul <fevzi.ozgul@devfactory.com>
2016-06-03 22:13:53 +0300

    Fixing squid: S1948 Fields in a "Serializable" class should either be transient or serializable (#1304)

--- src/main/java/redis/clients/util/JedisByteHashMap.java
+++ src/main/java/redis/clients/util/JedisByteHashMap.java
@@ -11,7 +11,7 @@ import java.util.Set;
 
 public class JedisByteHashMap implements Map<byte[], byte[]>, Cloneable, Serializable {
   private static final long serialVersionUID = -6971431362627219416L;
-  private Map<ByteArrayWrapper, byte[]> internalMap = new HashMap<ByteArrayWrapper, byte[]>();
+  private transient Map<ByteArrayWrapper, byte[]> internalMap = new HashMap<ByteArrayWrapper, byte[]>();
 
   public void clear() {
     internalMap.clear();

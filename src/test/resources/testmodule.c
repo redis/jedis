a911ff6cc87fa430dac4cbe0017633796907b839
Marcos Lilljedahl <marcosnils@gmail.com>
2016-06-21 00:56:47 -0300

    Fix OnLoad signature and configure command parameters properly

--- src/test/resources/testmodule.c
+++ src/test/resources/testmodule.c
@@ -6,12 +6,12 @@ int HelloworldRand_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, i
     return REDISMODULE_OK;
 }
 
-int RedisModule_OnLoad(RedisModuleCtx *ctx) {
+int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
  if (RedisModule_Init(ctx,"testmodule",1,REDISMODULE_APIVER_1)
         == REDISMODULE_ERR) return REDISMODULE_ERR;
 
     if (RedisModule_CreateCommand(ctx,"testmodule.simple",
         HelloworldRand_RedisCommand,
-        "write deny-oom",1,2,1) == REDISMODULE_ERR)
+        "readonly",0,0,0) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
 }

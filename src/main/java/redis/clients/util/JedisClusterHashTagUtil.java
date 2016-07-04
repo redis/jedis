ff74424be01e50dbd7f511c39c01f6e1b4ec5816
Idan Shinberg <idans@ybrantdigital.com>
2016-07-04 22:37:58 +0300

    Trimmed The extractTagMethod to accept a single boolean param indicating  if the origin key should be returned on mismatch or not

--- src/main/java/redis/clients/util/JedisClusterHashTagUtil.java
+++ src/main/java/redis/clients/util/JedisClusterHashTagUtil.java
@@ -1,8 +1,7 @@
 package redis.clients.util;
 
 /**
- * Holds various methods/utilities to manipualte and parse redis hash-tags.
- * See <a
+ * Holds various methods/utilities to manipualte and parse redis hash-tags. See <a
  * href="http://redis.io/topics/cluster-spec">Cluster-Spec : Keys hash tags</a>
  */
 public final class JedisClusterHashTagUtil {
@@ -12,30 +11,22 @@ public final class JedisClusterHashTagUtil {
   }
 
   public static String getHashTag(String key) {
-    return extractHashTag(key, false, false);
+    return extractHashTag(key, true);
   }
 
   public static boolean isClusterCompliantMatchPattern(String matchPattern) {
-    String tag = extractHashTag(matchPattern, true, true);
-    return tag != null && !tag.isEmpty() ;
+    String tag = extractHashTag(matchPattern, false);
+    return tag != null && !tag.isEmpty();
   }
 
-  private static String extractHashTag(String key, boolean includeBrackets, boolean returnNullWhenTagNotFound) {
+  private static String extractHashTag(String key, boolean returnKeyOnAbsence) {
     int s = key.indexOf("{");
     if (s > -1) {
       int e = key.indexOf("}", s + 1);
       if (e > -1 && e != s + 1) {
-        if (includeBrackets) {
-          return key.substring(s, e + 1);
-        } else {
-          return key.substring(s + 1, e);
-        }
+        return key.substring(s + 1, e);
       }
     }
-    if (returnNullWhenTagNotFound) {
-      return null;
-    } else {
-      return key;
-    }
-  }  
+    return returnKeyOnAbsence ? key : null;
+  }
 }

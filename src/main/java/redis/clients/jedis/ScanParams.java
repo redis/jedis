24edeb5b970df8db48eb266feb310a5cc258c514
Idan Shinberg <idans@ybrantdigital.com>
2016-07-04 12:29:38 +0300

    Changed count return type to boxed Integer version to match behavior with match

--- src/main/java/redis/clients/jedis/ScanParams.java
+++ src/main/java/redis/clients/jedis/ScanParams.java
@@ -16,49 +16,48 @@ import redis.clients.util.SafeEncoder;
 
 public class ScanParams {
 
-    private final Map<Keyword, ByteBuffer> params = new EnumMap<Keyword, ByteBuffer>(Keyword.class);
+  private final Map<Keyword, ByteBuffer> params = new EnumMap<Keyword, ByteBuffer>(Keyword.class);
 
+  public final static String SCAN_POINTER_START = String.valueOf(0);
+  public final static byte[] SCAN_POINTER_START_BINARY = SafeEncoder.encode(SCAN_POINTER_START);
 
-    public final static String SCAN_POINTER_START = String.valueOf(0);
-    public final static byte[] SCAN_POINTER_START_BINARY = SafeEncoder.encode(SCAN_POINTER_START);
+  public ScanParams match(final byte[] pattern) {
+    params.put(MATCH, ByteBuffer.wrap(pattern));
+    return this;
+  }
 
-    public ScanParams match(final byte[] pattern) {
-        params.put(MATCH, ByteBuffer.wrap(pattern));
-        return this;
+  public ScanParams match(final String pattern) {
+    params.put(MATCH, ByteBuffer.wrap(SafeEncoder.encode(pattern)));
+    return this;
+  }
+
+  public ScanParams count(final Integer count) {
+    params.put(COUNT, ByteBuffer.wrap(Protocol.toByteArray(count)));
+    return this;
+  }
+
+  public Collection<byte[]> getParams() {
+    List<byte[]> paramsList = new ArrayList<byte[]>(params.size());
+    for (Map.Entry<Keyword, ByteBuffer> param : params.entrySet()) {
+      paramsList.add(param.getKey().raw);
+      paramsList.add(param.getValue().array());
     }
+    return Collections.unmodifiableCollection(paramsList);
+  }
 
-    public ScanParams match(final String pattern) {
-        params.put(MATCH, ByteBuffer.wrap(SafeEncoder.encode(pattern)));
-        return this;
+  String match() {
+    if (params.containsKey(MATCH)) {
+      return new String(params.get(MATCH).array());
+    } else {
+      return null;
     }
+  }
 
-    public ScanParams count(final int count) {
-        params.put(COUNT, ByteBuffer.wrap(Protocol.toByteArray(count)));
-        return this;
+  Integer count() {
+    if (params.containsKey(COUNT)) {
+      return params.get(COUNT).getInt();
+    } else {
+      return null;
     }
-
-    public Collection<byte[]> getParams() {
-        List<byte[]> paramsList = new ArrayList<byte[]>(params.size());
-        for (Map.Entry<Keyword, ByteBuffer> param : params.entrySet()) {
-            paramsList.add(param.getKey().raw);
-            paramsList.add(param.getValue().array());
-        }
-        return Collections.unmodifiableCollection(paramsList);
-    }
-
-    String match() {
-        if (params.containsKey(MATCH)) {
-            return new String(params.get(MATCH).array());
-        } else {
-            return null ;
-        }
-    }
-    
-    int count() {
-        if (params.containsKey(COUNT)) {
-            return params.get(COUNT).getInt();
-        } else {
-            return 0;
-        }
-    }       
+  }
 }

1e159d5ab429efa608396ff92cc37a0db29aa746
stroller <fujian1115@gmail.com>
2016-09-20 05:10:43 +0800

    Support setting client name for JedisCluster (#1386)
    [Motivation]
    
    JedisCluster should support setting client name similar with other types
    of Jedis.
    
    [Modifications]
    
    (1) add private member: clientName for JedisClusterInfoCache;
    (2) add new constructor with clientName for JedisCluster.
    
    [Result]
    
    JedisCluster support setting client name.

--- src/main/java/redis/clients/jedis/JedisClusterInfoCache.java
+++ src/main/java/redis/clients/jedis/JedisClusterInfoCache.java
@@ -31,19 +31,21 @@ public class JedisClusterInfoCache {
   private int connectionTimeout;
   private int soTimeout;
   private String password;
+  private String clientName;
 
   private static final int MASTER_NODE_INDEX = 2;
 
   public JedisClusterInfoCache(final GenericObjectPoolConfig poolConfig, int timeout) {
-    this(poolConfig, timeout, timeout, null);
+    this(poolConfig, timeout, timeout, null, null);
   }
 
   public JedisClusterInfoCache(final GenericObjectPoolConfig poolConfig,
-      final int connectionTimeout, final int soTimeout, final String password) {
+      final int connectionTimeout, final int soTimeout, final String password, final String clientName) {
     this.poolConfig = poolConfig;
     this.connectionTimeout = connectionTimeout;
     this.soTimeout = soTimeout;
     this.password = password;
+    this.clientName = clientName;
   }
 
   public void discoverClusterNodesAndSlots(Jedis jedis) {
@@ -156,7 +158,7 @@ public class JedisClusterInfoCache {
       if (existingPool != null) return existingPool;
 
       JedisPool nodePool = new JedisPool(poolConfig, node.getHost(), node.getPort(),
-          connectionTimeout, soTimeout, password, 0, null, false, null, null, null);
+          connectionTimeout, soTimeout, password, 0, clientName, false, null, null, null);
       nodes.put(nodeKey, nodePool);
       return nodePool;
     } finally {

b0584a020c2f746c90f8c373ed959a3bb461a0c2
Marcos Nils <marcosnils@gmail.com>
2016-10-24 15:01:56 +0200

    Change BinaryJedis bitfield return type to List<Long> (#1414)
    Fixes #1413
    
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- src/main/java/redis/clients/jedis/BinaryJedis.java
+++ src/main/java/redis/clients/jedis/BinaryJedis.java
@@ -3726,10 +3726,10 @@ public class BinaryJedis implements BasicCommands, BinaryJedisCommands, MultiKey
   }
 
   @Override
-  public List<byte[]> bitfield(byte[] key, byte[]... arguments) {
+  public List<Long> bitfield(byte[] key, byte[]... arguments) {
     checkIsInMultiOrPipeline();
     client.bitfield(key, arguments);
-    return client.getBinaryMultiBulkReply();
+    return client.getIntegerMultiBulkReply();
   }
 
   @Override

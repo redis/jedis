a6a59692e067e1cf810e4d3ff3506fb65757e041
Idan Sheinberg <ishinberg0@gmail.com>
2016-11-04 15:54:48 +0200

    Revert "Fixed MasterListener so a broader exception will be catched"
    This reverts commit 93be077b7bbe41801f8c25eb9b78e27fa9f58b20.

--- src/main/java/redis/clients/jedis/JedisSentinelPool.java
+++ src/main/java/redis/clients/jedis/JedisSentinelPool.java
@@ -298,7 +298,7 @@ public class JedisSentinelPool extends JedisPoolAbstract {
             }
           }, "+switch-master");
 
-        } catch (JedisException e) {
+        } catch (JedisConnectionException e) {
 
           if (running.get()) {
             log.log(Level.SEVERE, "Lost connection to Sentinel at " + host + ":" + port

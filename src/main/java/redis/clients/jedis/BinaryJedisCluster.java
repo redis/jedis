aa83bd02dfbbc8220cf8d803f0ffb5f920d4e0a6
Marcos Nils <marcosnils@gmail.com>
2017-01-04 16:40:22 +0200

    Add psetex to JedisCluster (#1457)

--- src/main/java/redis/clients/jedis/BinaryJedisCluster.java
+++ src/main/java/redis/clients/jedis/BinaryJedisCluster.java
@@ -264,6 +264,16 @@ public class BinaryJedisCluster implements BasicCommands, BinaryJedisClusterComm
   }
 
   @Override
+  public String psetex(final byte[] key, final long milliseconds, final byte[] value) {
+    return new JedisClusterCommand<String>(connectionHandler, maxAttempts) {
+      @Override
+      public String execute(Jedis connection) {
+        return connection.psetex(key, milliseconds, value);
+      }
+    }.runBinary(key);
+  }
+
+  @Override
   public String setex(final byte[] key, final int seconds, final byte[] value) {
     return new JedisClusterCommand<String>(connectionHandler, maxAttempts) {
       @Override

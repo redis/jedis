d40b2f36b80db31a83b859c91ea3da08f3f50b01
Christoph Kutzinski <kutzi@gmx.de>
2017-01-23 15:08:07 +0100

    #1458 add a more prominent warning for keys() (#1459)
    * #1458: more prominent warning about keys() performance implications
    
    Also add Javadoc for scan()
    
    * #1458: add 'see also' link to official Redis documentation
    
    * #1458: add a bit of JavaDoc to ScanParams and ScanResult
    
    * Add convenience method to check for the end of the iteration
    
    * Add convenience method to check for the end of the iteration (unit test)
    
    * Add convenience method to check for the end of the iteration (unit test)

--- src/main/java/redis/clients/jedis/Jedis.java
+++ src/main/java/redis/clients/jedis/Jedis.java
@@ -230,34 +230,6 @@ public class Jedis extends BinaryJedis implements JedisCommands, MultiKeyCommand
     return client.getStatusCodeReply();
   }
 
-  /**
-   * Returns all the keys matching the glob-style pattern as space separated strings. For example if
-   * you have in the database the keys "foo" and "foobar" the command "KEYS foo*" will return
-   * "foo foobar".
-   * <p>
-   * Note that while the time complexity for this operation is O(n) the constant times are pretty
-   * low. For example Redis running on an entry level laptop can scan a 1 million keys database in
-   * 40 milliseconds. <b>Still it's better to consider this one of the slow commands that may ruin
-   * the DB performance if not used with care.</b>
-   * <p>
-   * In other words this command is intended only for debugging and special operations like creating
-   * a script to change the DB schema. Don't use it in your normal code. Use Redis Sets in order to
-   * group together a subset of objects.
-   * <p>
-   * Glob style patterns examples:
-   * <ul>
-   * <li>h?llo will match hello hallo hhllo
-   * <li>h*llo will match hllo heeeello
-   * <li>h[ae]llo will match hello and hallo, but not hillo
-   * </ul>
-   * <p>
-   * Use \ to escape special chars if you want to match them verbatim.
-   * <p>
-   * Time complexity: O(n) (with n being the number of keys in the DB, and assuming keys and pattern
-   * of limited length)
-   * @param pattern
-   * @return Multi bulk reply
-   */
   @Override
   public Set<String> keys(final String pattern) {
     checkIsInMultiOrPipeline();

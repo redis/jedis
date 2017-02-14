1a7f760d097c5d9362bb6ebeebf27ee3a5a862ff
shmulik-ubimo <shmulik@ubimo.com>
2017-02-14 09:17:19 +0200

    Fix formatting issue in JedisExhaustedPoolException

--- src/main/java/redis/clients/jedis/exceptions/JedisExhaustedPoolException.java
+++ src/main/java/redis/clients/jedis/exceptions/JedisExhaustedPoolException.java
@@ -1,8 +1,8 @@
 package redis.clients.jedis.exceptions;
 
 /**
- * This exception will be thrown when the Jedis client isn't able to retrieve a connection from
- * the pool, since all the connections are being used (a.k.a. an "exhausted" pool).
+ * This exception will be thrown when the Jedis client isn't able to retrieve a connection from the
+ * pool, since all the connections are being used (a.k.a. an "exhausted" pool).
  */
 public class JedisExhaustedPoolException extends JedisException {
 

027f8dab306575c1ecf6f6a9a7521254f54a0bda
sazzad16 <sazzad.cse.sust@gmail.com>
2017-06-18 09:06:12 +0800

    Add missing methods in RedisPipeline and BinaryRedisPipeline

--- src/main/java/redis/clients/jedis/commands/BinaryRedisPipeline.java
+++ src/main/java/redis/clients/jedis/commands/BinaryRedisPipeline.java
@@ -1,5 +1,6 @@
 package redis.clients.jedis.commands;
 
+import redis.clients.jedis.BitPosParams;
 import redis.clients.jedis.GeoCoordinate;
 import redis.clients.jedis.GeoRadiusResponse;
 import redis.clients.jedis.GeoUnit;
@@ -8,6 +9,7 @@ import redis.clients.jedis.Response;
 import redis.clients.jedis.SortingParams;
 import redis.clients.jedis.Tuple;
 import redis.clients.jedis.params.GeoRadiusParam;
+import redis.clients.jedis.params.SetParams;
 import redis.clients.jedis.params.ZAddParams;
 import redis.clients.jedis.params.ZIncrByParams;
 
@@ -301,4 +303,24 @@ public interface BinaryRedisPipeline {
   Response<List<Long>> bitfield(byte[] key, byte[]... elements);
 
   Response<Long> hstrlen(byte[] key, byte[] field);
+
+  Response<Long> bitpos(byte[] key, boolean value);
+
+  Response<Long> bitpos(byte[] key, boolean value, BitPosParams params);
+
+  Response<String> set(byte[] key, byte[] value, SetParams params);
+
+  Response<List<byte[]>> srandmember(byte[] key, int count);
+
+  Response<Long> objectRefcount(byte[] key);
+
+  Response<byte[]> objectEncoding(byte[] key);
+
+  Response<Long> objectIdletime(byte[] key);
+
+  Response<Double> incrByFloat(byte[] key, double increment);
+
+  Response<String> psetex(byte[] key, long milliseconds, byte[] value);
+
+  Response<Double> hincrByFloat(byte[] key, byte[] field, double increment);
 }

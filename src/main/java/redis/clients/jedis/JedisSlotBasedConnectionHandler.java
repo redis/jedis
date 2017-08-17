0ea89a87e106f45ec9b3b2274dca565b5955b4fc
zhaoding <zhaoding@example.com>
2017-08-17 10:53:05 +0800

    根据槽返回对应的池

--- src/main/java/redis/clients/jedis/JedisSlotBasedConnectionHandler.java
+++ src/main/java/redis/clients/jedis/JedisSlotBasedConnectionHandler.java
@@ -10,73 +10,86 @@ import redis.clients.jedis.exceptions.JedisNoReachableClusterNodeException;
 
 public class JedisSlotBasedConnectionHandler extends JedisClusterConnectionHandler {
 
-  public JedisSlotBasedConnectionHandler(Set<HostAndPort> nodes,
-      final GenericObjectPoolConfig poolConfig, int timeout) {
-    this(nodes, poolConfig, timeout, timeout);
-  }
+	public JedisSlotBasedConnectionHandler(Set<HostAndPort> nodes, final GenericObjectPoolConfig poolConfig,
+			int timeout) {
+		this(nodes, poolConfig, timeout, timeout);
+	}
 
-  public JedisSlotBasedConnectionHandler(Set<HostAndPort> nodes,
-      final GenericObjectPoolConfig poolConfig, int connectionTimeout, int soTimeout) {
-    super(nodes, poolConfig, connectionTimeout, soTimeout, null);
-  }
+	public JedisSlotBasedConnectionHandler(Set<HostAndPort> nodes, final GenericObjectPoolConfig poolConfig,
+			int connectionTimeout, int soTimeout) {
+		super(nodes, poolConfig, connectionTimeout, soTimeout, null);
+	}
 
-  public JedisSlotBasedConnectionHandler(Set<HostAndPort> nodes, GenericObjectPoolConfig poolConfig, int connectionTimeout, int soTimeout, String password) {
-    super(nodes, poolConfig, connectionTimeout, soTimeout, password);
-  }
+	public JedisSlotBasedConnectionHandler(Set<HostAndPort> nodes, GenericObjectPoolConfig poolConfig,
+			int connectionTimeout, int soTimeout, String password) {
+		super(nodes, poolConfig, connectionTimeout, soTimeout, password);
+	}
 
-  public JedisSlotBasedConnectionHandler(Set<HostAndPort> nodes, GenericObjectPoolConfig poolConfig, int connectionTimeout, int soTimeout, String password, String clientName) {
-    super(nodes, poolConfig, connectionTimeout, soTimeout, password, clientName);
-  }
+	public JedisSlotBasedConnectionHandler(Set<HostAndPort> nodes, GenericObjectPoolConfig poolConfig,
+			int connectionTimeout, int soTimeout, String password, String clientName) {
+		super(nodes, poolConfig, connectionTimeout, soTimeout, password, clientName);
+	}
 
-  @Override
-  public Jedis getConnection() {
-    // In antirez's redis-rb-cluster implementation,
-    // getRandomConnection always return valid connection (able to
-    // ping-pong)
-    // or exception if all connections are invalid
+	@Override
+	public Jedis getConnection() {
 
-    List<JedisPool> pools = cache.getShuffledNodesPool();
+		return getJedisPool().getResource();
 
-    for (JedisPool pool : pools) {
-      Jedis jedis = null;
-      try {
-        jedis = pool.getResource();
+	}
 
-        if (jedis == null) {
-          continue;
-        }
+	@Override
+	public Jedis getConnectionFromSlot(int slot) {
+		return getJedisPoolFromSlot(slot).getResource();
+	}
 
-        String result = jedis.ping();
+	@Override
+	public JedisPool getJedisPoolFromSlot(int slot) {
+		JedisPool connectionPool = cache.getSlotPool(slot);
+		if (connectionPool != null) {
+			// It can't guaranteed to get valid connection because of node
+			// assignment
+			return connectionPool;
+		} else {
+			renewSlotCache(); // It's abnormal situation for cluster mode, that we have just nothing for slot,
+								// try to rediscover state
+			connectionPool = cache.getSlotPool(slot);
+			if (connectionPool != null) {
+				return connectionPool;
+			} else {
+				// no choice, fallback to new connection to random node
+				return getJedisPool();
+			}
+		}
+	}
 
-        if (result.equalsIgnoreCase("pong")) return jedis;
+	@Override
+	public JedisPool getJedisPool() {
+		// In antirez's redis-rb-cluster implementation,
+		// getRandomConnection always return valid connection (able to
+		// ping-pong)
+		// or exception if all connections are invalid
+		List<JedisPool> pools = cache.getShuffledNodesPool();
 
-        jedis.close();
-      } catch (JedisException ex) {
-        if (jedis != null) {
-          jedis.close();
-        }
-      }
-    }
+		for (JedisPool pool : pools) {
+			Jedis jedis = null;
+			try {
+				jedis = pool.getResource();
 
-    throw new JedisNoReachableClusterNodeException("No reachable node in cluster");
-  }
+				if (jedis == null) {
+					continue;
+				}
 
-  @Override
-  public Jedis getConnectionFromSlot(int slot) {
-    JedisPool connectionPool = cache.getSlotPool(slot);
-    if (connectionPool != null) {
-      // It can't guaranteed to get valid connection because of node
-      // assignment
-      return connectionPool.getResource();
-    } else {
-      renewSlotCache(); //It's abnormal situation for cluster mode, that we have just nothing for slot, try to rediscover state
-      connectionPool = cache.getSlotPool(slot);
-      if (connectionPool != null) {
-        return connectionPool.getResource();
-      } else {
-        //no choice, fallback to new connection to random node
-        return getConnection();
-      }
-    }
-  }
+				String result = jedis.ping();
+				if (result.equalsIgnoreCase("pong"))
+					return pool;
+
+				jedis.close();
+			} catch (JedisException ex) {
+				if (jedis != null) {
+					jedis.close();
+				}
+			}
+		}
+		throw new JedisNoReachableClusterNodeException("No reachable node in cluster");
+	}
 }

bdb0a1fb93e952992377e3e01bc3610fa5b4dd66
sazzad16 <sazzad.cse.sust@gmail.com>
2017-10-22 11:57:37 +0600

    Reduced encoding while creating Tuple.
    In following code:
    ```
    new Tuple(SafeEncoder.encode(iterator.next())
    ```
    there are actually 2 conversions happening. First a byte[] to String. And then String to byte[] in Tuple constructor.
    
    Changing the code to:
    ```
    new Tuple(iterator.next())
    ```
    reduces the number of conversions to 0.
    
    Also, used available DOUBLE.build().

--- src/main/java/redis/clients/jedis/Jedis.java
+++ src/main/java/redis/clients/jedis/Jedis.java
@@ -3256,8 +3256,7 @@ public class Jedis extends BinaryJedis implements JedisCommands, MultiKeyCommand
     List<byte[]> rawResults = (List<byte[]>) result.get(1);
     Iterator<byte[]> iterator = rawResults.iterator();
     while (iterator.hasNext()) {
-      results.add(new Tuple(SafeEncoder.encode(iterator.next()), Double.valueOf(SafeEncoder
-          .encode(iterator.next()))));
+      results.add(new Tuple(iterator.next(), BuilderFactory.DOUBLE.build(iterator.next())));
     }
     return new ScanResult<Tuple>(newcursor, results);
   }

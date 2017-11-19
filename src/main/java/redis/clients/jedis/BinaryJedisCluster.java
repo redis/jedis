0680aa183701407ce4245778ca6a2a132ed2a854
M Sazzadul Hoque <sazzad16@users.noreply.github.com>
2017-11-19 06:47:18 +0600

    Added final to the parameters of public methods. (#1598)

--- src/main/java/redis/clients/jedis/BinaryJedisCluster.java
+++ src/main/java/redis/clients/jedis/BinaryJedisCluster.java
@@ -1302,7 +1302,7 @@ public class BinaryJedisCluster implements BasicCommands, BinaryJedisClusterComm
   }
 
   @Override
-  public Object eval(final byte[] script, byte[] key) {
+  public Object eval(final byte[] script, final byte[] key) {
     return new JedisClusterCommand<Object>(connectionHandler, maxAttempts) {
       @Override
       public Object execute(Jedis connection) {
@@ -1312,7 +1312,7 @@ public class BinaryJedisCluster implements BasicCommands, BinaryJedisClusterComm
   }
 
   @Override
-  public Object evalsha(final byte[] sha1, byte[] key) {
+  public Object evalsha(final byte[] sha1, final byte[] key) {
     return new JedisClusterCommand<Object>(connectionHandler, maxAttempts) {
       @Override
       public Object execute(Jedis connection) {
@@ -1372,7 +1372,7 @@ public class BinaryJedisCluster implements BasicCommands, BinaryJedisClusterComm
   }
 
   @Override
-  public String scriptKill(byte[] key) {
+  public String scriptKill(final byte[] key) {
     return new JedisClusterCommand<String>(connectionHandler, maxAttempts) {
       @Override
       public String execute(Jedis connection) {

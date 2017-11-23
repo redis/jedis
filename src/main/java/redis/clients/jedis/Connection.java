aacd1d77d8c53ac57ae60091c5115e76f650bf03
M Sazzadul Hoque <sazzad16@users.noreply.github.com>
2017-11-23 23:11:15 +0600

    Added missing flush in getRawObjectMultiBulkReply. (#1619)
    Removed flush from getObjectMultiBulkReply because it just calls getRawObjectMultiBulkReply and flush is added there.

--- src/main/java/redis/clients/jedis/Connection.java
+++ src/main/java/redis/clients/jedis/Connection.java
@@ -273,11 +273,11 @@ public class Connection implements Closeable {
 
   @SuppressWarnings("unchecked")
   public List<Object> getRawObjectMultiBulkReply() {
+    flush();
     return (List<Object>) readProtocolWithCheckingBroken();
   }
 
   public List<Object> getObjectMultiBulkReply() {
-    flush();
     return getRawObjectMultiBulkReply();
   }
 

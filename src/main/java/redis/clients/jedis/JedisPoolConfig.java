9341970affad711ec13ed5224a055a700ff34e49
zhixin02.liu <zhixin02.liu@vipshop.com>
2017-11-23 10:23:17 +0800

    Change JedisPoolConfig's default value

--- src/main/java/redis/clients/jedis/JedisPoolConfig.java
+++ src/main/java/redis/clients/jedis/JedisPoolConfig.java
@@ -6,7 +6,8 @@ public class JedisPoolConfig extends GenericObjectPoolConfig {
   public JedisPoolConfig() {
     // defaults to make your life with connection pool easier :)
     setTestWhileIdle(true);
-    setMinEvictableIdleTimeMillis(60000);
+    setSoftMinEvictableIdleTimeMillis(60000);
+    setMinEvictableIdleTimeMillis(-1);
     setTimeBetweenEvictionRunsMillis(30000);
     setNumTestsPerEvictionRun(-1);
   }

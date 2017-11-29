8c5369562390bee531eb35c00744821b0c4821e3
M Sazzadul Hoque <sazzad.cse.sust@gmail.com>
2017-11-29 16:50:03 +0600

    Fix issue due to rebase/merge

--- src/main/java/redis/clients/jedis/BinaryJedis.java
+++ src/main/java/redis/clients/jedis/BinaryJedis.java
@@ -2441,7 +2441,7 @@ public class BinaryJedis implements BasicCommands, BinaryJedisCommands, MultiKey
   public Set<Tuple> zrangeByScoreWithScores(final byte[] key, final double min, final double max) {
     checkIsInMultiOrPipeline();
     client.zrangeByScoreWithScores(key, min, max);
-    return getBinaryTupledSet();
+    return getTupledSet();
   }
 
   @Override
@@ -2503,7 +2503,7 @@ public class BinaryJedis implements BasicCommands, BinaryJedisCommands, MultiKey
       final int offset, final int count) {
     checkIsInMultiOrPipeline();
     client.zrangeByScoreWithScores(key, min, max, offset, count);
-    return getBinaryTupledSet();
+    return getTupledSet();
   }
 
   @Override
@@ -2561,7 +2561,7 @@ public class BinaryJedis implements BasicCommands, BinaryJedisCommands, MultiKey
   public Set<Tuple> zrevrangeByScoreWithScores(final byte[] key, final double max, final double min) {
     checkIsInMultiOrPipeline();
     client.zrevrangeByScoreWithScores(key, max, min);
-    return getBinaryTupledSet();
+    return getTupledSet();
   }
 
   @Override
@@ -2569,7 +2569,7 @@ public class BinaryJedis implements BasicCommands, BinaryJedisCommands, MultiKey
       final double min, final int offset, final int count) {
     checkIsInMultiOrPipeline();
     client.zrevrangeByScoreWithScores(key, max, min, offset, count);
-    return getBinaryTupledSet();
+    return getTupledSet();
   }
 
   @Override

129d1efc2a1928bb8d39984f8a74614ebe488325
M Sazzadul Hoque <sazzad16@users.noreply.github.com>
2017-11-29 12:07:31 +0600

    Call client for zcount in BinaryJedis (#1683)

--- src/main/java/redis/clients/jedis/BinaryJedis.java
+++ src/main/java/redis/clients/jedis/BinaryJedis.java
@@ -2260,7 +2260,9 @@ public class BinaryJedis implements BasicCommands, BinaryJedisCommands, MultiKey
 
   @Override
   public Long zcount(final byte[] key, final double min, final double max) {
-    return zcount(key, toByteArray(min), toByteArray(max));
+    checkIsInMultiOrPipeline();
+    client.zcount(key, min, max);
+    return client.getIntegerReply();
   }
 
   @Override

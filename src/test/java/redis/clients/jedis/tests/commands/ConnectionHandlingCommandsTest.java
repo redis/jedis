a87374b689c0fe92bb9368224524ba39a0eb3e43
M Sazzadul Hoque <sazzad16@users.noreply.github.com>
2017-12-12 15:30:16 +0600

    Modify ConnectionHandlingCommandsTest (#1695)
    * Remove extending JedisCommandTestBase. This saves unnecessary setUp, tearDown.
    
    * Provide HostAndPort for Jedis and BinaryJedis. Only host (evidently localhost) is read which doesn't make complete sense. Either both host and port or none should be read.

--- src/test/java/redis/clients/jedis/tests/commands/ConnectionHandlingCommandsTest.java
+++ src/test/java/redis/clients/jedis/tests/commands/ConnectionHandlingCommandsTest.java
@@ -6,19 +6,21 @@ import org.junit.Test;
 
 import redis.clients.jedis.BinaryJedis;
 import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.tests.HostAndPortUtil;
 
-public class ConnectionHandlingCommandsTest extends JedisCommandTestBase {
-  protected static HostAndPort hnp = HostAndPortUtil.getRedisServers().get(0);
+public class ConnectionHandlingCommandsTest {
+  private static HostAndPort hnp = HostAndPortUtil.getRedisServers().get(0);
 
   @Test
   public void quit() {
+    Jedis jedis = new Jedis(hnp);
     assertEquals("OK", jedis.quit());
   }
 
   @Test
   public void binary_quit() {
-    BinaryJedis bj = new BinaryJedis(hnp.getHost());
+    BinaryJedis bj = new BinaryJedis(hnp);
     assertEquals("OK", bj.quit());
   }
 }

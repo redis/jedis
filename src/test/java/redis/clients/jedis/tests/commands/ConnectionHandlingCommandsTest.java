32e6d231effec90b67da824b77ffa8c08c32db7a
M Sazzadul Hoque <sazzad.cse.sust@gmail.com>
2017-12-12 06:15:40 +0600

    Provide host and port for Jedis and BinaryJedis

--- src/test/java/redis/clients/jedis/tests/commands/ConnectionHandlingCommandsTest.java
+++ src/test/java/redis/clients/jedis/tests/commands/ConnectionHandlingCommandsTest.java
@@ -5,19 +5,22 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import redis.clients.jedis.BinaryJedis;
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.tests.HostAndPortUtil;
 
 public class ConnectionHandlingCommandsTest {
+  private static HostAndPort hnp = HostAndPortUtil.getRedisServers().get(0);
 
   @Test
   public void quit() {
-    Jedis jedis = new Jedis();
+    Jedis jedis = new Jedis(hnp);
     assertEquals("OK", jedis.quit());
   }
 
   @Test
   public void binary_quit() {
-    BinaryJedis bj = new BinaryJedis();
+    BinaryJedis bj = new BinaryJedis(hnp);
     assertEquals("OK", bj.quit());
   }
 }

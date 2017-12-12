ca4415f17aafd2d5016bee6b687cfffdf554544c
M Sazzadul Hoque <sazzad16@users.noreply.github.com>
2017-12-12 18:21:27 +0600

    Refactor imports (#1703)

--- src/test/java/redis/clients/jedis/tests/collections/SetFromListTest.java
+++ src/test/java/redis/clients/jedis/tests/collections/SetFromListTest.java
@@ -1,6 +1,8 @@
 package redis.clients.jedis.tests.collections;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;

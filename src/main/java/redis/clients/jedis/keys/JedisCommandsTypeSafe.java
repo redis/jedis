3a59d71cfb4542def39950246d74e75add8a174b
Ondrej Zizka <zizka@seznam.cz>
2018-01-15 13:21:03 +0100

    Support type safe keys https://github.com/xetorthio/jedis/issues/1745

---
+++ src/main/java/redis/clients/jedis/keys/JedisCommandsTypeSafe.java
@@ -0,0 +1,299 @@
+package redis.clients.jedis.keys;
+
+import redis.clients.jedis.*;
+import redis.clients.jedis.commands.JedisCommands;
+import redis.clients.jedis.params.geo.GeoRadiusParam;
+import redis.clients.jedis.params.set.SetParams;
+import redis.clients.jedis.params.sortedset.ZAddParams;
+import redis.clients.jedis.params.sortedset.ZIncrByParams;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public interface JedisCommandsTypeSafe extends JedisCommands {
+
+    String set(String key, String value);
+
+    String set(String key, String value, SetParams params);
+
+    String get(String key);
+
+    Boolean exists(String key);
+
+    Long persist(String key);
+
+    String type(String key);
+
+    byte[] dump(String key);
+
+    String restore(String key, int ttl, byte[] serializedValue);
+
+    Long expire(String key, int seconds);
+
+    Long pexpire(String key, long milliseconds);
+
+    Long expireAt(String key, long unixTime);
+
+    Long pexpireAt(String key, long millisecondsTimestamp);
+
+    Long ttl(String key);
+
+    Long pttl(String key);
+
+    Long touch(String key);
+
+    Boolean setbit(String key, long offset, boolean value);
+
+    Boolean setbit(String key, long offset, String value);
+
+    Boolean getbit(String key, long offset);
+
+    Long setrange(String key, long offset, String value);
+
+    String getrange(String key, long startOffset, long endOffset);
+
+    String getSet(String key, String value);
+
+    Long setnx(String key, String value);
+
+    String setex(String key, int seconds, String value);
+
+    String psetex(String key, long milliseconds, String value);
+
+    Long decrBy(String key, long decrement);
+
+    Long decr(String key);
+
+    Long incrBy(String key, long increment);
+
+    Double incrByFloat(String key, double increment);
+
+    Long incr(String key);
+
+    Long append(String key, String value);
+
+    String substr(String key, int start, int end);
+
+    default Long hset(HashKey key, String field, String value) { return hset(key.getKey(), field, value); }
+
+    default Long hset(HashKey key, Map<String, String> hash) { return hset(key.getKey(), hash); }
+
+    default String hget(HashKey key, String field) { return hget(key.getKey(), field); }
+
+    default Long hsetnx(HashKey key, String field, String value) { return hsetnx(key.getKey(), field, value); }
+
+    default String hmset(HashKey key, Map<String, String> hash) { return hmset(key.getKey(), hash); }
+
+    default List<String> hmget(HashKey key, String... fields) { return hmget(key.getKey(), fields); }
+
+    default Long hincrBy(HashKey key, String field, long value) { return hincrBy(key.getKey(), field, value); }
+
+    default Double hincrByFloat(HashKey key, String field, double value) { return hincrByFloat(key.getKey(), field, value); }
+
+    default Boolean hexists(HashKey key, String field) { return hexists(key.getKey(), field); }
+
+    default Long hdel(HashKey key, String... field) { return hdel(key.getKey(), field); }
+
+    default Long hlen(HashKey key) { return hlen(key.getKey()); }
+
+    default Set<String> hkeys(HashKey key) { return hkeys(key.getKey()); }
+
+    default List<String> hvals(HashKey key) { return hvals(key.getKey()); }
+
+    default Map<String, String> hgetAll(HashKey key) { return hgetAll(key.getKey()); }
+
+    Long rpush(String key, String... string);
+
+    Long lpush(String key, String... string);
+
+    Long llen(String key);
+
+    List<String> lrange(String key, long start, long stop);
+
+    String ltrim(String key, long start, long stop);
+
+    String lindex(String key, long index);
+
+    String lset(String key, long index, String value);
+
+    Long lrem(String key, long count, String value);
+
+    String lpop(String key);
+
+    String rpop(String key);
+
+    Long sadd(String key, String... member);
+
+    Set<String> smembers(String key);
+
+    Long srem(String key, String... member);
+
+    String spop(String key);
+
+    Set<String> spop(String key, long count);
+
+    Long scard(String key);
+
+    Boolean sismember(String key, String member);
+
+    String srandmember(String key);
+
+    List<String> srandmember(String key, int count);
+
+    Long strlen(String key);
+
+    Long zadd(String key, double score, String member);
+
+    Long zadd(String key, double score, String member, ZAddParams params);
+
+    Long zadd(String key, Map<String, Double> scoreMembers);
+
+    Long zadd(String key, Map<String, Double> scoreMembers, ZAddParams params);
+
+    Set<String> zrange(String key, long start, long stop);
+
+    Long zrem(String key, String... members);
+
+    Double zincrby(String key, double increment, String member);
+
+    Double zincrby(String key, double increment, String member, ZIncrByParams params);
+
+    Long zrank(String key, String member);
+
+    Long zrevrank(String key, String member);
+
+    Set<String> zrevrange(String key, long start, long stop);
+
+    Set<Tuple> zrangeWithScores(String key, long start, long stop);
+
+    Set<Tuple> zrevrangeWithScores(String key, long start, long stop);
+
+    Long zcard(String key);
+
+    Double zscore(String key, String member);
+
+    List<String> sort(String key);
+
+    List<String> sort(String key, SortingParams sortingParameters);
+
+    Long zcount(String key, double min, double max);
+
+    Long zcount(String key, String min, String max);
+
+    Set<String> zrangeByScore(String key, double min, double max);
+
+    Set<String> zrangeByScore(String key, String min, String max);
+
+    Set<String> zrevrangeByScore(String key, double max, double min);
+
+    Set<String> zrangeByScore(String key, double min, double max, int offset, int count);
+
+    Set<String> zrevrangeByScore(String key, String max, String min);
+
+    Set<String> zrangeByScore(String key, String min, String max, int offset, int count);
+
+    Set<String> zrevrangeByScore(String key, double max, double min, int offset, int count);
+
+    Set<Tuple> zrangeByScoreWithScores(String key, double min, double max);
+
+    Set<Tuple> zrevrangeByScoreWithScores(String key, double max, double min);
+
+    Set<Tuple> zrangeByScoreWithScores(String key, double min, double max, int offset, int count);
+
+    Set<String> zrevrangeByScore(String key, String max, String min, int offset, int count);
+
+    Set<Tuple> zrangeByScoreWithScores(String key, String min, String max);
+
+    Set<Tuple> zrevrangeByScoreWithScores(String key, String max, String min);
+
+    Set<Tuple> zrangeByScoreWithScores(String key, String min, String max, int offset, int count);
+
+    Set<Tuple> zrevrangeByScoreWithScores(String key, double max, double min, int offset, int count);
+
+    Set<Tuple> zrevrangeByScoreWithScores(String key, String max, String min, int offset, int count);
+
+    Long zremrangeByRank(String key, long start, long stop);
+
+    Long zremrangeByScore(String key, double min, double max);
+
+    Long zremrangeByScore(String key, String min, String max);
+
+    Long zlexcount(String key, String min, String max);
+
+    Set<String> zrangeByLex(String key, String min, String max);
+
+    Set<String> zrangeByLex(String key, String min, String max, int offset, int count);
+
+    Set<String> zrevrangeByLex(String key, String max, String min);
+
+    Set<String> zrevrangeByLex(String key, String max, String min, int offset, int count);
+
+    Long zremrangeByLex(String key, String min, String max);
+
+    Long linsert(String key, ListPosition where, String pivot, String value);
+
+    Long lpushx(String key, String... string);
+
+    Long rpushx(String key, String... string);
+
+    List<String> blpop(int timeout, String key);
+
+    List<String> brpop(int timeout, String key);
+
+    Long del(String key);
+
+    Long unlink(String key);
+
+    String echo(String string);
+
+    Long move(String key, int dbIndex);
+
+    Long bitcount(String key);
+
+    Long bitcount(String key, long start, long end);
+
+    Long bitpos(String key, boolean value);
+
+    Long bitpos(String key, boolean value, BitPosParams params);
+
+    default ScanResult<Map.Entry<String, String>> hscan(HashKey key, String cursor) { return hscan(key.getKey(), cursor); }
+
+    default ScanResult<Map.Entry<String, String>> hscan(HashKey key, String cursor, ScanParams params) { return hscan(key.getKey(), cursor, params); }
+
+    ScanResult<String> sscan(String key, String cursor);
+
+    ScanResult<Tuple> zscan(String key, String cursor);
+
+    ScanResult<Tuple> zscan(String key, String cursor, ScanParams params);
+
+    ScanResult<String> sscan(String key, String cursor, ScanParams params);
+
+    Long pfadd(String key, String... elements);
+
+    long pfcount(String key);
+
+    Long geoadd(String key, double longitude, double latitude, String member);
+
+    Long geoadd(String key, Map<String, GeoCoordinate> memberCoordinateMap);
+
+    Double geodist(String key, String member1, String member2);
+
+    Double geodist(String key, String member1, String member2, GeoUnit unit);
+
+    List<String> geohash(String key, String... members);
+
+    List<GeoCoordinate> geopos(String key, String... members);
+
+    List<GeoRadiusResponse> georadius(String key, double longitude, double latitude, double radius, GeoUnit unit);
+
+    List<GeoRadiusResponse> georadius(String key, double longitude, double latitude, double radius, GeoUnit unit, GeoRadiusParam param);
+
+    List<GeoRadiusResponse> georadiusByMember(String key, String member, double radius, GeoUnit unit);
+
+    List<GeoRadiusResponse> georadiusByMember(String key, String member, double radius, GeoUnit unit, GeoRadiusParam param);
+
+    List<Long> bitfield(String key, String... arguments);
+
+    default Long hstrlen(HashKey key, String field) { return hstrlen(key.getKey(), String field); }
+}
---
+++ src/main/java/redis/clients/jedis/keys/TypeSafeKey.java
@@ -0,0 +1,15 @@
+package redis.clients.jedis.keys;
+
+public class TypeSafeKey {
+
+    private final String key;
+
+    public String getKey() {
+        return key;
+    }
+
+    public TypeSafeKey(String key) {
+        this.key = key;
+    }
+
+}

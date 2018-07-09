0d1251101a1a24829485075adcd064d3533bbc48
Guy Korland <gkorland@gmail.com>
2018-07-09 09:19:55 +0300

    replace leave with have

--- src/main/java/redis/clients/jedis/BinaryJedis.java
+++ src/main/java/redis/clients/jedis/BinaryJedis.java
@@ -1289,7 +1289,7 @@ public class BinaryJedis implements BasicCommands, BinaryJedisCommands, MultiKey
    * Remove the first count occurrences of the value element from the list. If count is zero all the
    * elements are removed. If count is negative elements are removed from tail to head, instead to
    * go from head to tail that is the normal behaviour. So for example LREM with count -2 and hello
-   * as value to remove against the list (a,b,c,hello,x,hello,hello) will have the list
+   * as value to remove against the list (a,b,c,hello,x,hello,hello) will leave the list
    * (a,b,c,hello,x). The number of removed elements is returned as an integer, see below for more
    * information about the returned value. Note that non existing keys are considered like empty
    * lists by LREM, so LREM against non existing keys will always return 0.

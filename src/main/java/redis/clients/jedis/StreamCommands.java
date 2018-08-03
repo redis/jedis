7e21a8e1c2b5b7b083cd7bd3f4c59859805b5f34
wangxiao <wangxiao@localhost.com>
2018-08-03 17:25:47 +0800

    添加xread的方法重载定义

--- src/main/java/redis/clients/jedis/StreamCommands.java
+++ src/main/java/redis/clients/jedis/StreamCommands.java
@@ -14,8 +14,8 @@ public interface StreamCommands {
     /**
      * 添加元素到Stream，采用entryId为*
      *
-     * @param key     键名
-     * @param pairs   键值对
+     * @param key   键名
+     * @param pairs 键值对
      * @return 实际存储的序号，如果输入的非“*”序号中不含“-”，Redis会自动添加“-0”并返回修改后的值
      */
     String xaddDefault(String key, String entryId, String... pairs);
@@ -23,8 +23,8 @@ public interface StreamCommands {
     /**
      * 添加元素到Stream，采用entryId为*
      *
-     * @param key     键名
-     * @param pairs   键值对
+     * @param key   键名
+     * @param pairs 键值对
      * @return 实际存储的序号，如果输入的非“*”序号中不含“-”，Redis会自动添加“-0”并返回修改后的值
      */
     String xaddDefault(String key, Map<String, String> pairs);
@@ -144,4 +144,47 @@ public interface StreamCommands {
      * @return 元素集合
      */
     List<StreamParams> xrevrange(String key, String startEntryId, String endEntryId, long count);
+
+    /**
+     * 读取Stream中的数据
+     *
+     * @param params 键名组,起始序号ID组。ID接受“$”，但此时Redis会返回nil
+     * @return 每个Stream为一个从起始序号开始的全部元素，相当于count=0（允许redis返回nil，自动处理为空集），返回的是所有监听Stream的结果集的集合，以键名为key
+     */
+    Map<String, List<StreamParams>> xread(String... params);
+
+    /**
+     * 读取Stream中的数据
+     *
+     * @param pairs 键名-起始序号ID的键值对。ID接受“$”，但此时Redis会返回nil
+     * @return 每个Stream为一个从起始序号开始的全部元素，相当于count=0（允许redis返回nil，自动处理为空集），返回的是所有监听Stream的结果集的集合，以键名为key
+     */
+    Map<String, List<StreamParams>> xread(Map<String, String> pairs);
+
+    /**
+     * 读取Stream中的数据
+     *
+     * @param count  最大取得的元素数量
+     * @param params 键名组,起始序号ID组。ID接受“$”，但此时Redis会返回nil
+     * @return 每个Stream为一个从起始序号开始的全部元素，相当于count=0（允许redis返回nil，自动处理为空集），返回的是所有监听Stream的结果集的集合，以键名为key
+     */
+    Map<String, List<StreamParams>> xread(long count, String... params);
+
+    /**
+     * 读取Stream中的数据
+     *
+     * @param count  最大取得的元素数量
+     * @param pairs 键名-起始序号ID的键值对。ID接受“$”，但此时Redis会返回nil
+     * @return 每个Stream为一个从起始序号开始的全部元素，相当于count=0（允许redis返回nil，自动处理为空集），返回的是所有监听Stream的结果集的集合，以键名为key
+     */
+    Map<String, List<StreamParams>> xread(long count, Map<String, String> pairs);
+
+    /**
+     * 当使用BLOCK参数时，如果entryId不为“$”或大于最大元素的值，block参数不会生效，如果block生效，则只会返回最新的一个元素
+     *
+     * @param keys  键名
+     * @param block 阻塞的时间，单位为毫秒
+     * @return 取得的值, 只会有一个元素，Map的key为第一个取得了元素的Stream的键名。Redis可能返回nil，自动处理为空集合
+     */
+    Map<String, StreamParams> xreadBlock(long block, String... keys);
 }

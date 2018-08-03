7634b4131182988cc10ee2fdb385cb26b6e1d7e8
wangxiao <wangxiao@localhost.com>
2018-08-03 17:39:10 +0800

    添加xdel、xtrim方法定义

--- src/main/java/redis/clients/jedis/StreamCommands.java
+++ src/main/java/redis/clients/jedis/StreamCommands.java
@@ -173,7 +173,7 @@ public interface StreamCommands {
     /**
      * 读取Stream中的数据
      *
-     * @param count  最大取得的元素数量
+     * @param count 最大取得的元素数量
      * @param pairs 键名-起始序号ID的键值对。ID接受“$”，但此时Redis会返回nil
      * @return 每个Stream为一个从起始序号开始的全部元素，相当于count=0（允许redis返回nil，自动处理为空集），返回的是所有监听Stream的结果集的集合，以键名为key
      */
@@ -187,4 +187,32 @@ public interface StreamCommands {
      * @return 取得的值, 只会有一个元素，Map的key为第一个取得了元素的Stream的键名。Redis可能返回nil，自动处理为空集合
      */
     Map<String, StreamParams> xreadBlock(long block, String... keys);
+
+    /**
+     * 删除一个元素
+     *
+     * @param key     键名
+     * @param entryId 序号
+     * @return 删除的元素个数
+     */
+    long xdel(String key, String entryId);
+
+    /**
+     * 按最大容量裁剪Stream，默认不使用“~”参数
+     *
+     * @param key    键名
+     * @param maxlen 最大容量
+     * @return 删除的元素个数
+     */
+    long xtrimWithMaxlen(String key, long maxlen);
+
+    /**
+     * 按最大容量裁剪Stream
+     *
+     * @param key    键名
+     * @param approx 是否使用“~”参数
+     * @param maxlen 最大容量
+     * @return 删除的元素个数
+     */
+    long xtrimWithMaxlen(String key, boolean approx, long maxlen);
 }

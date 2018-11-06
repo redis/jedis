392c969208a92591adf7a495ade6422adc172b65
M Sazzadul Hoque <sazzad16@users.noreply.github.com>
2018-11-06 14:20:51 +0200

    Update src/main/java/redis/clients/jedis/commands/JedisClusterCommands.java
    Co-Authored-By: gkorland <gkorland@gmail.com>

--- src/main/java/redis/clients/jedis/commands/JedisClusterCommands.java
+++ src/main/java/redis/clients/jedis/commands/JedisClusterCommands.java
@@ -315,6 +315,5 @@ public interface JedisClusterCommands {
    * @return lenth of the value for key
    */
   Long hstrlen(String key, String field);
-  
   Long waitReplicas(final String key, final int replicas, final long timeout);
 }

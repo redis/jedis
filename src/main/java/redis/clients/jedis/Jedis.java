df1bffa3c77f4ede4c912f2c3e78b5c8857725e7
Guy Korland <gkorland@gmail.com>
2018-12-17 21:49:44 +0200

    Move dataSource reset before connection returned
    Move dataSource reset before connection returned to pool.
    If datasource reset after the connection returned to pool, it might reset the datasource after it was allocated to another thread

--- src/main/java/redis/clients/jedis/Jedis.java
+++ src/main/java/redis/clients/jedis/Jedis.java
@@ -3452,12 +3452,13 @@ public class Jedis extends BinaryJedis implements JedisCommands, MultiKeyCommand
   @Override
   public void close() {
     if (dataSource != null) {
-      if (client.isBroken()) {
-        this.dataSource.returnBrokenResource(this);
-      } else {
-        this.dataSource.returnResource(this);
-      }
+      JedisPoolAbstract pool = this.dataSource;
       this.dataSource = null;
+      if (client.isBroken()) {
+        pool.returnBrokenResource(this);
+      } else {
+        pool.returnResource(this);
+      }
     } else {
       super.close();
     }

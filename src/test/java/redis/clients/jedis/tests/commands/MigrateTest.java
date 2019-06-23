a514bfe5fbe0215740f85882e5eb33a815d5a6b7
sazzad16 <sazzad.cse.sust@gmail.com>
2019-06-23 19:28:21 +0600

    fix migrateCopyReplaceAuth

--- src/test/java/redis/clients/jedis/tests/commands/MigrateTest.java
+++ src/test/java/redis/clients/jedis/tests/commands/MigrateTest.java
@@ -151,15 +151,15 @@ public class MigrateTest extends JedisCommandTestBase {
   @Test
   public void migrateCopyReplaceAuth() {
     jedis.set("foo", "bar1");
-    dest.set("foo", "bar2");
+    destAuth.set("foo", "bar2");
     assertEquals("OK", jedis.migrate(host, portAuth, dbAuth, timeout, new MigrateParams().copy().replace().auth("foobared"), "foo"));
-    assertEquals("bar1", dest.get("foo"));
+    assertEquals("bar1", destAuth.get("foo"));
     assertEquals("bar1", jedis.get("foo"));
 
     jedis.set(bfoo, bbar1);
-    dest.set(bfoo, bbar2);
+    destAuth.set(bfoo, bbar2);
     assertEquals("OK", jedis.migrate(host, portAuth, dbAuth, timeout, new MigrateParams().copy().replace().auth("foobared"), bfoo));
-    assertArrayEquals(bbar1, dest.get(bfoo));
+    assertArrayEquals(bbar1, destAuth.get(bfoo));
     assertArrayEquals(bbar1, jedis.get(bfoo));
   }
 

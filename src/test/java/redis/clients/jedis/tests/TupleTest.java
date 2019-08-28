e9ff11454bae6256dcfbcbaa47bdd143a77e0cc5
Gabriel Dutra <gdutra@quotemedia.com>
2019-08-28 15:42:16 -0700

    refactoring TupleTests

--- src/test/java/redis/clients/jedis/tests/TupleTest.java
+++ src/test/java/redis/clients/jedis/tests/TupleTest.java
@@ -1,73 +1,73 @@
 package redis.clients.jedis.tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import redis.clients.jedis.Tuple;
 
 import java.util.HashSet;
-import org.junit.Test;
+import java.util.Set;
 
-import redis.clients.jedis.Tuple;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class TupleTest {
 
-  @Test
-  public void compareEqual() {
-    Tuple t1 = new Tuple("foo", 1d);
-    Tuple t2 = new Tuple("foo", 1d);
+    @Test
+    public void compareEqual() {
+        Tuple t1 = new Tuple("foo", 1d);
+        Tuple t2 = new Tuple("foo", 1d);
 
-    assertEquals(0, t1.compareTo(t2));
-    assertEquals(0, t2.compareTo(t1));
-    assertTrue(t1.equals(t2));
-    assertTrue(t2.equals(t1));
-  }
+        assertEquals(0, t1.compareTo(t2));
+        assertEquals(0, t2.compareTo(t1));
+        assertEquals(t1, t2);
+        assertEquals(t2, t1);
+    }
 
-  @Test
-  public void compareSameScore() {
-    Tuple t1 = new Tuple("foo", 1d);
-    Tuple t2 = new Tuple("bar", 1d);
+    @Test
+    public void compareSameScore() {
+        Tuple t1 = new Tuple("foo", 1d);
+        Tuple t2 = new Tuple("bar", 1d);
 
-    assertEquals(1, t1.compareTo(t2));
-    assertEquals(-1, t2.compareTo(t1));
-    assertFalse(t1.equals(t2));
-    assertFalse(t2.equals(t1));
-  }
+        assertEquals(1, t1.compareTo(t2));
+        assertEquals(-1, t2.compareTo(t1));
+        assertNotEquals(t1, t2);
+        assertNotEquals(t2, t1);
+    }
 
-  @Test
-  public void compareSameScoreObject() {
-    Double score = 1d;
-    Tuple t1 = new Tuple("foo", score);
-    Tuple t2 = new Tuple("bar", score);
+    @Test
+    public void compareSameScoreObject() {
+        Double score = 1d;
+        Tuple t1 = new Tuple("foo", score);
+        Tuple t2 = new Tuple("bar", score);
 
-    assertEquals(1, t1.compareTo(t2));
-    assertEquals(-1, t2.compareTo(t1));
-    assertFalse(t1.equals(t2));
-    assertFalse(t2.equals(t1));
-  }
+        assertEquals(1, t1.compareTo(t2));
+        assertEquals(-1, t2.compareTo(t1));
+        assertNotEquals(t1, t2);
+        assertNotEquals(t2, t1);
+    }
 
-  @Test
-  public void compareNoMatch() {
-    Tuple t1 = new Tuple("foo", 1d);
-    Tuple t2 = new Tuple("bar", 2d);
+    @Test
+    public void compareNoMatch() {
+        Tuple t1 = new Tuple("foo", 1d);
+        Tuple t2 = new Tuple("bar", 2d);
 
-    assertEquals(-1, t1.compareTo(t2));
-    assertEquals(1, t2.compareTo(t1));
-    assertFalse(t1.equals(t2));
-    assertFalse(t2.equals(t1));
-  }
+        assertEquals(-1, t1.compareTo(t2));
+        assertEquals(1, t2.compareTo(t1));
+        assertNotEquals(t1, t2);
+        assertNotEquals(t2, t1);
+    }
 
-  @Test
-  public void testSameElement() {
-    Tuple t1 = new Tuple("user1", 10.0);
-    Tuple t2 = new Tuple("user1", 5.0);
+    @Test
+    public void testSameElement() {
+        Tuple t1 = new Tuple("user1", 10.0);
+        Tuple t2 = new Tuple("user1", 5.0);
 
-    // Intentionally skipping compareTo.
-    assertFalse(t1.equals(t2));
-    assertFalse(t2.equals(t1));
+        // Intentionally skipping compareTo.
+        assertNotEquals(t1, t2);
+        assertNotEquals(t2, t1);
 
-    HashSet<Tuple> hashSet = new HashSet<Tuple>();
-    hashSet.add(t1);
-    hashSet.add(t2);
-    assertEquals(2, hashSet.size());
-  }
+        Set<Tuple> hashSet = new HashSet<>();
+        hashSet.add(t1);
+        hashSet.add(t2);
+        assertEquals(2, hashSet.size());
+    }
 }

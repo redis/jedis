7f6bfc733ae4d47ddd682bb9c6c68de87a3d6953
M Sazzadul Hoque <sazzad16@users.noreply.github.com>
2019-12-09 18:01:18 +0600

    PipelineBase should not contain multi key operations (#2079)

--- src/main/java/redis/clients/jedis/PipelineBase.java
+++ src/main/java/redis/clients/jedis/PipelineBase.java
@@ -1975,15 +1975,13 @@ public abstract class PipelineBase extends Queable implements BinaryRedisPipelin
     return getResponse(BuilderFactory.BYTE_ARRAY_LIST);            
   }
 
-  public Response<Object> sendCommand(ProtocolCommand cmd, String... args){
-    String key = args.length > 0 ? args[0] : cmd.toString();
-    getClient(key).sendCommand(cmd, args);
+  public Response<Object> sendCommand(final String sampleKey, final ProtocolCommand cmd, final String... args) {
+    getClient(sampleKey).sendCommand(cmd, args);
     return getResponse(BuilderFactory.OBJECT);
   }
 
-  public Response<Object> sendCommand(ProtocolCommand cmd, byte[]... args){
-    byte[] key = args.length > 0 ? args[0] : cmd.getRaw();
-    getClient(key).sendCommand(cmd, args);
+  public Response<Object> sendCommand(final byte[] sampleKey, final ProtocolCommand cmd, final byte[]... args) {
+    getClient(sampleKey).sendCommand(cmd, args);
     return getResponse(BuilderFactory.OBJECT);
   }
 }

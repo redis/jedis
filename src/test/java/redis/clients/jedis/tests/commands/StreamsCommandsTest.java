a28c7ce7b42741d102cbc7e9d533eb0f058638b2
Guy Korland <gkorland@gmail.com>
2020-03-14 22:35:41 +0200

    Update StreamsCommandsTest.java

--- src/test/java/redis/clients/jedis/tests/commands/StreamsCommandsTest.java
+++ src/test/java/redis/clients/jedis/tests/commands/StreamsCommandsTest.java
@@ -318,7 +318,11 @@ public class StreamsCommandsTest extends JedisCommandTestBase {
       e.printStackTrace();
     }
     
-    jedis.xclaim("xpendeing-stream", "xpendeing-group", "xpendeing-consumer2", 500, 0, 0, false, pendingRange.get(0).getID());
+    List<StreamEntry> claimRange = jedis.xclaim("xpendeing-stream", "xpendeing-group", "xpendeing-consumer2", 500, 0, 0, false, pendingRange.get(0).getID());
+    assertEquals(1, pendingRange.size());
+    
+    Long pendingMessageNum = jedis.xgroupDelConsumer("xpendeing-stream", "xpendeing-group", "xpendeing-consumer2");
+    assertEquals(1L, pendingMessageNum.longValue()); 
   }
 
   @Test

d7aba13a8b65e66dedc01c51b73e3794cbe68a62
Tugdual Grall <tugdual@gmail.com>
2020-08-11 15:53:44 +0200

    add SafeEncoder.encode() method for complete encoding  (#2214)
    * add SafeEncoder.encode() generic method for complete encoding of response -- fix #2213

--- src/test/java/redis/clients/jedis/tests/commands/AllKindOfValuesCommandsTest.java
+++ src/test/java/redis/clients/jedis/tests/commands/AllKindOfValuesCommandsTest.java
@@ -7,11 +7,13 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
+import static redis.clients.jedis.Protocol.Command.HGETALL;
 import static redis.clients.jedis.Protocol.Command.GET;
 import static redis.clients.jedis.Protocol.Command.LRANGE;
 import static redis.clients.jedis.Protocol.Command.PING;
 import static redis.clients.jedis.Protocol.Command.RPUSH;
 import static redis.clients.jedis.Protocol.Command.SET;
+import static redis.clients.jedis.Protocol.Command.XINFO;
 import static redis.clients.jedis.ScanParams.SCAN_POINTER_START;
 import static redis.clients.jedis.ScanParams.SCAN_POINTER_START_BINARY;
 import static redis.clients.jedis.params.SetParams.setParams;
@@ -31,6 +33,7 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Protocol.Keyword;
 import redis.clients.jedis.ScanParams;
 import redis.clients.jedis.ScanResult;
+import redis.clients.jedis.StreamEntryID;
 import redis.clients.jedis.util.SafeEncoder;
 import redis.clients.jedis.exceptions.JedisDataException;
 
@@ -866,6 +869,39 @@ public class AllKindOfValuesCommandsTest extends JedisCommandTestBase {
       assertArrayEquals(expected.get(i), list.get(i));
 
     assertEquals("PONG", SafeEncoder.encode((byte[]) jedis.sendCommand(PING)));
+  }
+
+
+  @Test
+  public void encodeCompleteResponse(){
+    HashMap<String,String> entry = new HashMap<>();
+    entry.put("foo", "bar");
+    jedis.xadd( "mystream", StreamEntryID.NEW_ENTRY, entry );
+    String status = jedis.xgroupCreate("mystream", "mygroup", null, false);
+
+    Object obj = jedis.sendCommand(XINFO, "STREAM", "mystream");
+    List encodeObj =  (List)SafeEncoder.encodeObject(obj);
+
+    assertEquals( 14, encodeObj.size() );
+    assertEquals( "length", encodeObj.get(0) );
+    assertEquals( 1L, encodeObj.get(1) );
+
+    List entryAsList = new ArrayList(2);
+    entryAsList.add("foo");
+    entryAsList.add("bar");
+
+    assertEquals( entryAsList, ((List)encodeObj.get(11)).get(1) );
+
+    assertEquals("PONG", SafeEncoder.encodeObject(jedis.sendCommand(PING)));
+
+    entry.put("foo2", "bar2");
+    jedis.hset("hash:test:encode", entry);
+    encodeObj =   (List)SafeEncoder.encodeObject(jedis.sendCommand(HGETALL, "hash:test:encode"));
+
+    assertEquals( 4, encodeObj.size() );
+    assertTrue(encodeObj.contains("foo"));
+    assertTrue(encodeObj.contains("foo2"));
+
   }
 
 }

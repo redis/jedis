e7b50df3c1eeab053ce9cac6644cde198013d009
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2021-01-29 20:08:35 +0600

    refactor/format

--- src/main/java/redis/clients/jedis/Connection.java
+++ src/main/java/redis/clients/jedis/Connection.java
@@ -6,8 +6,6 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLParameters;

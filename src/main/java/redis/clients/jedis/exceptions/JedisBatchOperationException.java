a83d64dd918b41be3ad52dabf725fe68fc83b39e
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2021-02-11 00:19:26 +0600

    address review

--- src/main/java/redis/clients/jedis/exceptions/JedisBatchOperationException.java
+++ src/main/java/redis/clients/jedis/exceptions/JedisBatchOperationException.java
@@ -4,6 +4,9 @@ package redis.clients.jedis.exceptions;
  * @deprecated This class is extending {@link JedisDataException} to maintain backward
  * compatibility. But this will not do so in future. Instead, This would extend
  * {@link JedisException} directly.
+ *
+ * If you are catching this exception, you should either catch {@link JedisBatchOperationException}
+ * or {@link JedisException}. You should not catch {@link JedisDataException}.
  */
 @Deprecated
 public class JedisBatchOperationException extends JedisDataException {

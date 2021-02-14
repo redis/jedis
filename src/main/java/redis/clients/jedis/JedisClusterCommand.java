c665dc1b4e6f9770e20997e9efa761ccba790403
Johan Walles <walles@spotify.com>
2021-02-14 18:19:57 +0100

    Improve a comment

--- src/main/java/redis/clients/jedis/JedisClusterCommand.java
+++ src/main/java/redis/clients/jedis/JedisClusterCommand.java
@@ -164,7 +164,12 @@ public abstract class JedisClusterCommand<T> {
    */
   private boolean handleConnectionProblem(int attemptsLeft, int consecutiveConnectionFailures, Instant doneDeadline) {
     if (this.maxAttempts < 3) {
-      // keep the old implementations as is?
+      // Since we only renew the slots cache after two consecutive connection
+      // failures (see consecutiveConnectionFailures above), we need to special
+      // case the situation where we max out after two or fewer attempts.
+      //
+      // Otherwise, on two or fewer max attempts, the slots cache would never be
+      // renewed.
       if (attemptsLeft == 0) {
         this.connectionHandler.renewSlotCache();
         return true;

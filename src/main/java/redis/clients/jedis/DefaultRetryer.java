9a77cfefbaf21217c4619a053dec1ed08f5a7f5b
Johan Walles <walles@spotify.com>
2021-02-18 06:01:39 +0100

    Initial attempt at pluggable retriers

--- src/main/java/redis/clients/jedis/DefaultRetryer.java
+++ src/main/java/redis/clients/jedis/DefaultRetryer.java
@@ -1,85 +1,29 @@
 package redis.clients.jedis;
 
+import java.util.function.Function;
 import redis.clients.jedis.exceptions.JedisAskDataException;
 import redis.clients.jedis.exceptions.JedisClusterMaxAttemptsException;
-import redis.clients.jedis.exceptions.JedisClusterOperationException;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 import redis.clients.jedis.exceptions.JedisMovedDataException;
 import redis.clients.jedis.exceptions.JedisNoReachableClusterNodeException;
 import redis.clients.jedis.exceptions.JedisRedirectionException;
-import redis.clients.jedis.util.JedisClusterCRC16;
 
-public abstract class JedisClusterCommand<T> {
+public class DefaultRetryer extends Retryer {
 
   private final JedisClusterConnectionHandler connectionHandler;
   private final int maxAttempts;
 
-  public JedisClusterCommand(JedisClusterConnectionHandler connectionHandler, int maxAttempts) {
+  public DefaultRetryer(JedisClusterConnectionHandler connectionHandler, int maxAttempts) {
     this.connectionHandler = connectionHandler;
     this.maxAttempts = maxAttempts;
   }
 
-  public abstract T execute(Jedis connection);
-
-  public T run(String key) {
-    return runWithRetries(JedisClusterCRC16.getSlot(key), this.maxAttempts, false, null);
+  @Override
+  protected <R> R runWithRetries(int slot, Function<Jedis, R> command) {
+    return runWithRetries(command, slot, maxAttempts, false, null);
   }
 
-  public T run(int keyCount, String... keys) {
-    if (keys == null || keys.length == 0) {
-      throw new JedisClusterOperationException("No way to dispatch this command to Redis Cluster.");
-    }
-
-    // For multiple keys, only execute if they all share the same connection slot.
-    int slot = JedisClusterCRC16.getSlot(keys[0]);
-    if (keys.length > 1) {
-      for (int i = 1; i < keyCount; i++) {
-        int nextSlot = JedisClusterCRC16.getSlot(keys[i]);
-        if (slot != nextSlot) {
-          throw new JedisClusterOperationException("No way to dispatch this command to Redis "
-              + "Cluster because keys have different slots.");
-        }
-      }
-    }
-
-    return runWithRetries(slot, this.maxAttempts, false, null);
-  }
-
-  public T runBinary(byte[] key) {
-    return runWithRetries(JedisClusterCRC16.getSlot(key), this.maxAttempts, false, null);
-  }
-
-  public T runBinary(int keyCount, byte[]... keys) {
-    if (keys == null || keys.length == 0) {
-      throw new JedisClusterOperationException("No way to dispatch this command to Redis Cluster.");
-    }
-
-    // For multiple keys, only execute if they all share the same connection slot.
-    int slot = JedisClusterCRC16.getSlot(keys[0]);
-    if (keys.length > 1) {
-      for (int i = 1; i < keyCount; i++) {
-        int nextSlot = JedisClusterCRC16.getSlot(keys[i]);
-        if (slot != nextSlot) {
-          throw new JedisClusterOperationException("No way to dispatch this command to Redis "
-              + "Cluster because keys have different slots.");
-        }
-      }
-    }
-
-    return runWithRetries(slot, this.maxAttempts, false, null);
-  }
-
-  public T runWithAnyNode() {
-    Jedis connection = null;
-    try {
-      connection = connectionHandler.getConnection();
-      return execute(connection);
-    } finally {
-      releaseConnection(connection);
-    }
-  }
-
-  private T runWithRetries(final int slot, int attempts, boolean tryRandomNode, JedisRedirectionException redirect) {
+  private <R> R runWithRetries(Function<Jedis, R> command, final int slot, int attempts, boolean tryRandomNode, JedisRedirectionException redirect) {
     if (attempts <= 0) {
       throw new JedisClusterMaxAttemptsException("No more cluster attempts left.");
     }
@@ -101,7 +45,7 @@ public abstract class JedisClusterCommand<T> {
         }
       }
 
-      return execute(connection);
+      return command.apply(connection);
 
     } catch (JedisNoReachableClusterNodeException jnrcne) {
       throw jnrcne;
@@ -119,7 +63,7 @@ public abstract class JedisClusterCommand<T> {
         this.connectionHandler.renewSlotCache();
       }
 
-      return runWithRetries(slot, attempts - 1, tryRandomNode, redirect);
+      return runWithRetries(command, slot, attempts - 1, tryRandomNode, redirect);
     } catch (JedisRedirectionException jre) {
       // if MOVED redirection occurred,
       if (jre instanceof JedisMovedDataException) {
@@ -131,7 +75,7 @@ public abstract class JedisClusterCommand<T> {
       releaseConnection(connection);
       connection = null;
 
-      return runWithRetries(slot, attempts - 1, false, jre);
+      return runWithRetries(command, slot, attempts - 1, false, jre);
     } finally {
       releaseConnection(connection);
     }
--- src/main/java/redis/clients/jedis/Retryer.java
+++ src/main/java/redis/clients/jedis/Retryer.java
@@ -1,13 +1,63 @@
 package redis.clients.jedis;
 
 import java.util.function.Function;
+import redis.clients.jedis.exceptions.JedisClusterOperationException;
+import redis.clients.jedis.util.JedisClusterCRC16;
 
-public interface Retryer {
+public abstract class Retryer {
   /**
    * Execute a Redis command with retries.
    *
    * @param slot From one of the {@code JedisClusterCRC16#get*())} methods
    * @return The result of running the given command with retries
    */
-  <R> R runWithRetries(int slot, Function<Jedis, R> command);
+  protected abstract <R> R runWithRetries(int slot, Function<Jedis, R> command);
+
+  public <R> R run(Function<Jedis, R> command, String key) {
+    return runWithRetries(JedisClusterCRC16.getSlot(key), command);
+  }
+
+  public <R> R run(Function<Jedis, R> command, int keyCount, String... keys) {
+    if (keys == null || keys.length == 0) {
+      throw new JedisClusterOperationException("No way to dispatch this command to Redis Cluster.");
+    }
+
+    // For multiple keys, only execute if they all share the same connection slot.
+    int slot = JedisClusterCRC16.getSlot(keys[0]);
+    if (keys.length > 1) {
+      for (int i = 1; i < keyCount; i++) {
+        int nextSlot = JedisClusterCRC16.getSlot(keys[i]);
+        if (slot != nextSlot) {
+          throw new JedisClusterOperationException("No way to dispatch this command to Redis "
+              + "Cluster because keys have different slots.");
+        }
+      }
+    }
+
+    return runWithRetries(slot, command);
+  }
+
+  public <R> R runBinary(Function<Jedis, R> command, byte[] key) {
+    return runWithRetries(JedisClusterCRC16.getSlot(key), command);
+  }
+
+  public <R> R runBinary(Function<Jedis, R> command, int keyCount, byte[]... keys) {
+    if (keys == null || keys.length == 0) {
+      throw new JedisClusterOperationException("No way to dispatch this command to Redis Cluster.");
+    }
+
+    // For multiple keys, only execute if they all share the same connection slot.
+    int slot = JedisClusterCRC16.getSlot(keys[0]);
+    if (keys.length > 1) {
+      for (int i = 1; i < keyCount; i++) {
+        int nextSlot = JedisClusterCRC16.getSlot(keys[i]);
+        if (slot != nextSlot) {
+          throw new JedisClusterOperationException("No way to dispatch this command to Redis "
+              + "Cluster because keys have different slots.");
+        }
+      }
+    }
+
+    return runWithRetries(slot, command);
+  }
 }

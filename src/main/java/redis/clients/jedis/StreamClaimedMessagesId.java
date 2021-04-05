838c98767cd430cee3319765207ef86e8835efcd
Sathish <sathish1293@gmail.com>
2021-04-05 14:14:11 +0530

    Indentation fix in the newly created file in previous commit.

--- src/main/java/redis/clients/jedis/StreamClaimedMessagesId.java
+++ src/main/java/redis/clients/jedis/StreamClaimedMessagesId.java
@@ -5,22 +5,22 @@ import java.util.List;
 
 public class StreamClaimedMessagesId implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private final StreamEntryID id;
-    private final List<StreamEntryID> streamEntryIDs;
+  private final StreamEntryID id;
+  private final List<StreamEntryID> streamEntryIDs;
 
-    public StreamClaimedMessagesId(StreamEntryID id, List<StreamEntryID> streamEntryIDs) {
-        this.id = id;
-        this.streamEntryIDs = streamEntryIDs;
-    }
+  public StreamClaimedMessagesId(StreamEntryID id, List<StreamEntryID> streamEntryIDs) {
+    this.id = id;
+    this.streamEntryIDs = streamEntryIDs;
+  }
 
-    public StreamEntryID getId() {
-        return id;
-    }
+  public StreamEntryID getId() {
+    return id;
+  }
 
-    public List<StreamEntryID> getStreamEntryIDs() {
-        return streamEntryIDs;
-    }
+  public List<StreamEntryID> getStreamEntryIDs() {
+    return streamEntryIDs;
+  }
 
 }

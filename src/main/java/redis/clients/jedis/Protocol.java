1efddafb07259d15933e07be687621d3827ce2f6
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2021-07-14 22:08:22 +0600

    Few more Sentinel commands

--- src/main/java/redis/clients/jedis/Protocol.java
+++ src/main/java/redis/clients/jedis/Protocol.java
@@ -299,4 +299,19 @@ public final class Protocol {
       return raw;
     }
   }
+
+  public static enum SentinelKeyword implements Rawable {
+    MYID, MASTERS, MASTER, SENTINELS, SLAVES, REPLICAS;
+
+    private final byte[] raw;
+
+    private SentinelKeyword() {
+      raw = SafeEncoder.encode(name());
+    }
+
+    @Override
+    public byte[] getRaw() {
+      return raw;
+    }
+  }
 }

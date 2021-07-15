f3319dc377e8bb45fd448f9d869dcd551b2da154
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2021-07-15 17:55:35 +0600

    Test Busy exception

--- src/test/java/redis/clients/jedis/tests/ExceptionsTest.java
+++ src/test/java/redis/clients/jedis/tests/ExceptionsTest.java
@@ -107,6 +107,33 @@ public class ExceptionsTest {
   @Test
   public void busy() {
     try {
+      throw new JedisBusyException(MESSAGE);
+    } catch (Exception e) {
+      assertEquals(JedisBusyException.class, e.getClass());
+      assertEquals(MESSAGE, e.getMessage());
+      assertNull(e.getCause());
+    }
+
+    try {
+      throw new JedisBusyException(CAUSE);
+    } catch (Exception e) {
+      assertEquals(JedisBusyException.class, e.getClass());
+      assertEquals(CAUSE, e.getCause());
+      assertEquals(CAUSE.toString(), e.getMessage());
+    }
+
+    try {
+      throw new JedisBusyException(MESSAGE, CAUSE);
+    } catch (Exception e) {
+      assertEquals(JedisBusyException.class, e.getClass());
+      assertEquals(MESSAGE, e.getMessage());
+      assertEquals(CAUSE, e.getCause());
+    }
+  }
+
+  @Test
+  public void cluster() {
+    try {
       throw new JedisClusterException(MESSAGE);
     } catch (Exception e) {
       assertEquals(JedisClusterException.class, e.getClass());

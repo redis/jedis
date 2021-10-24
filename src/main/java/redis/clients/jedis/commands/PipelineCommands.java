8b437c113ab48f6f269928c7dab86a0c8a98f5b1
AvitalFineRedis <avital.fine@redislabs.com>
2021-10-24 19:45:07 +0200

    Hash Commands

--- src/main/java/redis/clients/jedis/commands/PipelineCommands.java
+++ src/main/java/redis/clients/jedis/commands/PipelineCommands.java
@@ -2,6 +2,7 @@ package redis.clients.jedis.commands;
 
 import redis.clients.jedis.Response;
 
-public interface PipelineCommands extends PipelineKeyCommands, PipelineStringCommands, PipelineListCommands {
+public interface PipelineCommands extends PipelineKeyCommands, PipelineStringCommands, PipelineListCommands,
+PipelineHashCommands{
 
   }

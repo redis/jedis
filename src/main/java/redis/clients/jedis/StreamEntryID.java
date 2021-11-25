e54295fe3e457ac5b73d05dbc3866c19dc52f48f
suoyi123wang <1512721306@qq.com>
2021-11-25 17:02:55 +0800

    deal possible StringIndexOutOfBoundsException and NumberFormatException

--- src/main/java/redis/clients/jedis/StreamEntryID.java
+++ src/main/java/redis/clients/jedis/StreamEntryID.java
@@ -65,9 +65,15 @@ public class StreamEntryID implements Comparable<StreamEntryID>, Serializable {
   }
 
   public StreamEntryID(String id) {
-    String[] split = id.split("-");
-    this.time = Long.parseLong(split[0]);
-    this.sequence = Long.parseLong(split[1]);
+    if (id.contains("-")) {
+      String[] split = id.split("-");
+      try {
+        this.time = Long.parseLong(split[0]);
+        this.sequence = Long.parseLong(split[1]);
+      } catch (NumberFormatException e){
+        // pass
+      }
+    }
   }
 
   public StreamEntryID(long time, long sequence) {

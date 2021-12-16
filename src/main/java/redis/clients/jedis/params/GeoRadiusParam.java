ba6a4fe19dadb109d4ded2e069f7e9b2ae5079a7
AvitalFineRedis <avital.fine@redislabs.com>
2021-12-16 11:05:17 +0100

    JedisClusterPipelineTest

--- src/main/java/redis/clients/jedis/params/GeoRadiusParam.java
+++ src/main/java/redis/clients/jedis/params/GeoRadiusParam.java
@@ -1,13 +1,8 @@
 package redis.clients.jedis.params;
 
-import static redis.clients.jedis.Protocol.Keyword.ASC;
-import static redis.clients.jedis.Protocol.Keyword.COUNT;
-import static redis.clients.jedis.Protocol.Keyword.DESC;
-import static redis.clients.jedis.Protocol.Keyword.WITHCOORD;
-import static redis.clients.jedis.Protocol.Keyword.WITHDIST;
-import static redis.clients.jedis.Protocol.Keyword.WITHHASH;
-
 import redis.clients.jedis.CommandArguments;
+
+import static redis.clients.jedis.Protocol.Keyword.*;
 
 public class GeoRadiusParam implements IParams {
 
@@ -16,6 +11,7 @@ public class GeoRadiusParam implements IParams {
   private boolean withHash = false;
 
   private Integer count = null;
+  private boolean any = false;
   private boolean asc = false;
   private boolean desc = false;
 
@@ -58,6 +54,17 @@ public class GeoRadiusParam implements IParams {
     return this;
   }
 
+  public GeoRadiusParam count(int count, boolean any) {
+    if (count > 0) {
+      this.count = count;
+
+      if (any) {
+        this.any = true;
+      }
+    }
+    return this;
+  }
+
   @Override
   public void addParams(CommandArguments args) {
 
@@ -73,6 +80,9 @@ public class GeoRadiusParam implements IParams {
 
     if (count != null) {
       args.add(COUNT).add(count);
+      if (any) {
+        args.add(ANY);
+      }
     }
 
     if (asc) {

c404a8526c412e80d4dabf6cf8200ae48c288036
ZEEKLING <lingzhaohui@zeekling.cn>
2021-12-19 14:28:20 +0800

    Replace deprecated interface with new interface of JedisPoolConfig (#2749)
    * Replace old interface with new interface of JedisPoolConfig
    
    * Replace old interface with new interface of ConnectionPoolConfig
    
    * reorder import
    
    * reorder import
    
    Co-authored-by: M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>

--- src/main/java/redis/clients/jedis/ConnectionPoolConfig.java
+++ src/main/java/redis/clients/jedis/ConnectionPoolConfig.java
@@ -1,5 +1,6 @@
 package redis.clients.jedis;
 
+import java.time.Duration;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 public class ConnectionPoolConfig extends GenericObjectPoolConfig<Connection> {
@@ -7,8 +8,8 @@ public class ConnectionPoolConfig extends GenericObjectPoolConfig<Connection> {
   public ConnectionPoolConfig() {
     // defaults to make your life with connection pool easier :)
     setTestWhileIdle(true);
-    setMinEvictableIdleTimeMillis(60000);
-    setTimeBetweenEvictionRunsMillis(30000);
+    setMinEvictableIdleTime(Duration.ofMillis(60000));
+    setTimeBetweenEvictionRuns(Duration.ofMillis(30000));
     setNumTestsPerEvictionRun(-1);
   }
 }

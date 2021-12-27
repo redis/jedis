81d9e9e2e049f56fbc312fb06f15d839b6d311b9
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2021-12-27 17:40:18 +0600

    Format

--- src/main/java/redis/clients/jedis/CommandObjects.java
+++ src/main/java/redis/clients/jedis/CommandObjects.java
@@ -1849,67 +1849,68 @@ public class CommandObjects {
   }
 
   public final CommandObject<List<GeoRadiusResponse>> geosearch(String key, String member,
-      double radius, GeoUnit unit){
+      double radius, GeoUnit unit) {
     return new CommandObject<>(commandArguments(GEOSEARCH).key(key).add(FROMMEMBER).add(member)
         .add(BYRADIUS).add(radius).add(unit), BuilderFactory.GEORADIUS_WITH_PARAMS_RESULT);
   }
 
   public final CommandObject<List<GeoRadiusResponse>> geosearch(String key, GeoCoordinate coord,
-      double radius, GeoUnit unit){
-    return new CommandObject<>(commandArguments(GEOSEARCH).key(key).add(FROMLONLAT).add(coord.getLongitude())
-        .add(coord.getLatitude()).add(BYRADIUS).add(radius).add(unit), BuilderFactory.GEORADIUS_WITH_PARAMS_RESULT);
+      double radius, GeoUnit unit) {
+    return new CommandObject<>(commandArguments(GEOSEARCH).key(key)
+        .add(FROMLONLAT).add(coord.getLongitude()).add(coord.getLatitude())
+        .add(BYRADIUS).add(radius).add(unit), BuilderFactory.GEORADIUS_WITH_PARAMS_RESULT);
   }
 
   public final CommandObject<List<GeoRadiusResponse>> geosearch(String key, String member,
-      double width, double height, GeoUnit unit){
+      double width, double height, GeoUnit unit) {
     return new CommandObject<>(commandArguments(GEOSEARCH).key(key).add(FROMMEMBER).add(member)
         .add(BYBOX).add(width).add(height).add(unit), BuilderFactory.GEORADIUS_WITH_PARAMS_RESULT);
   }
 
   public final CommandObject<List<GeoRadiusResponse>> geosearch(String key, GeoCoordinate coord,
-      double width, double height, GeoUnit unit){
-    return new CommandObject<>(commandArguments(GEOSEARCH).key(key).add(FROMLONLAT).add(coord.getLongitude())
-            .add(coord.getLatitude()).add(BYBOX).add(width).add(height).add(unit),
-            BuilderFactory.GEORADIUS_WITH_PARAMS_RESULT);
+      double width, double height, GeoUnit unit) {
+    return new CommandObject<>(commandArguments(GEOSEARCH).key(key)
+        .add(FROMLONLAT).add(coord.getLongitude()).add(coord.getLatitude())
+        .add(BYBOX).add(width).add(height).add(unit), BuilderFactory.GEORADIUS_WITH_PARAMS_RESULT);
   }
 
-  public final CommandObject<List<GeoRadiusResponse>> geosearch(String key, GeoSearchParam params){
+  public final CommandObject<List<GeoRadiusResponse>> geosearch(String key, GeoSearchParam params) {
     return new CommandObject<>(commandArguments(GEOSEARCH).key(key).addParams(params),
-            BuilderFactory.GEORADIUS_WITH_PARAMS_RESULT);
+        BuilderFactory.GEORADIUS_WITH_PARAMS_RESULT);
   }
 
   public final CommandObject<Long> geosearchStore(String dest, String src, String member,
-      double radius, GeoUnit unit){
-    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src).add(FROMMEMBER).add(member)
-            .add(BYRADIUS).add(radius).add(unit), BuilderFactory.LONG);
+      double radius, GeoUnit unit) {
+    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src)
+        .add(FROMMEMBER).add(member).add(BYRADIUS).add(radius).add(unit), BuilderFactory.LONG);
   }
 
   public final CommandObject<Long> geosearchStore(String dest, String src, GeoCoordinate coord,
-      double radius, GeoUnit unit){
-    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src).add(FROMLONLAT).add(coord.getLongitude())
-            .add(coord.getLatitude()).add(BYRADIUS).add(radius).add(unit), BuilderFactory.LONG);
+      double radius, GeoUnit unit) {
+    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src)
+        .add(FROMLONLAT).add(coord.getLongitude()).add(coord.getLatitude())
+        .add(BYRADIUS).add(radius).add(unit), BuilderFactory.LONG);
   }
 
   public final CommandObject<Long> geosearchStore(String dest, String src, String member,
       double width, double height, GeoUnit unit) {
-    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src).add(FROMMEMBER).add(member)
-            .add(BYBOX).add(width).add(height).add(unit), BuilderFactory.LONG);
+    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src)
+        .add(FROMMEMBER).add(member).add(BYBOX).add(width).add(height).add(unit), BuilderFactory.LONG);
   }
 
   public final CommandObject<Long> geosearchStore(String dest, String src, GeoCoordinate coord,
-      double width, double height, GeoUnit unit){
-    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src).add(FROMLONLAT).add(coord.getLongitude())
-            .add(coord.getLatitude()).add(BYBOX).add(width).add(height).add(unit), BuilderFactory.LONG);
+      double width, double height, GeoUnit unit) {
+    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src)
+        .add(FROMLONLAT).add(coord.getLongitude()).add(coord.getLatitude())
+        .add(BYBOX).add(width).add(height).add(unit), BuilderFactory.LONG);
   }
 
-  public final CommandObject<Long> geosearchStore(String dest, String src, GeoSearchParam params){
-    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src).addParams(params),
-            BuilderFactory.LONG);
+  public final CommandObject<Long> geosearchStore(String dest, String src, GeoSearchParam params) {
+    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src).addParams(params), BuilderFactory.LONG);
   }
 
-  public final CommandObject<Long> geosearchStoreStoreDist(String dest, String src, GeoSearchParam params){
-    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src).addParams(params).add(STOREDIST),
-            BuilderFactory.LONG);
+  public final CommandObject<Long> geosearchStoreStoreDist(String dest, String src, GeoSearchParam params) {
+    return new CommandObject<>(commandArguments(GEOSEARCHSTORE).key(dest).add(src).addParams(params).add(STOREDIST), BuilderFactory.LONG);
   }
   // Geo commands
 
--- src/main/java/redis/clients/jedis/params/GeoSearchParam.java
+++ src/main/java/redis/clients/jedis/params/GeoSearchParam.java
@@ -1,8 +1,5 @@
 package redis.clients.jedis.params;
 
-import redis.clients.jedis.GeoCoordinate;
-import redis.clients.jedis.CommandArguments;
-import redis.clients.jedis.args.GeoUnit;
 import static redis.clients.jedis.Protocol.Keyword.ANY;
 import static redis.clients.jedis.Protocol.Keyword.ASC;
 import static redis.clients.jedis.Protocol.Keyword.BYBOX;
@@ -15,153 +12,156 @@ import static redis.clients.jedis.Protocol.Keyword.WITHHASH;
 import static redis.clients.jedis.Protocol.Keyword.FROMMEMBER;
 import static redis.clients.jedis.Protocol.Keyword.FROMLONLAT;
 
-
+import redis.clients.jedis.GeoCoordinate;
+import redis.clients.jedis.CommandArguments;
+import redis.clients.jedis.args.GeoUnit;
 
 public class GeoSearchParam implements IParams {
-    private boolean frommember = false;
-    private boolean fromlonlat = false;
-    private String member;
-    private GeoCoordinate lonlat;
 
-    private boolean byradius = false;
-    private boolean bybox = false;
-    private double radius;
-    private double width;
-    private double height;
-    private GeoUnit unit;
+  private boolean fromMember = false;
+  private boolean fromLonLat = false;
+  private String member;
+  private GeoCoordinate lonlat;
 
-    private boolean withCoord = false;
-    private boolean withDist = false;
-    private boolean withHash = false;
+  private boolean byradius = false;
+  private boolean bybox = false;
+  private double radius;
+  private double width;
+  private double height;
+  private GeoUnit unit;
 
-    private Integer count = null;
-    private boolean any = false;
-    private boolean asc = false;
-    private boolean desc = false;
+  private boolean withCoord = false;
+  private boolean withDist = false;
+  private boolean withHash = false;
 
-    public GeoSearchParam() { }
+  private Integer count = null;
+  private boolean any = false;
+  private boolean asc = false;
+  private boolean desc = false;
 
-    public static GeoSearchParam geoSearchParam() { return new GeoSearchParam(); }
+  public GeoSearchParam() {
+  }
 
-    public GeoSearchParam frommember(String member) {
-        this.frommember = true;
-        this.member = member;
-        return this;
+  public static GeoSearchParam geoSearchParam() {
+    return new GeoSearchParam();
+  }
+
+  public GeoSearchParam frommember(String member) {
+    this.fromMember = true;
+    this.member = member;
+    return this;
+  }
+
+  public GeoSearchParam fromLonLat(double longitude, double latitude) {
+    return fromLonLat(new GeoCoordinate(longitude, latitude));
+  }
+
+  public GeoSearchParam fromLonLat(GeoCoordinate coord) {
+    this.fromLonLat = true;
+    this.lonlat = coord;
+    return this;
+  }
+
+  public GeoSearchParam byRadius(double r, GeoUnit unit) {
+    this.byradius = true;
+    this.radius = r;
+    this.unit = unit;
+    return this;
+  }
+
+  public GeoSearchParam byBox(double width, double height, GeoUnit unit) {
+    this.bybox = true;
+    this.width = width;
+    this.height = height;
+    this.unit = unit;
+    return this;
+  }
+
+  public GeoSearchParam withCoord() {
+    withCoord = true;
+    return this;
+  }
+
+  public GeoSearchParam withDist() {
+    withDist = true;
+    return this;
+  }
+
+  public GeoSearchParam withHash() {
+    withHash = true;
+    return this;
+  }
+
+  public GeoSearchParam ascending() {
+    asc = true;
+    return this;
+  }
+
+  public GeoSearchParam descending() {
+    desc = true;
+    return this;
+  }
+
+  public GeoSearchParam count(int count) {
+    if (count > 0) {
+      this.count = count;
+    }
+    return this;
+  }
+
+  public GeoSearchParam count(int count, boolean any) {
+    if (count > 0) {
+      this.count = count;
+
+      if (any) {
+        this.any = true;
+      }
+    }
+    return this;
+  }
+
+  @Override
+  public void addParams(CommandArguments args) {
+    if (this.fromMember) {
+      args.add(FROMMEMBER);
+      args.add(this.member);
+    } else if (this.fromLonLat) {
+      args.add(FROMLONLAT);
+      args.add(lonlat.getLongitude());
+      args.add(lonlat.getLatitude());
     }
 
-    public GeoSearchParam fromlonlat(double longitude, double latitude) {
-        this.fromlonlat = true;
-        this.lonlat = new GeoCoordinate(longitude, latitude);
-        return this;
+    if (this.byradius) {
+      args.add(BYRADIUS);
+      args.add(this.radius);
+    } else if (this.bybox) {
+      args.add(BYBOX);
+      args.add(this.width);
+      args.add(this.height);
+    }
+    args.add(this.unit);
+
+    if (withCoord) {
+      args.add(WITHCOORD);
+    }
+    if (withDist) {
+      args.add(WITHDIST);
+    }
+    if (withHash) {
+      args.add(WITHHASH);
     }
 
-    public GeoSearchParam fromlonlat(GeoCoordinate coord) {
-        this.fromlonlat = true;
-        this.lonlat = coord;
-        return this;
+    if (count != null) {
+      args.add(COUNT).add(count);
+      if (any) {
+        args.add(ANY);
+      }
     }
 
-
-    public GeoSearchParam byradius(double r, GeoUnit unit){
-        this.byradius = true;
-        this.radius = r;
-        this.unit = unit;
-        return this;
+    if (asc) {
+      args.add(ASC);
+    } else if (desc) {
+      args.add(DESC);
     }
-
-    public GeoSearchParam bybox(double width, double height, GeoUnit unit){
-        this.bybox = true;
-        this.width = width;
-        this.height = height;
-        this.unit = unit;
-        return this;
-    }
-
-    public GeoSearchParam withCoord() {
-        withCoord = true;
-        return this;
-    }
-
-    public GeoSearchParam withDist() {
-        withDist = true;
-        return this;
-    }
-
-    public GeoSearchParam withHash() {
-        withHash = true;
-        return this;
-    }
-
-    public GeoSearchParam sortAscending() {
-        asc = true;
-        return this;
-    }
-
-    public GeoSearchParam sortDescending() {
-        desc = true;
-        return this;
-    }
-
-    public GeoSearchParam count(int count) {
-        if (count > 0) {
-            this.count = count;
-        }
-        return this;
-    }
-
-    public GeoSearchParam count(int count, boolean any) {
-        if (count > 0) {
-            this.count = count;
-
-            if (any) {
-                this.any = true;
-            }
-        }
-        return this;
-    }
-
-    @Override
-    public void addParams(CommandArguments args) {
-        if (this.frommember) {
-            args.add(FROMMEMBER);
-            args.add(this.member);
-        } else if (this.fromlonlat) {
-            args.add(FROMLONLAT);
-            args.add(lonlat.getLongitude());
-            args.add(lonlat.getLatitude());
-        }
-
-        if (this.byradius) {
-            args.add(BYRADIUS);
-            args.add(this.radius);
-        } else if (this.bybox) {
-            args.add(BYBOX);
-            args.add(this.width);
-            args.add(this.height);
-        }
-        args.add(this.unit);
-
-        if (withCoord) {
-            args.add(WITHCOORD);
-        }
-        if (withDist) {
-            args.add(WITHDIST);
-        }
-        if (withHash) {
-            args.add(WITHHASH);
-        }
-
-        if (count != null) {
-            args.add(COUNT).add(count);
-            if (any) {
-                args.add(ANY);
-            }
-        }
-
-        if (asc) {
-            args.add(ASC);
-        } else if (desc) {
-            args.add(DESC);
-        }
-    }
+  }
 }

fc6500ded0eaae5ff3c131d3bee584075260ba7a
zeekling <lingzhaohui@zeekling.cn>
2022-01-16 10:21:15 +0800

    add doc for client commands

--- src/main/java/redis/clients/jedis/commands/ClientBinaryCommands.java
+++ src/main/java/redis/clients/jedis/commands/ClientBinaryCommands.java
@@ -16,7 +16,6 @@ public interface ClientBinaryCommands {
    *
    * @param ipPort The ip:port should match a line returned by the CLIENT LIST command (addr field).
    * @return close success return OK
-   * @see <a href="https://redis.io/commands/client-kill">client kill</a>
    */
   String clientKill(byte[] ipPort);
 
@@ -25,8 +24,7 @@ public interface ClientBinaryCommands {
    *
    * @param ip   The ip should match a line returned by the CLIENT LIST command (addr field).
    * @param port The port should match a line returned by the CLIENT LIST command (addr field).
-   * @return close success return OK
-   * @see <a href="https://redis.io/commands/client-kill">client kill</a>
+   * @return Close success return OK
    */
   String clientKill(String ip, int port);
 
@@ -34,72 +32,62 @@ public interface ClientBinaryCommands {
    * Close a given client connection.
    *
    * @param params connect info will be closed
-   * @return close success return OK
-   * @see <a href="https://redis.io/commands/client-kill">client kill</a>
+   * @return Close success return OK
    */
   long clientKill(ClientKillParams params);
 
   /**
-   * returns the name of the current connection as set by CLIENT SETNAME
+   * Returns the name of the current connection as set by CLIENT SETNAME
    *
-   * @return current connect name
-   * @see <a href="https://redis.io/commands/client-getname">CLIENT GETNAME</a>
+   * @return Current connect name
    */
   byte[] clientGetnameBinary();
 
   /**
-   * returns information and statistics about the client connections server
+   * Returns information and statistics about the client connections server
    * in a mostly human readable format.
    *
-   * @return all clients info connected to redis-server
-   * @see <a href="https://redis.io/commands/client-list">CLIENT LIST</a>
+   * @return All clients info connected to redis-server
    */
   byte[] clientListBinary();
 
   /**
-   * returns information and statistics about the client connections server
+   * Returns information and statistics about the client connections server
    * in a mostly human readable format filter by client type.
    *
    * @return all clients info connected to redis-server
-   * @see <a href="https://redis.io/commands/client-list">CLIENT LIST [TYPE NORMAL|MASTER|REPLICA|PUBSUB] </a>
    */
   byte[] clientListBinary(ClientType type);
 
   /**
-   * returns information and statistics about the client connections server
+   * Returns information and statistics about the client connections server
    * in a mostly human readable format filter by client ids.
    *
-   * @param clientIds unique 64-bit client IDs
-   * @return all clients info connected to redis-server
-   * @see <a href="https://redis.io/commands/client-list">CLIENT LIST [ID client-id [client-id ...]]</a>
+   * @param clientIds Unique 64-bit client IDs
+   * @return All clients info connected to redis-server
    */
   byte[] clientListBinary(long... clientIds);
 
   /**
-   * returns information and statistics about the current client connection
+   * Returns information and statistics about the current client connection
    * in a mostly human readable format.
    *
-   * @return information and statistics about the current client connection
-   * @see <a href="https://redis.io/commands/client-info">CLIENT INFO</a>
-   * @since redis 6.2
+   * @return Information and statistics about the current client connection
    */
   byte[] clientInfoBinary();
 
   /**
-   * assigns a name to the current connection.
+   * Assigns a name to the current connection.
    *
-   * @param name current connection name
+   * @param name Current connection name
    * @return OK if the connection name was successfully set.
-   * @see <a href="https://redis.io/commands/client-setname">CLIENT SETNAME </a>
    */
   String clientSetname(byte[] name);
 
   /**
-   * returns the ID of the current connection.
+   * Returns the ID of the current connection.
    *
    * @return The id of the client.
-   * @see <a href="https://redis.io/commands/client-id">CLIENT ID</a>
-   * @since redis 5.0.0
    */
   long clientId();
 
@@ -108,11 +96,9 @@ public interface ClientBinaryCommands {
    * blocking operation, such as for instance BRPOP or XREAD or WAIT.
    *
    * @param clientId The id of the client
-   * @return specifically:
+   * @return Specifically:
    * 1 if the client was unblocked successfully.
    * 0 if the client wasn't unblocked.
-   * @see <a href="https://redis.io/commands/client-unblock">CLIENT UNBLOCK client-id</a>
-   * @since redis 5.0.0
    */
   long clientUnblock(long clientId);
 
@@ -122,11 +108,9 @@ public interface ClientBinaryCommands {
    *
    * @param clientId    The id of the client
    * @param unblockType TIMEOUT|ERROR
-   * @return specifically:
+   * @return Specifically:
    * 1 if the client was unblocked successfully.
    * 0 if the client wasn't unblocked.
-   * @see <a href="https://redis.io/commands/client-unblock">CLIENT UNBLOCK client-id [TIMEOUT|ERROR]</a>
-   * @since redis 5.0.0
    */
   long clientUnblock(long clientId, UnblockType unblockType);
 
@@ -136,7 +120,6 @@ public interface ClientBinaryCommands {
    *
    * @param timeout WRITE|ALL
    * @return The command returns OK or an error if the timeout is invalid.
-   * @see <a href="https://redis.io/commands/client-pause">CLIENT PAUSE timeout</a>
    */
   String clientPause(long timeout);
 
@@ -144,10 +127,9 @@ public interface ClientBinaryCommands {
    * A connections control command able to suspend all the
    * Redis clients for the specified amount of time (in milliseconds)
    *
-   * @param timeout command timeout
+   * @param timeout Command timeout
    * @param mode    WRITE|ALL
    * @return The command returns OK or an error if the timeout is invalid.
-   * @see <a href="https://redis.io/commands/client-pause">CLIENT PAUSE timeout [WRITE|ALL]</a>
    */
   String clientPause(long timeout, ClientPauseMode mode);
 
--- src/main/java/redis/clients/jedis/commands/ClientCommands.java
+++ src/main/java/redis/clients/jedis/commands/ClientCommands.java
@@ -15,8 +15,7 @@ public interface ClientCommands {
    * Close a given client connection.
    *
    * @param ipPort The ip:port should match a line returned by the CLIENT LIST command (addr field).
-   * @return close success return OK
-   * @see <a href="https://redis.io/commands/client-kill">client kill</a>
+   * @return Close success return OK
    */
   String clientKill(String ipPort);
 
@@ -25,81 +24,70 @@ public interface ClientCommands {
    *
    * @param ip   The ip should match a line returned by the CLIENT LIST command (addr field).
    * @param port The port should match a line returned by the CLIENT LIST command (addr field).
-   * @return close success return OK
-   * @see <a href="https://redis.io/commands/client-kill">client kill</a>
+   * @return Close success return OK
    */
   String clientKill(String ip, int port);
 
   /**
    * Close a given client connection.
    *
-   * @param params connect info will be closed
-   * @return close success return OK
-   * @see <a href="https://redis.io/commands/client-kill">client kill</a>
+   * @param params Connection info will be closed
+   * @return Close success return OK
    */
   long clientKill(ClientKillParams params);
 
   /**
-   * returns the name of the current connection as set by CLIENT SETNAME
+   * Returns the name of the current connection as set by CLIENT SETNAME
    *
-   * @return current connect name
-   * @see <a href="https://redis.io/commands/client-getname">CLIENT GETNAME</a>
+   * @return Current connect name
    */
   String clientGetname();
 
   /**
-   * returns information and statistics about the client connections server
+   * Returns information and statistics about the client connections server
    * in a mostly human readable format.
    *
-   * @return all clients info connected to redis-server
-   * @see <a href="https://redis.io/commands/client-list">CLIENT LIST</a>
+   * @return All clients info connected to redis-server
    */
   String clientList();
 
   /**
-   * returns information and statistics about the client connections server
+   * Returns information and statistics about the client connections server
    * in a mostly human readable format filter by client type.
    *
-   * @return all clients info connected to redis-server
-   * @see <a href="https://redis.io/commands/client-list">CLIENT LIST [TYPE NORMAL|MASTER|REPLICA|PUBSUB] </a>
+   * @return All clients info connected to redis-server
    */
   String clientList(ClientType type);
 
   /**
-   * returns information and statistics about the client connections server
+   * Returns information and statistics about the client connections server
    * in a mostly human readable format filter by client ids.
    *
-   * @param clientIds unique 64-bit client IDs
-   * @return all clients info connected to redis-server
-   * @see <a href="https://redis.io/commands/client-list">CLIENT LIST [ID client-id [client-id ...]]</a>
+   * @param clientIds Unique 64-bit client IDs
+   * @return All clients info connected to redis-server
    */
   String clientList(long... clientIds);
 
   /**
-   * returns information and statistics about the current client connection
+   * Returns information and statistics about the current client connection
    * in a mostly human readable format.
    *
-   * @return information and statistics about the current client connection
-   * @see <a href="https://redis.io/commands/client-info">CLIENT INFO</a>
-   * @since redis 6.2
+   * @return Information and statistics about the current client connection
    */
   String clientInfo();
 
   /**
-   * assigns a name to the current connection.
+   * Assigns a name to the current connection.
    *
    * @param name current connection name
    * @return OK if the connection name was successfully set.
-   * @see <a href="https://redis.io/commands/client-setname">CLIENT SETNAME </a>
    */
   String clientSetname(String name);
 
   /**
-   * returns the ID of the current connection.
+   * Returns the ID of the current connection.
    *
    * @return The id of the client.
-   * @see <a href="https://redis.io/commands/client-id">CLIENT ID</a>
-   * @since redis 5.0.0
    */
   long clientId();
 
@@ -108,11 +96,9 @@ public interface ClientCommands {
    * blocking operation, such as for instance BRPOP or XREAD or WAIT.
    *
    * @param clientId The id of the client
-   * @return specifically:
+   * @return Specifically:
    * 1 if the client was unblocked successfully.
    * 0 if the client wasn't unblocked.
-   * @see <a href="https://redis.io/commands/client-unblock">CLIENT UNBLOCK client-id</a>
-   * @since redis 5.0.0
    */
   long clientUnblock(long clientId);
 
@@ -122,11 +108,9 @@ public interface ClientCommands {
    *
    * @param clientId    The id of the client
    * @param unblockType TIMEOUT|ERROR
-   * @return specifically:
+   * @return Specifically:
    * 1 if the client was unblocked successfully.
    * 0 if the client wasn't unblocked.
-   * @see <a href="https://redis.io/commands/client-unblock">CLIENT UNBLOCK client-id [TIMEOUT|ERROR]</a>
-   * @since redis 5.0.0
    */
   long clientUnblock(long clientId, UnblockType unblockType);
 
@@ -136,7 +120,6 @@ public interface ClientCommands {
    *
    * @param timeout WRITE|ALL
    * @return The command returns OK or an error if the timeout is invalid.
-   * @see <a href="https://redis.io/commands/client-pause">CLIENT PAUSE timeout</a>
    */
   String clientPause(long timeout);
 
@@ -144,10 +127,9 @@ public interface ClientCommands {
    * A connections control command able to suspend all the
    * Redis clients for the specified amount of time (in milliseconds)
    *
-   * @param timeout command timeout
+   * @param timeout Command timeout
    * @param mode    WRITE|ALL
    * @return The command returns OK or an error if the timeout is invalid.
-   * @see <a href="https://redis.io/commands/client-pause">CLIENT PAUSE timeout [WRITE|ALL]</a>
    */
   String clientPause(long timeout, ClientPauseMode mode);
 

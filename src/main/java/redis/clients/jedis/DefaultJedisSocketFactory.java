5ba4b7bc2d4958dff7c3e8e30d0aab2dabb5a302
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2022-01-18 21:41:13 +0600

    Remove unused imports

--- src/main/java/redis/clients/jedis/DefaultJedisSocketFactory.java
+++ src/main/java/redis/clients/jedis/DefaultJedisSocketFactory.java
@@ -1,11 +1,9 @@
 package redis.clients.jedis;
 
 import java.io.IOException;
-import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;

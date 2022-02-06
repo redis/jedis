6b0b26f9d9dfe89ec1475c8bfbc6629c9b374796
Avital-Fine <avital.fine@redis.com>
2022-02-06 16:25:55 +0100

    tab fix

--- src/main/java/redis/clients/jedis/CommandObjects.java
+++ src/main/java/redis/clients/jedis/CommandObjects.java
@@ -130,7 +130,7 @@ public class CommandObjects {
 
   public final CommandObject<Long> pexpire(byte[] key, long milliseconds, ExpiryOption expiryOption) {
     return new CommandObject<>(commandArguments(PEXPIRE).key(key).add(milliseconds).add(expiryOption),
-            BuilderFactory.LONG);
+        BuilderFactory.LONG);
   }
 
   public final CommandObject<Long> expireTime(String key) {

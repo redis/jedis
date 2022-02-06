de5815224dc4a7752d34cfe3b9de7b2cabd69223
Avital-Fine <98389525+Avital-Fine@users.noreply.github.com>
2022-02-06 13:49:04 +0100

    Update src/main/java/redis/clients/jedis/args/ZMPopOption.java
    Co-authored-by: dengliming <liming.d.pro@gmail.com>

--- src/main/java/redis/clients/jedis/args/ZMPopOption.java
+++ src/main/java/redis/clients/jedis/args/ZMPopOption.java
@@ -2,7 +2,7 @@ package redis.clients.jedis.args;
 
 import redis.clients.jedis.util.SafeEncoder;
 
-public enum ZMPopOption implements Rawable{
+public enum ZMPopOption implements Rawable {
   MIN, MAX;
 
   private final byte[] raw;

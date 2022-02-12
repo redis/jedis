d42736f6a6a19c4101d38fcd4c51e25730cb4584
Razi007 <abcrazi@gmail.com>
2022-02-12 13:17:06 +0530

    modified KeyedListElement

--- src/main/java/redis/clients/jedis/resps/KeyedListElement.java
+++ src/main/java/redis/clients/jedis/resps/KeyedListElement.java
@@ -2,6 +2,8 @@ package redis.clients.jedis.resps;
 
 import redis.clients.jedis.util.SafeEncoder;
 
+import java.awt.*;
+
 /**
  * This class is used to represent a List element when it is returned with respective key name.
  */
@@ -9,6 +11,7 @@ public class KeyedListElement {
 
   private final String key;
   private final String element;
+  private final List elementList;
 
   public KeyedListElement(byte[] key, byte[] element) {
     this(SafeEncoder.encode(key), SafeEncoder.encode(element));
@@ -17,6 +20,13 @@ public class KeyedListElement {
   public KeyedListElement(String key, String element) {
     this.key = key;
     this.element = element;
+    this.elementList = null;
+  }
+
+  public KeyedListElement(String key, List elementList) {
+    this.key = key;
+    this.elementList = elementList;
+    this.element = null;
   }
 
   public String getKey() {
@@ -27,22 +37,28 @@ public class KeyedListElement {
     return element;
   }
 
+  public List getElementList() { return elementList; }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (!(o instanceof KeyedListElement)) return false;
 
     KeyedListElement other = (KeyedListElement) o;
-    return key.equals(other.key) && element.equals(other.element);
+    return key.equals(other.key) && element.equals(other.element) && elementList.equals(other.elementList);
   }
 
   @Override
   public int hashCode() {
-    return 31 * key.hashCode() + element.hashCode();
+    return 31 * key.hashCode() + element.hashCode() + elementList.hashCode();
   }
 
   @Override
   public String toString() {
-    return "KeyedListElement{" + "key=" + key + ", element='" + element + "} ";
+    return "KeyedListElement{" +
+            "key='" + key + '\'' +
+            ", element='" + element + '\'' +
+            ", elementList=" + elementList +
+            '}';
   }
 }

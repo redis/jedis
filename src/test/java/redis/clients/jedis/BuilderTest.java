01e17d7e3372dc957dec29b0dcb9feabcce4ada2
Carolin Brandt <c.e.brandt@tudelft.nl>
2022-06-10 15:59:15 +0200

    Add test for BuilderFactory.build

--- src/test/java/redis/clients/jedis/BuilderTest.java
+++ src/test/java/redis/clients/jedis/BuilderTest.java
@@ -1,7 +1,9 @@
 package redis.clients.jedis;
 
 import static org.junit.Assert.assertEquals;
+import static redis.clients.jedis.BuilderFactory.DOUBLE;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BuilderTest {
@@ -16,5 +18,18 @@ public class BuilderTest {
     assertEquals(Double.valueOf(Double.POSITIVE_INFINITY), build);
     build = BuilderFactory.DOUBLE.build("-inf".getBytes());
     assertEquals(Double.valueOf(Double.NEGATIVE_INFINITY), build);
+  }
+
+  @Test
+  public void testBuild() {
+    try {
+      Double build = DOUBLE.build("".getBytes());
+      build = DOUBLE.build("inf".getBytes());
+      build = DOUBLE.build("+inf".getBytes());
+      build = DOUBLE.build("-inf".getBytes());
+      Assert.fail("testBuild should have thrown NumberFormatException");
+    } catch (NumberFormatException expected) {
+      Assert.assertEquals("empty String", expected.getMessage());
+    }
   }
 }

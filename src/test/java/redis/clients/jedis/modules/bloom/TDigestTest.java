ec6ad55f9fced08a9f168a6e94271f4939559815
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2022-10-14 09:34:28 +0600

    Test: Address TDIGEST.BYREVRANK changes (#3168)

--- src/test/java/redis/clients/jedis/modules/bloom/TDigestTest.java
+++ src/test/java/redis/clients/jedis/modules/bloom/TDigestTest.java
@@ -186,7 +186,7 @@ public class TDigestTest extends RedisModuleCommandsTestBase {
     assertEquals(Arrays.asList(1l, 2l), client.tdigestRank(key, 2d, 4d));
     assertEquals(Arrays.asList(0l, 1l), client.tdigestRevRank(key, 5d, 4d));
     assertEquals(Arrays.asList(2d, 3d), client.tdigestByRank(key, 0l, 1l));
-    assertEquals(Arrays.asList(5d, 3d), client.tdigestByRevRank(key, 1l, 2l));
+    assertEquals(Arrays.asList(5d, 3d), client.tdigestByRevRank(key, 0l, 1l));
   }
 //
 //  private static KeyValue<Double, Long> randomValueWeight() {

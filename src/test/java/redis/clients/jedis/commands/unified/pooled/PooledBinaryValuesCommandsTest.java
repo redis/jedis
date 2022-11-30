50339114f23cba3656493571aba4a9882b994270
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2022-11-30 21:44:42 +0600

    Pipelining from UnifiedJedis

--- src/test/java/redis/clients/jedis/commands/unified/pooled/PooledBinaryValuesCommandsTest.java
+++ src/test/java/redis/clients/jedis/commands/unified/pooled/PooledBinaryValuesCommandsTest.java
@@ -13,7 +13,7 @@ public class PooledBinaryValuesCommandsTest extends BinaryValuesCommandsTestBase
   }
 
   @AfterClass
-  public static void closeCluster() {
+  public static void cleanUp() {
     jedis.close();
   }
 
--- src/test/java/redis/clients/jedis/commands/unified/pooled/PooledHyperLogLogCommandsTest.java
+++ src/test/java/redis/clients/jedis/commands/unified/pooled/PooledHyperLogLogCommandsTest.java
@@ -13,7 +13,7 @@ public class PooledHyperLogLogCommandsTest extends HyperLogLogCommandsTestBase {
   }
 
   @AfterClass
-  public static void closeCluster() {
+  public static void cleanUp() {
     jedis.close();
   }
 

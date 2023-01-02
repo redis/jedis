5ce41c1ef9f9d41215e779bf69ba2b4e1c977c0b
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2023-01-02 14:00:26 +0600

    Apply code reviews

--- src/main/java/redis/clients/jedis/ReliableTransaction.java
+++ src/main/java/redis/clients/jedis/ReliableTransaction.java
@@ -4,7 +4,8 @@ import java.util.List;
 import redis.clients.jedis.exceptions.JedisException;
 
 /**
- * A transaction where each command will be immediately sent to Redis server and checked the 'QUEUED' reply.
+ * ReliableTransaction is a transaction where commands are immediately sent to Redis server and the
+ * 'QUEUED' reply checked.
  */
 public class ReliableTransaction extends TransactionBase {
 
@@ -15,13 +16,13 @@ public class ReliableTransaction extends TransactionBase {
   }
 
   /**
-   * Creates a transaction.
+   * Creates a new transaction.
    *
-   * If user wants to WATCH/UNWATCH keys and then call MULTI ({@link #multi()}) ownself, it should
+   * A user wanting to WATCH/UNWATCH keys followed by a call to MULTI ({@link #multi()}) it should
    * be {@code doMulti=false}.
    *
    * @param connection connection
-   * @param doMulti {@code false} for manual WATCH, UNWATCH and MULTI
+   * @param doMulti {@code false} should be set to enable manual WATCH, UNWATCH and MULTI
    */
   public ReliableTransaction(Connection connection, boolean doMulti) {
     super(connection, doMulti);
--- src/main/java/redis/clients/jedis/Transaction.java
+++ src/main/java/redis/clients/jedis/Transaction.java
@@ -20,13 +20,13 @@ public class Transaction extends TransactionBase {
   }
 
   /**
-   * Creates a transaction.
+   * Creates a new transaction.
    *
-   * If user wants to WATCH/UNWATCH keys and then call MULTI ({@link #multi()}) ownself, it should
+   * A user wanting to WATCH/UNWATCH keys followed by a call to MULTI ({@link #multi()}) it should
    * be {@code doMulti=false}.
    *
    * @param connection connection
-   * @param doMulti {@code false} for manual WATCH, UNWATCH and MULTI
+   * @param doMulti {@code false} should be set to enable manual WATCH, UNWATCH and MULTI
    */
   public Transaction(Connection connection, boolean doMulti) {
     super(connection, doMulti);

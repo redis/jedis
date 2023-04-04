8c10c0d2f59af93b7837dc4d8e498199050bb49e
David Maier <60782329+dmaier-redislabs@users.noreply.github.com>
2023-04-04 17:20:03 +0200

    Update SimpleCommandsTestSuite.java
    Added some additional comments about how to combine code samples with JUnit tests.

--- src/test/java/redis/clients/jedis/doc/SimpleCommandsTestSuite.java
+++ src/test/java/redis/clients/jedis/doc/SimpleCommandsTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 /**
  * Here are some basic requirements for documentation examples:
- 
+ *
  * - The code should be as clean as possible
  * - We should avoid external framework or library references as much as possible
  * - Minimal usage of assertions because the main target is to verify if the interface changed
@@ -15,6 +15,9 @@ import org.junit.Test;
  * - Imports refer to Jedis and Sytem packages only
  * - There is a single class that is named based on the example
  * - The class has a single 'run' method that runs the example
+ * 
+ * The comments '// HIDE_START' and '// HIDE_END' are used to mark code ranges that are by default hidden. 
+ * The UI component will allow to unhide those lines of code.
  */
 public class SimpleCommandsTestSuite {
 

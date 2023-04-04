856d68e6c2e428051918d06cf436d816d3b5b86b
David Maier <60782329+dmaier-redislabs@users.noreply.github.com>
2023-04-04 17:17:18 +0200

    Update SimpleCommandsTestSuite.java
    Added some comments

--- src/test/java/redis/clients/jedis/doc/SimpleCommandsTestSuite.java
+++ src/test/java/redis/clients/jedis/doc/SimpleCommandsTestSuite.java
@@ -3,7 +3,18 @@ import org.junit.Test;
 
 
 /**
- * Run documentation example tests
+ * Here are some basic requirements for documentation examples:
+ 
+ * - The code should be as clean as possible
+ * - We should avoid external framework or library references as much as possible
+ * - Minimal usage of assertions because the main target is to verify if the interface changed
+ * - A developer should be able to execute the example (copy and paste) with the least possible effort
+ *
+ * This leads to the following structure of a Java example
+ * 
+ * - Imports refer to Jedis and Sytem packages only
+ * - There is a single class that is named based on the example
+ * - The class has a single 'run' method that runs the example
  */
 public class SimpleCommandsTestSuite {
 

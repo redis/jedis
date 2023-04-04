af23b69608cbe367d22087410caab3fca01c2ced
David Maier <david.maier@redis.com>
2023-04-04 12:58:11 +0200

    Added a simple SET and GET example

---
+++ src/test/java/redis/clients/jedis/doc/SetGetTest.java
@@ -0,0 +1,29 @@
+// PAGES: Set, Get
+// HIDE_START
+package redis.clients.jedis.doc;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import redis.clients.jedis.commands.jedis.JedisCommandsTestBase;
+
+public class SetGetTest extends JedisCommandsTestBase {
+
+    @Test
+    public void setGetTest() {
+
+        // HIDE_END
+        String status = jedis.set("bike:1", "Process 134");
+
+        if ("OK".equals(status))
+            System.out.println("Successfully set the key.");
+
+        String value = jedis.get("bike:1");
+
+        if ( value != null)
+            System.out.println("The value is: " + value  + ".");
+
+        // OUTPUT:
+        assertEquals("OK", status);
+        assertEquals("Process 134", value);
+    }
+}

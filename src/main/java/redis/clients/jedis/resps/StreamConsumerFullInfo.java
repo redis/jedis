4ed997c423fa2f944d0a207764520ef08b638b7f
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2023-05-18 18:43:55 +0600

    Change StreamConsumerFullInfo.getSeenTime() to 'Long'

--- src/main/java/redis/clients/jedis/resps/StreamConsumerFullInfo.java
+++ src/main/java/redis/clients/jedis/resps/StreamConsumerFullInfo.java
@@ -42,8 +42,7 @@ public class StreamConsumerFullInfo implements Serializable {
     return name;
   }
 
-  // TODO: Long
-  public long getSeenTime() {
+  public Long getSeenTime() {
     return seenTime;
   }
 

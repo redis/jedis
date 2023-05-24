1da50e08180663b9bea506974caf4cc4dbf02101
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2023-05-24 16:59:52 +0600

    Undo some of Jedis 5 breaking changes (#3436)

--- src/main/java/redis/clients/jedis/Jedis.java
+++ src/main/java/redis/clients/jedis/Jedis.java
@@ -9449,7 +9449,7 @@ public class Jedis implements ServerCommands, DatabaseCommands, JedisCommands, J
   }
 
   @Override
-  public List<StreamConsumerInfo> xinfoConsumers(String key, String group) {
+  public List<StreamConsumersInfo> xinfoConsumers(String key, String group) {
     return connection.executeCommand(commandObjects.xinfoConsumers(key, group));
   }
 

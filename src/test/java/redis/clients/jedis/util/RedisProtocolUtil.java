60d49eeda06e4f6f1505462975031ee364c7f900
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2023-06-06 18:56:47 +0600

    undo accidental commit

--- src/test/java/redis/clients/jedis/util/RedisProtocolUtil.java
+++ src/test/java/redis/clients/jedis/util/RedisProtocolUtil.java
@@ -5,16 +5,17 @@ import redis.clients.jedis.RedisProtocol;
 public class RedisProtocolUtil {
   
   public static RedisProtocol getRedisProtocol() {
-    return RedisProtocol.RESP2;
-//    String ver = System.getProperty("jedisProtocol");
-//    if (ver != null && !ver.isEmpty()) {
-//      for (RedisProtocol proto : RedisProtocol.values()) {
-//        if (proto.version().equals(ver)) {
-//          return proto;
-//        }
-//      }
-//      throw new IllegalArgumentException("Unknown protocol " + ver);
-//    }
-//    return null;
+//    return RedisProtocol.RESP2;
+//    return RedisProtocol.RESP3;
+    String ver = System.getProperty("jedisProtocol");
+    if (ver != null && !ver.isEmpty()) {
+      for (RedisProtocol proto : RedisProtocol.values()) {
+        if (proto.version().equals(ver)) {
+          return proto;
+        }
+      }
+      throw new IllegalArgumentException("Unknown protocol " + ver);
+    }
+    return null;
   }
 }

0dba977613738080ebc80be3f643ec14ea5ae9c1
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2023-06-20 19:56:11 +0600

    Make ClusterPipeliningTest#assertThreadsCount less strict

--- src/test/java/redis/clients/jedis/ClusterPipeliningTest.java
+++ src/test/java/redis/clients/jedis/ClusterPipeliningTest.java
@@ -1095,6 +1095,6 @@ public class ClusterPipeliningTest {
         .filter(thread -> thread != null && thread.getName() != null
             && thread.getName().startsWith("pool-"))
         .count();
-    MatcherAssert.assertThat(count, Matchers.lessThan(9));
+    MatcherAssert.assertThat(count, Matchers.lessThanOrEqualTo(20));
   }
 }

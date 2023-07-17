8553bf6736b721ea8eda30dfeabd59e4f7b7b6b1
Brian Sam-Bodden <bsb@redis.com>
2023-07-17 07:51:25 -0700

    Refactor Hash Tutorial to Java 8 (from 17)

--- src/test/java/io/redis/examples/HashExample.java
+++ src/test/java/io/redis/examples/HashExample.java
@@ -3,6 +3,8 @@ package io.redis.examples;
 
 import redis.clients.jedis.UnifiedJedis;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 //REMOVE_START
 import org.junit.Test;
@@ -18,26 +20,27 @@ public class HashExample {
       //REMOVE_END
 
       //STEP_START set_get_all
-      var res1 = jedis.hset("bike:1", Map.of(
-          "model", "Deimos", //
-          "brand", "Ergonom", //
-          "type", "Enduro bikes", //
-          "price", "4972"
-      ));
+      Map<String, String> bike1 = new HashMap<>();
+      bike1.put("model", "Deimos");
+      bike1.put("brand", "Ergonom");
+      bike1.put("type", "Enduro bikes");
+      bike1.put("price", "4972");
+
+      Long res1 = jedis.hset("bike:1", bike1);
       System.out.println(res1); // 4
 
-      var res2 = jedis.hget("bike:1", "model");
+      String res2 = jedis.hget("bike:1", "model");
       System.out.println(res2); // Deimos
 
-      var res3 = jedis.hget("bike:1", "price");
+      String res3 = jedis.hget("bike:1", "price");
       System.out.println(res3); // 4972
 
-      var res4 = jedis.hgetAll("bike:1");
+      Map<String, String> res4 = jedis.hgetAll("bike:1");
       System.out.println(res4); // {type=Enduro bikes, brand=Ergonom, price=4972, model=Deimos}
       //STEP_END
 
       //REMOVE_START
-      assertEquals(4, res1);
+      assertEquals(4, res1.longValue());
       assertEquals("Deimos", res2);
       assertEquals("4972", res3);
       assertEquals("Deimos", res4.get("model"));
@@ -47,7 +50,7 @@ public class HashExample {
       //REMOVE_END
 
       //STEP_START hmget
-      var res5 = jedis.hmget("bike:1", "model", "price");
+      List<String> res5 = jedis.hmget("bike:1", "model", "price");
       System.out.println(res5); // [Deimos, 4972]
       //STEP_END
 
@@ -56,42 +59,42 @@ public class HashExample {
       //REMOVE_END
 
       //STEP_START hincrby
-      var res6 = jedis.hincrBy("bike:1", "price", 100);
+      Long res6 = jedis.hincrBy("bike:1", "price", 100);
       System.out.println(res6); // 5072
-      var res7 = jedis.hincrBy("bike:1", "price", -100);
+      Long res7 = jedis.hincrBy("bike:1", "price", -100);
       System.out.println(res7); // 4972
       //STEP_END
 
       //REMOVE_START
-      assertEquals(5072, res6);
-      assertEquals(4972, res7);
+      assertEquals(5072, res6.longValue());
+      assertEquals(4972, res7.longValue());
       //REMOVE_END
 
   
 
       //STEP_START incrby_get_mget
-      var res8 = jedis.hincrBy("bike:1:stats", "rides", 1);
+      Long res8 = jedis.hincrBy("bike:1:stats", "rides", 1);
       System.out.println(res8); // 1
-      var res9 = jedis.hincrBy("bike:1:stats", "rides", 1);
+      Long res9 = jedis.hincrBy("bike:1:stats", "rides", 1);
       System.out.println(res9); // 2
-      var res10 = jedis.hincrBy("bike:1:stats", "rides", 1);
+      Long res10 = jedis.hincrBy("bike:1:stats", "rides", 1);
       System.out.println(res10); // 3
-      var res11 = jedis.hincrBy("bike:1:stats", "crashes", 1);
+      Long res11 = jedis.hincrBy("bike:1:stats", "crashes", 1);
       System.out.println(res11); // 1
-      var res12 = jedis.hincrBy("bike:1:stats", "owners", 1);
+      Long res12 = jedis.hincrBy("bike:1:stats", "owners", 1);
       System.out.println(res12); // 1
-      var res13 = jedis.hget("bike:1:stats", "rides");
+      String res13 = jedis.hget("bike:1:stats", "rides");
       System.out.println(res13); // 3
-      var res14 = jedis.hmget("bike:1:stats", "crashes", "owners");
+      List<String> res14 = jedis.hmget("bike:1:stats", "crashes", "owners");
       System.out.println(res14); // [1, 1]
       //STEP_END
 
       //REMOVE_START
-      assertEquals(1, res8);
-      assertEquals(2, res9);
-      assertEquals(3, res10);
-      assertEquals(1, res11);
-      assertEquals(1, res12);
+      assertEquals(1, res8.longValue());
+      assertEquals(2, res9.longValue());
+      assertEquals(3, res10.longValue());
+      assertEquals(1, res11.longValue());
+      assertEquals(1, res12.longValue());
       assertEquals("3", res13);
       assertEquals("[1, 1]", res14.toString());
       //REMOVE_END

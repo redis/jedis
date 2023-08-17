62afc9e1d03a2531e3f2e9f75ffe2a05c4f14e1b
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2023-08-17 22:12:04 +0600

    [skip ci] in RedisGraphPipelineCommands

--- src/main/java/redis/clients/jedis/graph/RedisGraphPipelineCommands.java
+++ src/main/java/redis/clients/jedis/graph/RedisGraphPipelineCommands.java
@@ -10,23 +10,33 @@ import redis.clients.jedis.Response;
 @Deprecated
 public interface RedisGraphPipelineCommands {
 
+  @Deprecated
   Response<ResultSet> graphQuery(String name, String query);
 
+  @Deprecated
   Response<ResultSet> graphReadonlyQuery(String name, String query);
 
+  @Deprecated
   Response<ResultSet> graphQuery(String name, String query, long timeout);
 
+  @Deprecated
   Response<ResultSet> graphReadonlyQuery(String name, String query, long timeout);
 
+  @Deprecated
   Response<ResultSet> graphQuery(String name, String query, Map<String, Object> params);
 
+  @Deprecated
   Response<ResultSet> graphReadonlyQuery(String name, String query, Map<String, Object> params);
 
+  @Deprecated
   Response<ResultSet> graphQuery(String name, String query, Map<String, Object> params, long timeout);
 
+  @Deprecated
   Response<ResultSet> graphReadonlyQuery(String name, String query, Map<String, Object> params, long timeout);
 
+  @Deprecated
   Response<String> graphDelete(String name);
 
+  @Deprecated
   Response<List<String>> graphProfile(String graphName, String query);
 }

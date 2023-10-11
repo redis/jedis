0c0a239bb7045d6e3d2cd3b84dfc17ff77a724b4
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2023-10-12 00:33:06 +0600

    Allow getting schema field name

--- src/test/java/redis/clients/jedis/modules/search/UtilTest.java
+++ src/test/java/redis/clients/jedis/modules/search/UtilTest.java
@@ -1,8 +1,14 @@
 package redis.clients.jedis.modules.search;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
 import org.junit.Test;
+
 import redis.clients.jedis.search.RediSearchUtil;
+
+import redis.clients.jedis.search.schemafields.NumericField;
+import redis.clients.jedis.search.schemafields.SchemaField;
 
 public class UtilTest {
 
@@ -12,5 +18,15 @@ public class UtilTest {
     byte[] bytes = RediSearchUtil.toByteArray(floats);
     byte[] expected = new byte[]{-51, -52, 76, 62};
     Assert.assertArrayEquals(expected, bytes);
+  }
+
+  @Test
+  public void getSchemaFieldName() {
+    SchemaField field = NumericField.of("$.num").as("num");
+
+    assertEquals("$.num", field.getFieldName().getName());
+    assertEquals("num", field.getFieldName().getAttribute());
+
+    assertEquals("$.num", field.getName());
   }
 }

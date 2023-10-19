3ca30da7814cc0775a1afaccdc024f76ce234636
c00603587 <chenshi35@huawei.com>
2023-10-19 16:20:20 +0800

    test case for SentinelMasterListener
    Signed-off-by: c00603587 <chenshi35@huawei.com>

--- src/test/java/redis/clients/jedis/ZSentinelMasterListenerTest.java
+++ src/test/java/redis/clients/jedis/ZSentinelMasterListenerTest.java
@@ -8,12 +8,13 @@ import redis.clients.jedis.providers.SentineledConnectionProvider;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 /**
  * In order to simulate the scenario of active/standby switching. this test case will effect all the
- * sentinel test case, you can run this test case separately
- * ZSentinelMasterListenerTest start with "Z" to ensure this test case should be run as last one
+ * sentinel test case, you can run this test case separately ZSentinelMasterListenerTest start with
+ * "Z" to ensure this test case should be run as last one
  */
 public class ZSentinelMasterListenerTest {
   private static final String MASTER_NAME = "mymaster";
@@ -40,161 +41,45 @@ public class ZSentinelMasterListenerTest {
   }
 
   @Test
-  public void testSentinelMasterSubscribeListener() {
-    // case 1: default : subscribe on ,active off
-    SentinelPoolConfig config = new SentinelPoolConfig();
-
-    JedisSentinelPool pool = new JedisSentinelPool(MASTER_NAME, sentinels, config, 1000, "foobared",
-        2);
-    HostAndPort hostPort1 = pool.getResource().connection.getHostAndPort();
-
-    Jedis sentinel = new Jedis(sentinel1);
-    sentinel.sendCommand(Protocol.Command.SENTINEL, "failover", MASTER_NAME);
-    try {
-      Thread.sleep(WAIT_FAILOVER_TIMES_MILLS); // sleep. let the failover finish
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-
-    HostAndPort hostPort2 = pool.getResource().connection.getHostAndPort();
-
-    pool.destroy();
-    assertNotEquals(hostPort1, hostPort2);
-  }
-
-  @Test
-  public void testSentinelMasterActiveDetectListener() {
-    // case 2: subscribe off ,active on
-    SentinelPoolConfig config = new SentinelPoolConfig();
-    config.setEnableActiveDetectListener(true);
-    config.setEnableDefaultSubscribeListener(false);
-
-    JedisSentinelPool pool = new JedisSentinelPool(MASTER_NAME, sentinels, config, 1000, "foobared",
-        2);
-    HostAndPort hostPort1 = pool.getResource().connection.getHostAndPort();
-
-    Jedis sentinel = new Jedis(sentinel1);
-    sentinel.sendCommand(Protocol.Command.SENTINEL, "failover", MASTER_NAME);
-    try {
-      Thread.sleep(WAIT_FAILOVER_TIMES_MILLS);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-
-    HostAndPort hostPort2 = pool.getResource().connection.getHostAndPort();
-
-    pool.destroy();
-    assertNotEquals(hostPort1, hostPort2);
-  }
-
-  @Test
-  public void testALLSentinelMasterListener() {
-    // case 2: subscribe on ,active on
+  public void testSentinelMasterListener() {
+    // case : subscribe on ,active on
     SentinelPoolConfig config = new SentinelPoolConfig();
     config.setEnableActiveDetectListener(true);
     config.setEnableDefaultSubscribeListener(true);
     config.setActiveDetectIntervalTimeMillis(5 * 1000);
     config.setSubscribeRetryWaitTimeMillis(5 * 1000);
 
+    // get resource by pool
     JedisSentinelPool pool = new JedisSentinelPool(MASTER_NAME, sentinels, config, 1000, "foobared",
         2);
-    HostAndPort hostPort1 = pool.getResource().connection.getHostAndPort();
+    HostAndPort masterGetFromPool = pool.getResource().connection.getHostAndPort();
 
+    // get resource by connection provider
+    SentineledConnectionProvider sentineledConnectionProvider = new SentineledConnectionProvider(
+        MASTER_NAME, DefaultJedisClientConfig.builder().timeoutMillis(1000).password("foobared")
+            .database(2).build(),
+        config, hostAndPortsSentinels, DefaultJedisClientConfig.builder().build());
+    JedisSentineled jedisSentineled = new JedisSentineled(sentineledConnectionProvider);
+    HostAndPort masterGetFromProvider = jedisSentineled.provider.getConnection().getHostAndPort();
+
+    assertEquals(masterGetFromPool, masterGetFromProvider);
+
+    // failover
     Jedis sentinel = new Jedis(sentinel1);
     sentinel.sendCommand(Protocol.Command.SENTINEL, "failover", MASTER_NAME);
+
     try {
       Thread.sleep(WAIT_FAILOVER_TIMES_MILLS);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
 
-    HostAndPort hostPort2 = pool.getResource().connection.getHostAndPort();
+    HostAndPort masterGetFromPool2 = pool.getResource().connection.getHostAndPort();
+    HostAndPort masterGetFromProvider2 = jedisSentineled.provider.getConnection().getHostAndPort();
 
     pool.destroy();
-    assertNotEquals(hostPort1, hostPort2);
-  }
-
-  @Test
-  public void testSentinelMasterSubscribeListenerForSentineledConnectionProvider() {
-    SentinelPoolConfig config = new SentinelPoolConfig();
-
-    SentineledConnectionProvider sentineledConnectionProvider = new SentineledConnectionProvider(
-        MASTER_NAME, DefaultJedisClientConfig.builder().timeoutMillis(1000).password("foobared")
-            .database(2).build(),
-        config, hostAndPortsSentinels, DefaultJedisClientConfig.builder().build());
-
-    try (JedisSentineled jedis = new JedisSentineled(sentineledConnectionProvider)) {
-
-      HostAndPort master1 = jedis.provider.getConnection().getHostAndPort();
-      Jedis sentinel = new Jedis(sentinel1);
-      sentinel.sendCommand(Protocol.Command.SENTINEL, "failover", MASTER_NAME);
-      try {
-        Thread.sleep(WAIT_FAILOVER_TIMES_MILLS);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-
-      HostAndPort master2 = jedis.provider.getConnection().getHostAndPort();
-
-      assertNotEquals(master1, master2);
-    }
-  }
-
-  @Test
-  public void testSentinelMasterActiveDetectListenerForSentineledConnectionProvider() {
-    SentinelPoolConfig config = new SentinelPoolConfig();
-    config.setEnableActiveDetectListener(true);
-    config.setEnableDefaultSubscribeListener(false);
-
-    SentineledConnectionProvider sentineledConnectionProvider = new SentineledConnectionProvider(
-        MASTER_NAME, DefaultJedisClientConfig.builder().timeoutMillis(1000).password("foobared")
-            .database(2).build(),
-        config, hostAndPortsSentinels, DefaultJedisClientConfig.builder().build());
-
-    try (JedisSentineled jedis = new JedisSentineled(sentineledConnectionProvider)) {
-
-      HostAndPort master1 = jedis.provider.getConnection().getHostAndPort();
-      Jedis sentinel = new Jedis(sentinel1);
-      sentinel.sendCommand(Protocol.Command.SENTINEL, "failover", MASTER_NAME);
-      try {
-        Thread.sleep(WAIT_FAILOVER_TIMES_MILLS);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-
-      HostAndPort master2 = jedis.provider.getConnection().getHostAndPort();
-
-      assertNotEquals(master1, master2);
-    }
-  }
-
-  @Test
-  public void testALLSentinelMasterListenerForSentineledConnectionProvider() {
-    SentinelPoolConfig config = new SentinelPoolConfig();
-    config.setEnableActiveDetectListener(true);
-    config.setEnableDefaultSubscribeListener(true);
-    config.setActiveDetectIntervalTimeMillis(5 * 1000);
-    config.setSubscribeRetryWaitTimeMillis(5 * 1000);
-
-    SentineledConnectionProvider sentineledConnectionProvider = new SentineledConnectionProvider(
-        MASTER_NAME, DefaultJedisClientConfig.builder().timeoutMillis(1000).password("foobared")
-            .database(2).build(),
-        config, hostAndPortsSentinels, DefaultJedisClientConfig.builder().build());
-
-    try (JedisSentineled jedis = new JedisSentineled(sentineledConnectionProvider)) {
-
-      HostAndPort master1 = jedis.provider.getConnection().getHostAndPort();
-      Jedis sentinel = new Jedis(sentinel1);
-      sentinel.sendCommand(Protocol.Command.SENTINEL, "failover", MASTER_NAME);
-      try {
-        Thread.sleep(WAIT_FAILOVER_TIMES_MILLS);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-
-      HostAndPort master2 = jedis.provider.getConnection().getHostAndPort();
-
-      assertNotEquals(master1, master2);
-    }
+    assertEquals(masterGetFromPool2, masterGetFromPool2);
+    assertNotEquals(masterGetFromPool, masterGetFromPool2);
+    assertNotEquals(masterGetFromProvider, masterGetFromProvider2);
   }
 }

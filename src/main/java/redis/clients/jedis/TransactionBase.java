2f95ce8dee225405887e0ad29b96994ed7b86174
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2023-10-30 15:53:21 +0600

    Prepare Transaction with failover (continuing)

--- src/main/java/redis/clients/jedis/TransactionBase.java
+++ src/main/java/redis/clients/jedis/TransactionBase.java
@@ -1,194 +1,29 @@
 package redis.clients.jedis;
 
-import static redis.clients.jedis.Protocol.Command.DISCARD;
-import static redis.clients.jedis.Protocol.Command.EXEC;
-import static redis.clients.jedis.Protocol.Command.MULTI;
-import static redis.clients.jedis.Protocol.Command.UNWATCH;
-import static redis.clients.jedis.Protocol.Command.WATCH;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.io.Closeable;
 import java.util.List;
-import java.util.Queue;
 
-import redis.clients.jedis.exceptions.JedisConnectionException;
-import redis.clients.jedis.exceptions.JedisDataException;
-import redis.clients.jedis.graph.GraphCommandObjects;
+public abstract class TransactionBase extends PipeliningBase implements Closeable {
 
-public abstract class TransactionBase extends TransactionCoreBase {
-
-  private final Queue<Response<?>> pipelinedResponses = new LinkedList<>();
-  protected final Connection connection;
-  private final boolean closeConnection;
-
-  private boolean broken = false;
-  private boolean inWatch = false;
-  private boolean inMulti = false;
-
-  /**
-   * Creates a new transaction.
-   *
-   * A MULTI command will be added to be sent to server. WATCH/UNWATCH/MULTI commands must not be
-   * called with this object.
-   * @param connection connection
-   */
-  public TransactionBase(Connection connection) {
-    this(connection, true);
+  protected TransactionBase() {
+    super(new CommandObjects());
   }
 
-  /**
-   * Creates a new transaction.
-   *
-   * A user wanting to WATCH/UNWATCH keys followed by a call to MULTI ({@link #multi()}) it should
-   * be {@code doMulti=false}.
-   *
-   * @param connection connection
-   * @param doMulti {@code false} should be set to enable manual WATCH, UNWATCH and MULTI
-   */
-  public TransactionBase(Connection connection, boolean doMulti) {
-    this(connection, doMulti, false);
-  }
+  public abstract void multi();
 
-  /**
-   * Creates a new transaction.
-   *
-   * A user wanting to WATCH/UNWATCH keys followed by a call to MULTI ({@link #multi()}) it should
-   * be {@code doMulti=false}.
-   *
-   * @param connection connection
-   * @param doMulti {@code false} should be set to enable manual WATCH, UNWATCH and MULTI
-   * @param closeConnection should the 'connection' be closed when 'close()' is called?
-   */
-  public TransactionBase(Connection connection, boolean doMulti, boolean closeConnection) {
-    this.connection = connection;
-    this.closeConnection = closeConnection;
-    setGraphCommands(new GraphCommandObjects(this.connection));
-    if (doMulti) multi();
-  }
+  public abstract String watch(final String... keys);
 
-  @Override
-  public final void multi() {
-    connection.sendCommand(MULTI);
-    processMultiResponse();
-    inMulti = true;
-  }
+  public abstract String watch(final byte[]... keys);
 
-  @Override
-  public String watch(final String... keys) {
-    connection.sendCommand(WATCH, keys);
-    String status = connection.getStatusCodeReply();
-    inWatch = true;
-    return status;
-  }
+  public abstract String unwatch();
 
-  @Override
-  public String watch(final byte[]... keys) {
-    connection.sendCommand(WATCH, keys);
-    String status = connection.getStatusCodeReply();
-    inWatch = true;
-    return status;
-  }
+  @Override public abstract void close();
 
-  @Override
-  public String unwatch() {
-    connection.sendCommand(UNWATCH);
-    String status = connection.getStatusCodeReply();
-    inWatch = false;
-    return status;
-  }
+  public abstract List<Object> exec();
 
-  protected abstract void processMultiResponse();
+  public abstract String discard();
 
-  protected abstract void processAppendStatus();
-
-  @Override
-  protected final <T> Response<T> appendCommand(CommandObject<T> commandObject) {
-    connection.sendCommand(commandObject.getArguments());
-    processAppendStatus();
-    Response<T> response = new Response<>(commandObject.getBuilder());
-    pipelinedResponses.add(response);
-    return response;
-  }
-
-  @Override
-  public final void close() {
-    try {
-      clear();
-    } finally {
-      if (closeConnection) {
-        connection.close();
-      }
-    }
-  }
-
-  @Override
-  public final void clear() {
-    if (broken) {
-      return;
-    }
-    if (inMulti) {
-      discard();
-    } else if (inWatch) {
-      unwatch();
-    }
-  }
-
-  protected abstract void processPipelinedResponses(int pipelineLength);
-
-  @Override
-  public List<Object> exec() {
-    if (!inMulti) {
-      throw new IllegalStateException("EXEC without MULTI");
-    }
-
-    try {
-      processPipelinedResponses(pipelinedResponses.size());
-      connection.sendCommand(EXEC);
-
-      List<Object> unformatted = connection.getObjectMultiBulkReply();
-      if (unformatted == null) {
-        pipelinedResponses.clear();
-        return null;
-      }
-
-      List<Object> formatted = new ArrayList<>(unformatted.size());
-      for (Object o : unformatted) {
-        try {
-          Response<?> response = pipelinedResponses.poll();
-          response.set(o);
-          formatted.add(response.get());
-        } catch (JedisDataException e) {
-          formatted.add(e);
-        }
-      }
-      return formatted;
-    } catch (JedisConnectionException jce) {
-      broken = true;
-      throw jce;
-    } finally {
-      inMulti = false;
-      inWatch = false;
-      pipelinedResponses.clear();
-    }
-  }
-
-  @Override
-  public String discard() {
-    if (!inMulti) {
-      throw new IllegalStateException("DISCARD without MULTI");
-    }
-
-    try {
-      processPipelinedResponses(pipelinedResponses.size());
-      connection.sendCommand(DISCARD);
-      return connection.getStatusCodeReply();
-    } catch (JedisConnectionException jce) {
-      broken = true;
-      throw jce;
-    } finally {
-      inMulti = false;
-      inWatch = false;
-      pipelinedResponses.clear();
-    }
+  public Response<Long> waitReplicas(int replicas, long timeout) {
+    return appendCommand(commandObjects.waitReplicas(replicas, timeout));
   }
 }

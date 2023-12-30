59f8d049a4227fe48b05c9f2bc827257b6fd5848
Ranjeet Singh <ranjeetsingh@192.168.1.11>
2023-12-30 19:43:37 +0530

    Added java example for bloom filter

---
+++ src/test/java/io/redis/examples/BloomFilterExample.java
@@ -0,0 +1,46 @@
+// EXAMPLE: bf_tutorial
+
+// HIDE_START
+package io.redis.examples;
+
+import org.junit.Test;
+import org.locationtech.jts.util.Assert;
+import redis.clients.jedis.UnifiedJedis;
+
+import java.util.List;
+
+public class BloomFilterExample {
+    @Test
+    public void run(){
+        UnifiedJedis unifiedJedis = new UnifiedJedis("redis://localhost:6379");
+        // HIDE_END
+
+        // STEP_START bloom
+        String res1 = unifiedJedis.bfReserve("bikes:models", 0.01, 1000);
+        System.out.println(res1); // >>> True
+
+        boolean res2 = unifiedJedis.bfAdd("bikes:models", "Smoky Mountain Striker");
+        System.out.println(res2); // >>> True
+
+        boolean res3 = unifiedJedis.bfExists("bikes:models", "Smoky Mountain Striker");
+        System.out.println(res3); // >>> True
+
+        List<Boolean> res4 = unifiedJedis.bfMAdd("bikes:models",
+                "Rocky Mountain Racer",
+                "Cloudy City Cruiser",
+                "Windy City Wippet");
+        System.out.println(res4); // >>> True
+
+        List<Boolean> res5 = unifiedJedis.bfMExists("bikes:models",
+                "Rocky Mountain Racer",
+                "Cloudy City Cruiser",
+                "Windy City Wippet");
+        System.out.println(res5); // >>> [True, True, True]
+        // STEP_END
+
+        // REMOVE_START
+        Assert.equals("OK",res1);
+        // REMOVE_END
+
+    }
+}

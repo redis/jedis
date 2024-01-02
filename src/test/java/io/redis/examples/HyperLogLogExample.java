507308f35a448d71120d936b1bcc89443e18a923
Ranjeet Singh <ranjeetsingh@192.168.1.7>
2024-01-02 21:51:20 +0530

    fix format and imports

--- src/test/java/io/redis/examples/HyperLogLogExample.java
+++ src/test/java/io/redis/examples/HyperLogLogExample.java
@@ -1,39 +1,40 @@
 // EXAMPLE: hll_tutorial
 package io.redis.examples;
-import org.junit.Assert;
+
 import org.junit.Test;
+import org.locationtech.jts.util.Assert;
 import redis.clients.jedis.UnifiedJedis;
 public class HyperLogLogExample {
 
     @Test
     public void run() {
         // HIDE_START
-        UnifiedJedis unifiedJedis = new UnifiedJedis("redis://localhost:6379");
+        UnifiedJedis jedis = new UnifiedJedis("redis://localhost:6379");
         // HIDE_END
 
         // REMOVE_START
-        unifiedJedis.del("bikes", "commuter_bikes", "all_bikes");
+        jedis.del("bikes", "commuter_bikes", "all_bikes");
         // REMOVE_END
 
         // STEP_START pfadd
-        long res1 = unifiedJedis.pfadd("bikes", "Hyperion", "Deimos", "Phoebe", "Quaoar");
+        long res1 = jedis.pfadd("bikes", "Hyperion", "Deimos", "Phoebe", "Quaoar");
         System.out.println(res1); // >>> 1
 
-        long res2 = unifiedJedis.pfcount("bikes");
+        long res2 = jedis.pfcount("bikes");
         System.out.println(res2); // >>> 4
 
-        long res3 = unifiedJedis.pfadd("commuter_bikes", "Salacia", "Mimas", "Quaoar");
+        long res3 = jedis.pfadd("commuter_bikes", "Salacia", "Mimas", "Quaoar");
         System.out.println(res3); // >>> 1
 
-        String res4 = unifiedJedis.pfmerge("all_bikes", "bikes", "commuter_bikes");
+        String res4 = jedis.pfmerge("all_bikes", "bikes", "commuter_bikes");
         System.out.println(res4); // >>> True
 
-        long res5 = unifiedJedis.pfcount("all_bikes");
+        long res5 = jedis.pfcount("all_bikes");
         System.out.println(res5); // >>> 6
         // STEP_END
 
         // REMOVE_START
-        Assert.assertEquals("OK", res4);
+         Assert.equals("OK", res4);
         // REMOVE_END
     }
 }

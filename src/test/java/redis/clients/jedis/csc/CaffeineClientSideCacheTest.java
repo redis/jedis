d783bce02e493dd556e7470ea53c75172f0f0018
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2024-04-29 23:04:25 +0600

    Rename test files

--- src/test/java/redis/clients/jedis/csc/CaffeineClientSideCacheTest.java
+++ src/test/java/redis/clients/jedis/csc/CaffeineClientSideCacheTest.java
@@ -4,10 +4,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheStats;
-import com.google.common.hash.Hashing;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.stats.CacheStats;
 
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
@@ -26,86 +25,69 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisClientConfig;
 import redis.clients.jedis.JedisPooled;
 
-public class GuavaClientSideCacheLibsTest {
-
+public class CaffeineClientSideCacheTest {
+  
   protected static final HostAndPort hnp = HostAndPorts.getRedisServers().get(1);
-
+  
   protected Jedis control;
-
+  
   @Before
   public void setUp() throws Exception {
     control = new Jedis(hnp, DefaultJedisClientConfig.builder().password("foobared").build());
     control.flushAll();
   }
-
+  
   @After
   public void tearDown() throws Exception {
     control.close();
   }
-
+  
   private static final Supplier<JedisClientConfig> clientConfig
       = () -> DefaultJedisClientConfig.builder().resp3().password("foobared").build();
-
+  
   private static final Supplier<GenericObjectPoolConfig<Connection>> singleConnectionPoolConfig
       = () -> {
         ConnectionPoolConfig poolConfig = new ConnectionPoolConfig();
         poolConfig.setMaxTotal(1);
         return poolConfig;
       };
-
+  
   @Test
   public void simple() {
-    GuavaClientSideCache guava = GuavaClientSideCache.builder().maximumSize(10).ttl(10)
-        .hashFunction(Hashing.farmHashFingerprint64()).build();
-    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), guava)) {
+    CaffeineClientSideCache caffeine = CaffeineClientSideCache.builder().maximumSize(10).ttl(10).build();
+    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), caffeine)) {
       control.set("foo", "bar");
       assertEquals("bar", jedis.get("foo"));
       control.del("foo");
       assertThat(jedis.get("foo"), Matchers.oneOf("bar", null)); // ?
     }
   }
-
+  
   @Test
   public void individualCommandsAndThenStats() {
-
-    Cache guava = CacheBuilder.newBuilder().recordStats().build();
-
-    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new GuavaClientSideCache(guava),
+    
+    Cache caffeine = Caffeine.newBuilder().recordStats().build();
+    
+    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(),
+        new CaffeineClientSideCache(caffeine, new OpenHftCommandHasher()),
         singleConnectionPoolConfig.get())) {
       control.set("foo", "bar");
-      assertEquals(0, guava.size());
+      assertEquals(0, caffeine.estimatedSize());
       assertEquals("bar", jedis.get("foo"));
-      assertEquals(1, guava.size());
+      assertEquals(1, caffeine.estimatedSize());
       control.flushAll();
-      assertEquals(1, guava.size());
+      assertEquals(1, caffeine.estimatedSize());
       assertEquals("bar", jedis.get("foo"));
-      assertEquals(1, guava.size());
+      assertEquals(1, caffeine.estimatedSize());
       jedis.ping();
-      assertEquals(0, guava.size());
+      assertEquals(0, caffeine.estimatedSize());
       assertNull(jedis.get("foo"));
-      assertEquals(0, guava.size());
+      assertEquals(0, caffeine.estimatedSize());
     }
-
-    CacheStats stats = guava.stats();
+    
+    CacheStats stats = caffeine.stats();
     assertEquals(1L, stats.hitCount());
     assertThat(stats.missCount(), Matchers.greaterThan(0L));
-  }
-
-  @Test
-  public void maximumSizeExact() {
-    control.set("k1", "v1");
-    control.set("k2", "v2");
-
-    Cache guava = CacheBuilder.newBuilder().maximumSize(1).recordStats().build();
-    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new GuavaClientSideCache(guava))) {
-      assertEquals(0, guava.size());
-      jedis.get("k1");
-      assertEquals(1, guava.size());
-      assertEquals(0, guava.stats().evictionCount());
-      jedis.get("k2");
-      assertEquals(1, guava.size());
-      assertEquals(1, guava.stats().evictionCount());
-    }
   }
 
   @Test
@@ -117,14 +99,14 @@ public class GuavaClientSideCacheLibsTest {
       control.set("k" + i, "v" + i);
     }
 
-    Cache guava = CacheBuilder.newBuilder().maximumSize(maxSize).recordStats().build();
-    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new GuavaClientSideCache(guava))) {
+    Cache caffeine = Caffeine.newBuilder().maximumSize(maxSize).recordStats().build();
+    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new CaffeineClientSideCache(caffeine))) {
       for (int i = 0; i < count; i++) {
         jedis.get("k" + i);
-        assertThat(guava.size(), Matchers.lessThanOrEqualTo(maxEstimatedSize));
+        assertThat(caffeine.estimatedSize(), Matchers.lessThan(maxEstimatedSize));
       }
     }
-    assertThat(guava.stats().evictionCount(), Matchers.greaterThan(count - maxEstimatedSize));
+    assertThat(caffeine.stats().evictionCount(), Matchers.greaterThan(count - maxEstimatedSize));
   }
 
   @Test
@@ -134,19 +116,19 @@ public class GuavaClientSideCacheLibsTest {
       control.set("k" + i, "v" + i);
     }
 
-    Cache guava = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.SECONDS).recordStats().build();
-    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new GuavaClientSideCache(guava))) {
+    Cache caffeine = Caffeine.newBuilder().expireAfterWrite(1, TimeUnit.SECONDS).recordStats().build();
+    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new CaffeineClientSideCache(caffeine))) {
       for (int i = 0; i < count; i++) {
         jedis.get("k" + i);
       }
     }
-    assertThat(guava.size(), Matchers.equalTo((long) count));
-    assertThat(guava.stats().evictionCount(), Matchers.equalTo(0L));
+    assertThat(caffeine.estimatedSize(), Matchers.equalTo((long) count));
+    assertThat(caffeine.stats().evictionCount(), Matchers.equalTo(0L));
 
     TimeUnit.SECONDS.sleep(2);
-    guava.cleanUp();
-    assertThat(guava.size(), Matchers.equalTo(0L));
-    assertThat(guava.stats().evictionCount(), Matchers.equalTo((long) count));
+    caffeine.cleanUp();
+    assertThat(caffeine.estimatedSize(), Matchers.equalTo(0L));
+    assertThat(caffeine.stats().evictionCount(), Matchers.equalTo((long) count));
   }
 
 }
--- src/test/java/redis/clients/jedis/csc/GuavaClientSideCacheTest.java
+++ src/test/java/redis/clients/jedis/csc/GuavaClientSideCacheTest.java
@@ -4,9 +4,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.stats.CacheStats;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheStats;
+import com.google.common.hash.Hashing;
 
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
@@ -25,69 +26,86 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisClientConfig;
 import redis.clients.jedis.JedisPooled;
 
-public class CaffeineClientSideCacheLibsTest {
-  
+public class GuavaClientSideCacheTest {
+
   protected static final HostAndPort hnp = HostAndPorts.getRedisServers().get(1);
-  
+
   protected Jedis control;
-  
+
   @Before
   public void setUp() throws Exception {
     control = new Jedis(hnp, DefaultJedisClientConfig.builder().password("foobared").build());
     control.flushAll();
   }
-  
+
   @After
   public void tearDown() throws Exception {
     control.close();
   }
-  
+
   private static final Supplier<JedisClientConfig> clientConfig
       = () -> DefaultJedisClientConfig.builder().resp3().password("foobared").build();
-  
+
   private static final Supplier<GenericObjectPoolConfig<Connection>> singleConnectionPoolConfig
       = () -> {
         ConnectionPoolConfig poolConfig = new ConnectionPoolConfig();
         poolConfig.setMaxTotal(1);
         return poolConfig;
       };
-  
+
   @Test
   public void simple() {
-    CaffeineClientSideCache caffeine = CaffeineClientSideCache.builder().maximumSize(10).ttl(10).build();
-    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), caffeine)) {
+    GuavaClientSideCache guava = GuavaClientSideCache.builder().maximumSize(10).ttl(10)
+        .hashFunction(Hashing.farmHashFingerprint64()).build();
+    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), guava)) {
       control.set("foo", "bar");
       assertEquals("bar", jedis.get("foo"));
       control.del("foo");
       assertThat(jedis.get("foo"), Matchers.oneOf("bar", null)); // ?
     }
   }
-  
+
   @Test
   public void individualCommandsAndThenStats() {
-    
-    Cache caffeine = Caffeine.newBuilder().recordStats().build();
-    
-    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(),
-        new CaffeineClientSideCache(caffeine, new OpenHftCommandHasher()),
+
+    Cache guava = CacheBuilder.newBuilder().recordStats().build();
+
+    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new GuavaClientSideCache(guava),
         singleConnectionPoolConfig.get())) {
       control.set("foo", "bar");
-      assertEquals(0, caffeine.estimatedSize());
+      assertEquals(0, guava.size());
       assertEquals("bar", jedis.get("foo"));
-      assertEquals(1, caffeine.estimatedSize());
+      assertEquals(1, guava.size());
       control.flushAll();
-      assertEquals(1, caffeine.estimatedSize());
+      assertEquals(1, guava.size());
       assertEquals("bar", jedis.get("foo"));
-      assertEquals(1, caffeine.estimatedSize());
+      assertEquals(1, guava.size());
       jedis.ping();
-      assertEquals(0, caffeine.estimatedSize());
+      assertEquals(0, guava.size());
       assertNull(jedis.get("foo"));
-      assertEquals(0, caffeine.estimatedSize());
+      assertEquals(0, guava.size());
     }
-    
-    CacheStats stats = caffeine.stats();
+
+    CacheStats stats = guava.stats();
     assertEquals(1L, stats.hitCount());
     assertThat(stats.missCount(), Matchers.greaterThan(0L));
+  }
+
+  @Test
+  public void maximumSizeExact() {
+    control.set("k1", "v1");
+    control.set("k2", "v2");
+
+    Cache guava = CacheBuilder.newBuilder().maximumSize(1).recordStats().build();
+    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new GuavaClientSideCache(guava))) {
+      assertEquals(0, guava.size());
+      jedis.get("k1");
+      assertEquals(1, guava.size());
+      assertEquals(0, guava.stats().evictionCount());
+      jedis.get("k2");
+      assertEquals(1, guava.size());
+      assertEquals(1, guava.stats().evictionCount());
+    }
   }
 
   @Test
@@ -99,14 +117,14 @@ public class CaffeineClientSideCacheLibsTest {
       control.set("k" + i, "v" + i);
     }
 
-    Cache caffeine = Caffeine.newBuilder().maximumSize(maxSize).recordStats().build();
-    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new CaffeineClientSideCache(caffeine))) {
+    Cache guava = CacheBuilder.newBuilder().maximumSize(maxSize).recordStats().build();
+    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new GuavaClientSideCache(guava))) {
       for (int i = 0; i < count; i++) {
         jedis.get("k" + i);
-        assertThat(caffeine.estimatedSize(), Matchers.lessThan(maxEstimatedSize));
+        assertThat(guava.size(), Matchers.lessThanOrEqualTo(maxEstimatedSize));
       }
     }
-    assertThat(caffeine.stats().evictionCount(), Matchers.greaterThan(count - maxEstimatedSize));
+    assertThat(guava.stats().evictionCount(), Matchers.greaterThan(count - maxEstimatedSize));
   }
 
   @Test
@@ -116,19 +134,19 @@ public class CaffeineClientSideCacheLibsTest {
       control.set("k" + i, "v" + i);
     }
 
-    Cache caffeine = Caffeine.newBuilder().expireAfterWrite(1, TimeUnit.SECONDS).recordStats().build();
-    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new CaffeineClientSideCache(caffeine))) {
+    Cache guava = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.SECONDS).recordStats().build();
+    try (JedisPooled jedis = new JedisPooled(hnp, clientConfig.get(), new GuavaClientSideCache(guava))) {
       for (int i = 0; i < count; i++) {
         jedis.get("k" + i);
       }
     }
-    assertThat(caffeine.estimatedSize(), Matchers.equalTo((long) count));
-    assertThat(caffeine.stats().evictionCount(), Matchers.equalTo(0L));
+    assertThat(guava.size(), Matchers.equalTo((long) count));
+    assertThat(guava.stats().evictionCount(), Matchers.equalTo(0L));
 
     TimeUnit.SECONDS.sleep(2);
-    caffeine.cleanUp();
-    assertThat(caffeine.estimatedSize(), Matchers.equalTo(0L));
-    assertThat(caffeine.stats().evictionCount(), Matchers.equalTo((long) count));
+    guava.cleanUp();
+    assertThat(guava.size(), Matchers.equalTo(0L));
+    assertThat(guava.stats().evictionCount(), Matchers.equalTo((long) count));
   }
 
 }

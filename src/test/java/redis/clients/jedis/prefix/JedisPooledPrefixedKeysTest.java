7175a5ac624376b4ec6c1db0d37bf29b462db0fd
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2024-06-11 17:27:28 +0600

    Merge fix: after introducing EndpointConfig in #3836

--- src/test/java/redis/clients/jedis/prefix/JedisPooledPrefixedKeysTest.java
+++ src/test/java/redis/clients/jedis/prefix/JedisPooledPrefixedKeysTest.java
@@ -1,18 +1,15 @@
 package redis.clients.jedis.prefix;
 
-import redis.clients.jedis.DefaultJedisClientConfig;
-import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.EndpointConfig;
 import redis.clients.jedis.HostAndPorts;
-import redis.clients.jedis.JedisClientConfig;
 import redis.clients.jedis.JedisPooled;
 
 public class JedisPooledPrefixedKeysTest extends PrefixedKeysTest<JedisPooled> {
 
-  private static final HostAndPort ADDRESS = HostAndPorts.getRedisServers().get(1);
-  private static final JedisClientConfig CLIENT_CONFIG = DefaultJedisClientConfig.builder().password("foobared").build();
+  private static final EndpointConfig ENDPOINT = HostAndPorts.getRedisEndpoint("standalone1");
 
   @Override
   JedisPooled nonPrefixingJedis() {
-    return new JedisPooled(ADDRESS, CLIENT_CONFIG);
+    return new JedisPooled(ENDPOINT.getHostAndPort(), ENDPOINT.getClientConfigBuilder().timeoutMillis(500).build());
   }
 }

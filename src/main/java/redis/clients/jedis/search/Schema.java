f136c6f7bf78236d271b907b654a93ad98bcc813
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2024-06-15 20:54:54 +0600

    Support indexing of MISSING and EMPTY values (#3866)

--- src/main/java/redis/clients/jedis/search/Schema.java
+++ src/main/java/redis/clients/jedis/search/Schema.java
@@ -365,7 +365,7 @@ public class Schema {
     @Override
     public void addTypeArgs(CommandArguments args) {
       args.add(algorithm);
-      args.add(attributes.size() * 2);
+      args.add(attributes.size() << 1);
       for (Map.Entry<String, Object> entry : attributes.entrySet()) {
         args.add(entry.getKey());
         args.add(entry.getValue());

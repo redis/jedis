a0b6e89c59dcac532790996fb839c751a854ab8b
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2024-07-16 16:17:02 +0600

    Shift codes left

--- src/test/java/io/redis/examples/BitfieldExample.java
+++ src/test/java/io/redis/examples/BitfieldExample.java
@@ -15,38 +15,38 @@ import redis.clients.jedis.UnifiedJedis;
 public class BitfieldExample {
     @Test
     public void run() {
-        try (UnifiedJedis jedis = new UnifiedJedis("redis://localhost:6379")) {
+        UnifiedJedis jedis = new UnifiedJedis("redis://localhost:6379");
 // HIDE_END
 
-            //REMOVE_START
-            // Clear any keys here before using them in tests.
-            jedis.del("bike:1:stats");
-            //REMOVE_END
+        //REMOVE_START
+        // Clear any keys here before using them in tests.
+        jedis.del("bike:1:stats");
+        //REMOVE_END
 
-            // STEP_START bf
-            List<Long> res1 = jedis.bitfield("bike:1:stats", "SET", "u32", "#0", "1000");
-            System.out.println(res1);   // >>> [0]
+        // STEP_START bf
+        List<Long> res1 = jedis.bitfield("bike:1:stats", "SET", "u32", "#0", "1000");
+        System.out.println(res1);   // >>> [0]
 
-            List<Long> res2 = jedis.bitfield("bike:1:stats", "INCRBY", "u32", "#0", "-50", "INCRBY", "u32", "#1", "1");
-            System.out.println(res2);   // >>> [950, 1]
+        List<Long> res2 = jedis.bitfield("bike:1:stats", "INCRBY", "u32", "#0", "-50", "INCRBY", "u32", "#1", "1");
+        System.out.println(res2);   // >>> [950, 1]
 
-            List<Long> res3 = jedis.bitfield("bike:1:stats", "INCRBY", "u32", "#0", "500", "INCRBY", "u32", "#1", "1");
-            System.out.println(res3);   // >>> [1450, 2]
+        List<Long> res3 = jedis.bitfield("bike:1:stats", "INCRBY", "u32", "#0", "500", "INCRBY", "u32", "#1", "1");
+        System.out.println(res3);   // >>> [1450, 2]
 
-            List<Long> res4 = jedis.bitfield("bike:1:stats", "GET", "u32", "#0", "GET", "u32", "#1");
-            System.out.println(res4);   // >>> [1450, 2]
-            // STEP_END
+        List<Long> res4 = jedis.bitfield("bike:1:stats", "GET", "u32", "#0", "GET", "u32", "#1");
+        System.out.println(res4);   // >>> [1450, 2]
+        // STEP_END
 
-            // Tests for 'bf' step.
-            // REMOVE_START
-            Assert.assertEquals("[0]", res1.toString());
-            Assert.assertEquals("[950, 1]", res2.toString());
-            Assert.assertEquals("[1450, 2]", res3.toString());
-            Assert.assertEquals("[1450, 2]", res4.toString());
-            // REMOVE_END
+        // Tests for 'bf' step.
+        // REMOVE_START
+        Assert.assertEquals("[0]", res1.toString());
+        Assert.assertEquals("[950, 1]", res2.toString());
+        Assert.assertEquals("[1450, 2]", res3.toString());
+        Assert.assertEquals("[1450, 2]", res4.toString());
+        // REMOVE_END
 
 // HIDE_START
-        }
+        jedis.close();
     }
 }
 // HIDE_END

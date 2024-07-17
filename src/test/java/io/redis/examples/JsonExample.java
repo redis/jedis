dc7d3cea117306a1a6ca37b707f77dde3d68fba8
Andy Stark <andrew.stark@redis.com>
2024-07-17 10:03:32 +0100

    Wrapped long strings

--- src/test/java/io/redis/examples/JsonExample.java
+++ src/test/java/io/redis/examples/JsonExample.java
@@ -199,52 +199,73 @@ public class JsonExample {
         // REMOVE_END
 
         // STEP_START set_bikes
-        String inventory_json = "{" +
-        "    \"inventory\": {" +
-        "        \"mountain_bikes\": [" +
-        "            {" +
-        "                \"id\": \"bike:1\"," +
-        "                \"model\": \"Phoebe\"," +
-        "                \"description\": \"This is a mid-travel trail slayer that is a fantastic daily driver or one bike quiver. The Shimano Claris 8-speed groupset gives plenty of gear range to tackle hills and there\u2019s room for mudguards and a rack too.  This is the bike for the rider who wants trail manners with low fuss ownership.\"," +
-        "                \"price\": 1920," +
-        "                \"specs\": {\"material\": \"carbon\", \"weight\": 13.1}," +
-        "                \"colors\": [\"black\", \"silver\"]" +
-        "            }," +
-        "            {" +
-        "                \"id\": \"bike:2\"," +
-        "                \"model\": \"Quaoar\"," +
-        "                \"description\": \"Redesigned for the 2020 model year, this bike impressed our testers and is the best all-around trail bike we've ever tested. The Shimano gear system effectively does away with an external cassette, so is super low maintenance in terms of wear and tear. All in all it's an impressive package for the price, making it very competitive.\"," +
-        "                \"price\": 2072," +
-        "                \"specs\": {\"material\": \"aluminium\", \"weight\": 7.9}," +
-        "                \"colors\": [\"black\", \"white\"]" +
-        "            }," +
-        "            {" +
-        "                \"id\": \"bike:3\"," +
-        "                \"model\": \"Weywot\"," +
-        "                \"description\": \"This bike gives kids aged six years and older a durable and uberlight mountain bike for their first experience on tracks and easy cruising through forests and fields. A set of powerful Shimano hydraulic disc brakes provide ample stopping ability. If you're after a budget option, this is one of the best bikes you could get.\"," +
-        "                \"price\": 3264," +
-        "                \"specs\": {\"material\": \"alloy\", \"weight\": 13.8}" +
-        "            }" +
-        "        ]," +
-        "        \"commuter_bikes\": [" +
-        "            {" +
-        "                \"id\": \"bike:4\"," +
-        "                \"model\": \"Salacia\"," +
-        "                \"description\": \"This bike is a great option for anyone who just wants a bike to get about on With a slick-shifting Claris gears from Shimano\u2019s, this is a bike which doesn\u2019t break the bank and delivers craved performance.  It\u2019s for the rider who wants both efficiency and capability.\"," +
-        "                \"price\": 1475," +
-        "                \"specs\": {\"material\": \"aluminium\", \"weight\": 16.6}," +
-        "                \"colors\": [\"black\", \"silver\"]" +
-        "            }," +
-        "            {" +
-        "                \"id\": \"bike:5\"," +
-        "                \"model\": \"Mimas\"," +
-        "                \"description\": \"A real joy to ride, this bike got very high scores in last years Bike of the year report. The carefully crafted 50-34 tooth chainset and 11-32 tooth cassette give an easy-on-the-legs bottom gear for climbing, and the high-quality Vittoria Zaffiro tires give balance and grip.It includes a low-step frame , our memory foam seat, bump-resistant shocks and conveniently placed thumb throttle. Put it all together and you get a bike that helps redefine what can be done for this price.\"," +
-        "                \"price\": 3941," +
-        "                \"specs\": {\"material\": \"alloy\", \"weight\": 11.6}" +
-        "            }" +
-        "        ]" +
-        "    }" +
-        "}";
+        String inventory_json = "{"
+        + "    \"inventory\": {"
+        + "        \"mountain_bikes\": ["
+        + "            {"
+        + "                \"id\": \"bike:1\","
+        + "                \"model\": \"Phoebe\","
+        + "                \"description\": \"This is a mid-travel trail slayer that is a "
+        + "fantastic daily driver or one bike quiver. The Shimano Claris 8-speed groupset "
+        + "gives plenty of gear range to tackle hills and there\u2019s room for mudguards "
+        + "and a rack too.  This is the bike for the rider who wants trail manners with "
+        + "low fuss ownership.\","
+        + "                \"price\": 1920,"
+        + "                \"specs\": {\"material\": \"carbon\", \"weight\": 13.1},"
+        + "                \"colors\": [\"black\", \"silver\"]"
+        + "            },"
+        + "            {"
+        + "                \"id\": \"bike:2\","
+        + "                \"model\": \"Quaoar\","
+        + "                \"description\": \"Redesigned for the 2020 model year, this "
+        + "bike impressed our testers and is the best all-around trail bike we've ever "
+        + "tested. The Shimano gear system effectively does away with an external cassette, "
+        + "so is super low maintenance in terms of wear and tear. All in all it's an "
+        + "impressive package for the price, making it very competitive.\","
+        + "                \"price\": 2072,"
+        + "                \"specs\": {\"material\": \"aluminium\", \"weight\": 7.9},"
+        + "                \"colors\": [\"black\", \"white\"]"
+        + "            },"
+        + "            {"
+        + "                \"id\": \"bike:3\","
+        + "                \"model\": \"Weywot\","
+        + "                \"description\": \"This bike gives kids aged six years and older "
+        + "a durable and uberlight mountain bike for their first experience on tracks and easy "
+        + "cruising through forests and fields. A set of powerful Shimano hydraulic disc brakes "
+        + "provide ample stopping ability. If you're after a budget option, this is one of the "
+        + "best bikes you could get.\","
+        + "                \"price\": 3264,"
+        + "                \"specs\": {\"material\": \"alloy\", \"weight\": 13.8}"
+        + "            }"
+        + "        ],"
+        + "        \"commuter_bikes\": ["
+        + "            {"
+        + "                \"id\": \"bike:4\","
+        + "                \"model\": \"Salacia\","
+        + "                \"description\": \"This bike is a great option for anyone who just "
+        + "wants a bike to get about on With a slick-shifting Claris gears from Shimano\u2019s, "
+        + "this is a bike which doesn\u2019t break the bank and delivers craved performance.  "
+        + "It\u2019s for the rider who wants both efficiency and capability.\","
+        + "                \"price\": 1475,"
+        + "                \"specs\": {\"material\": \"aluminium\", \"weight\": 16.6},"
+        + "                \"colors\": [\"black\", \"silver\"]"
+        + "            },"
+        + "            {"
+        + "                \"id\": \"bike:5\","
+        + "                \"model\": \"Mimas\","
+        + "                \"description\": \"A real joy to ride, this bike got very high scores "
+        + "in last years Bike of the year report. The carefully crafted 50-34 tooth chainset "
+        + "and 11-32 tooth cassette give an easy-on-the-legs bottom gear for climbing, and the "
+        + "high-quality Vittoria Zaffiro tires give balance and grip.It includes a low-step "
+        + "frame , our memory foam seat, bump-resistant shocks and conveniently placed thumb "
+        + "throttle. Put it all together and you get a bike that helps redefine what can be "
+        + "done for this price.\","
+        + "                \"price\": 3941,"
+        + "                \"specs\": {\"material\": \"alloy\", \"weight\": 11.6}"
+        + "            }"
+        + "        ]"
+        + "    }"
+        + "}";
 
         String res28 = jedis.jsonSet(
             "bikes:inventory", new Path2("$"), inventory_json
@@ -269,7 +290,42 @@ public class JsonExample {
         // Tests for 'get_bikes' step.
         // REMOVE_START
         Assert.assertEquals(
-            "[[{\"specs\":{\"material\":\"carbon\",\"weight\":13.1},\"price\":1920,\"description\":\"This is a mid-travel trail slayer that is a fantastic daily driver or one bike quiver. The Shimano Claris 8-speed groupset gives plenty of gear range to tackle hills and there\\u2019s room for mudguards and a rack too.  This is the bike for the rider who wants trail manners with low fuss ownership.\",\"model\":\"Phoebe\",\"id\":\"bike:1\",\"colors\":[\"black\",\"silver\"]},{\"specs\":{\"material\":\"aluminium\",\"weight\":7.9},\"price\":2072,\"description\":\"Redesigned for the 2020 model year, this bike impressed our testers and is the best all-around trail bike we've ever tested. The Shimano gear system effectively does away with an external cassette, so is super low maintenance in terms of wear and tear. All in all it's an impressive package for the price, making it very competitive.\",\"model\":\"Quaoar\",\"id\":\"bike:2\",\"colors\":[\"black\",\"white\"]},{\"specs\":{\"material\":\"alloy\",\"weight\":13.8},\"price\":3264,\"description\":\"This bike gives kids aged six years and older a durable and uberlight mountain bike for their first experience on tracks and easy cruising through forests and fields. A set of powerful Shimano hydraulic disc brakes provide ample stopping ability. If you're after a budget option, this is one of the best bikes you could get.\",\"model\":\"Weywot\",\"id\":\"bike:3\"}],[{\"specs\":{\"material\":\"aluminium\",\"weight\":16.6},\"price\":1475,\"description\":\"This bike is a great option for anyone who just wants a bike to get about on With a slick-shifting Claris gears from Shimano\\u2019s, this is a bike which doesn\\u2019t break the bank and delivers craved performance.  It\\u2019s for the rider who wants both efficiency and capability.\",\"model\":\"Salacia\",\"id\":\"bike:4\",\"colors\":[\"black\",\"silver\"]},{\"specs\":{\"material\":\"alloy\",\"weight\":11.6},\"price\":3941,\"description\":\"A real joy to ride, this bike got very high scores in last years Bike of the year report. The carefully crafted 50-34 tooth chainset and 11-32 tooth cassette give an easy-on-the-legs bottom gear for climbing, and the high-quality Vittoria Zaffiro tires give balance and grip.It includes a low-step frame , our memory foam seat, bump-resistant shocks and conveniently placed thumb throttle. Put it all together and you get a bike that helps redefine what can be done for this price.\",\"model\":\"Mimas\",\"id\":\"bike:5\"}]]",
+            "[[{\"specs\":{\"material\":\"carbon\",\"weight\":13.1},\"price\":1920,"
+            + "\"description\":\"This is a mid-travel trail slayer that is a "
+            + "fantastic daily driver or one bike quiver. The Shimano Claris 8-speed "
+            + "groupset gives plenty of gear range to tackle hills and "
+            + "there\\u2019s room for mudguards and a rack too.  This is the bike "
+            + "for the rider who wants trail manners with low fuss "
+            + "ownership.\",\"model\":\"Phoebe\",\"id\":\"bike:1\",\"colors\":"
+            + "[\"black\",\"silver\"]},{\"specs\":{\"material\":\"aluminium\",\"weight\":7.9},"
+            + "\"price\":2072,\"description\":\"Redesigned for the 2020 model year, this "
+            + "bike impressed our testers and is the best all-around trail bike we've "
+            + "ever tested. The Shimano gear system effectively does away with an "
+            + "external cassette, so is super low maintenance in terms of wear and tear. "
+            + "All in all it's an impressive package for the price, making it very "
+            + "competitive.\",\"model\":\"Quaoar\",\"id\":\"bike:2\",\"colors\":"
+            + "[\"black\",\"white\"]},{\"specs\":{\"material\":\"alloy\",\"weight\":13.8},"
+            + "\"price\":3264,\"description\":\"This bike gives kids aged six years and "
+            + "older a durable and uberlight mountain bike for their first experience "
+            + "on tracks and easy cruising through forests and fields. A set of "
+            + "powerful Shimano hydraulic disc brakes provide ample stopping ability. "
+            + "If you're after a budget option, this is one of the best bikes you could "
+            + "get.\",\"model\":\"Weywot\",\"id\":\"bike:3\"}],[{\"specs\":"
+            + "{\"material\":\"aluminium\",\"weight\":16.6},\"price\":1475,\"description\":"
+            + "\"This bike is a great option for anyone who just wants a bike to get about "
+            + "on With a slick-shifting Claris gears from Shimano\\u2019s, this is a bike "
+            + "which doesn\\u2019t break the bank and delivers craved performance.  "
+            + "It\\u2019s for the rider who wants both efficiency and "
+            + "capability.\",\"model\":\"Salacia\",\"id\":\"bike:4\",\"colors\":"
+            + "[\"black\",\"silver\"]},{\"specs\":{\"material\":\"alloy\",\"weight\":11.6},"
+            + "\"price\":3941,\"description\":\"A real joy to ride, this bike got very "
+            + "high scores in last years Bike of the year report. The carefully crafted "
+            + "50-34 tooth chainset and 11-32 tooth cassette give an easy-on-the-legs "
+            + "bottom gear for climbing, and the high-quality Vittoria Zaffiro tires "
+            + "give balance and grip.It includes a low-step frame , our memory foam "
+            + "seat, bump-resistant shocks and conveniently placed thumb throttle. Put "
+            + "it all together and you get a bike that helps redefine what can be done "
+            + "for this price.\",\"model\":\"Mimas\",\"id\":\"bike:5\"}]]",
              res29.toString()
         );
         // REMOVE_END
@@ -342,7 +398,13 @@ public class JsonExample {
         // Tests for 'filter1' step.
         // REMOVE_START
         Assert.assertEquals(
-            "[{\"specs\":{\"material\":\"aluminium\",\"weight\":7.9},\"price\":2072,\"description\":\"Redesigned for the 2020 model year, this bike impressed our testers and is the best all-around trail bike we've ever tested. The Shimano gear system effectively does away with an external cassette, so is super low maintenance in terms of wear and tear. All in all it's an impressive package for the price, making it very competitive.\",\"model\":\"Quaoar\",\"id\":\"bike:2\",\"colors\":[\"black\",\"white\"]}]",
+            "[{\"specs\":{\"material\":\"aluminium\",\"weight\":7.9},\"price\":2072,"
+            + "\"description\":\"Redesigned for the 2020 model year, this bike impressed "
+            + "our testers and is the best all-around trail bike we've ever tested. The "
+            + "Shimano gear system effectively does away with an external cassette, "
+            + "so is super low maintenance in terms of wear and tear. All in all it's an "
+            + "impressive package for the price, making it very competitive.\",\"model\":"
+            + "\"Quaoar\",\"id\":\"bike:2\",\"colors\":[\"black\",\"white\"]}]",
             res35.toString()
         );
         // REMOVE_END
@@ -472,7 +534,8 @@ public class JsonExample {
         // REMOVE_START
         Assert.assertEquals("[3, 3]", res43.toString());
         Assert.assertEquals(
-            "[[\"black\",\"silver\",\"\\\"pink\\\"\"],[\"black\",\"white\"],[\"black\",\"silver\",\"\\\"pink\\\"\"]]",
+            "[[\"black\",\"silver\",\"\\\"pink\\\"\"],[\"black\",\"white\"],"
+            + "[\"black\",\"silver\",\"\\\"pink\\\"\"]]",
             res44.toString());
         // REMOVE_END
 

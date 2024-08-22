beadf6aa6f05d155eaedf6196b7d7aee29aa0aa9
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2024-08-22 15:49:40 +0600

    Rename readonly config param to specify Redis Cluster (#3932)
    * Rename readonly config param to specify Redis Cluster
    
    * clarify javadoc

--- src/main/java/redis/clients/jedis/Connection.java
+++ src/main/java/redis/clients/jedis/Connection.java
@@ -467,8 +467,8 @@ public class Connection implements Closeable {
         }
       }
 
-      // set readonly flag to ALL connections (including master nodes) when enable read from replica
-      if (config.isReadOnlyForReplica()) {
+      // set READONLY flag to ALL connections (including master nodes) when enable read from replica
+      if (config.isReadOnlyForRedisClusterReplicas()) {
         fireAndForgetMsg.add(new CommandArguments(Command.READONLY));
       }
 

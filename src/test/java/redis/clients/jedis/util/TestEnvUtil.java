783acaede2328b233bfdc5f665a23c215e4530dc
ggivo <ivo.gaydazhiev@redis.com>
2024-11-19 11:49:51 +0200

    Switch ctest env setup default from 'local' to 'docker'

--- src/test/java/redis/clients/jedis/util/TestEnvUtil.java
+++ src/test/java/redis/clients/jedis/util/TestEnvUtil.java
@@ -3,7 +3,12 @@ package redis.clients.jedis.util;
 import java.util.Optional;
 
 public class TestEnvUtil {
-    private static final String TEST_ENV_PROVIDER = System.getenv().getOrDefault("TEST_ENV_PROVIDER", "");
+    // Redis servers running inside docker
+    public static final String ENV_DOCKER = "docker";
+    // Redis servers running localy on the test host
+    public static final String ENV_LOCAL = "local";
+
+    private static final String TEST_ENV_PROVIDER = System.getenv().getOrDefault("TEST_ENV_PROVIDER", ENV_DOCKER);
     private static final String TESTMODULE_SO = Optional.ofNullable(System.getenv("TESTMODULE_SO"))
             .orElseGet(() -> isContainerEnv()
                     ? "/redis/work/modules/testmodule.so"
@@ -14,6 +19,10 @@ public class TestEnvUtil {
     }
 
     public static boolean isContainerEnv() {
-        return TEST_ENV_PROVIDER.equals("docker");
+        return TEST_ENV_PROVIDER.equals(ENV_DOCKER);
+    }
+
+    public static boolean isLocalEnv() {
+        return TEST_ENV_PROVIDER.equals(ENV_LOCAL);
     }
 }

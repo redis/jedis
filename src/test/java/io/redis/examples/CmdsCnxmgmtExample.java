c8be658d5d84ec10ce984bfcdbf843f87b2c7521
Andy Stark <andrew.stark@redis.com>
2025-01-16 11:23:38 +0000

    DOC-4440 added placeholder for auth command examples

---
+++ src/test/java/io/redis/examples/CmdsCnxmgmtExample.java
@@ -0,0 +1,29 @@
+// EXAMPLE: cmds_cnxmgmt
+// REMOVE_START
+package io.redis.examples;
+
+import org.junit.Test;
+// REMOVE_END
+
+// HIDE_START
+public class CmdsCnxmgmtExample {
+    @Test
+    public void run() {
+// HIDE_END
+
+        // STEP_START auth1
+
+        // Not currently supported by Jedis.
+
+        // STEP_END
+       
+        // STEP_START auth2
+        
+        // Not currently supported by Jedis.
+
+        // STEP_END
+        
+        // HIDE_START
+    }
+}
+// HIDE_END

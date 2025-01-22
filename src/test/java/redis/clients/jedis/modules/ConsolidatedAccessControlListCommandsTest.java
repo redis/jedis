3ac0b894f7a6cd3468c6d3508e93e3d3d11f72a3
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2025-01-22 21:35:33 +0600

    Test modules acl categories

--- src/test/java/redis/clients/jedis/modules/ConsolidatedAccessControlListCommandsTest.java
+++ src/test/java/redis/clients/jedis/modules/ConsolidatedAccessControlListCommandsTest.java
@@ -1,11 +1,15 @@
 package redis.clients.jedis.modules;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.endsWith;
-import static org.hamcrest.Matchers.startsWith;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 import io.redis.test.annotations.SinceRedisVersion;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,10 +18,16 @@ import org.junit.runners.Parameterized;
 import redis.clients.jedis.DefaultJedisClientConfig;
 import redis.clients.jedis.RedisProtocol;
 import redis.clients.jedis.UnifiedJedis;
+import redis.clients.jedis.bloom.RedisBloomProtocol.*;
+import redis.clients.jedis.commands.ProtocolCommand;
 import redis.clients.jedis.commands.jedis.JedisCommandsTestBase;
 import redis.clients.jedis.exceptions.JedisAccessControlException;
+import redis.clients.jedis.json.JsonProtocol.JsonCommand;
+import redis.clients.jedis.search.SearchProtocol.SearchCommand;
+import redis.clients.jedis.timeseries.TimeSeriesProtocol.TimeSeriesCommand;
+import redis.clients.jedis.util.SafeEncoder;
 
-@SinceRedisVersion(value = "8.0.0")
+@SinceRedisVersion(value = "7.9.0")
 @RunWith(Parameterized.class)
 public class ConsolidatedAccessControlListCommandsTest extends JedisCommandsTestBase {
 
@@ -38,7 +48,55 @@ public class ConsolidatedAccessControlListCommandsTest extends JedisCommandsTest
   }
 
   @Test
-  public void aclHashesCommandsTest() {
+  public void listACLCategoriesTest() {
+    assertThat(jedis.aclCat(),
+        Matchers.hasItems("bloom", "cuckoo", "cms", "topk", "tdigest",
+            "search", "timeseries", "json"));
+  }
+
+  @Test
+  public void grantBloomCommandCatTest() {
+    grantModuleCommandCatTest("bloom", BloomFilterCommand.RESERVE, client -> client.bfReserve("foo", 0.01, 10_000));
+  }
+
+  @Test
+  public void grantCuckooCommandCatTest() {
+    grantModuleCommandCatTest("cuckoo", CuckooFilterCommand.RESERVE, client -> client.cfReserve("foo", 10_000));
+  }
+
+  @Test
+  public void grantCmsCommandCatTest() {
+    grantModuleCommandCatTest("cms", CountMinSketchCommand.INITBYDIM, client -> client.cmsInitByDim("foo", 16, 4));
+  }
+
+  @Test
+  public void grantTopkModuleCommandCatTest() {
+    grantModuleCommandCatTest("topk", TopKCommand.RESERVE, client -> client.topkReserve("foo", 1000));
+  }
+
+  @Test
+  public void grantTdigestCommandCatTest() {
+    grantModuleCommandCatTest("tdigest", TDigestCommand.CREATE, client -> client.tdigestCreate("foo"));
+  }
+
+  @org.junit.Ignore
+  @Test
+  public void grantSearchCommandCatTest() {
+    grantModuleCommandCatTest("search", SearchCommand.CREATE,
+        client -> client.ftCreate("index", Collections.emptySet()));
+  }
+
+  @Test
+  public void grantTimeseriesCommandCatTest() {
+    grantModuleCommandCatTest("timeseries", TimeSeriesCommand.CREATE, client -> client.tsCreate("foo"));
+  }
+
+  @Test
+  public void grantJsonCommandCatTest() {
+    grantModuleCommandCatTest("json", JsonCommand.GET, client -> client.jsonGet("foo"));
+  }
+
+  private void grantModuleCommandCatTest(String category, ProtocolCommand command, Consumer<UnifiedJedis> operation) {
     // create and enable an user with permission to all keys but no commands
     jedis.aclSetUser(USER_NAME, ">" + USER_PASSWORD, "on", "~*");
 
@@ -46,19 +104,18 @@ public class ConsolidatedAccessControlListCommandsTest extends JedisCommandsTest
     try (UnifiedJedis client = new UnifiedJedis(endpoint.getHostAndPort(),
         DefaultJedisClientConfig.builder().user(USER_NAME).password(USER_PASSWORD).build())) {
 
-      // user can't execute hash commands
-      JedisAccessControlException ace = assertThrows("Should throw a NOPERM exception",
-          JedisAccessControlException.class,
-          () -> client.hgetAll("foo"));
-      assertThat(ace.getMessage(), startsWith("NOPERM "));
-      assertThat(ace.getMessage(), endsWith(" has no permissions to run the 'hgetall' command"));
+      // user can't execute category commands
+      JedisAccessControlException noperm = assertThrows("Should throw a NOPERM exception",
+          JedisAccessControlException.class, () -> operation.accept(client));
+      assertEquals(String.format("NOPERM User %s has no permissions to run the '%s' command",
+          USER_NAME, SafeEncoder.encode(command.getRaw()).toLowerCase(Locale.ENGLISH)),
+          noperm.getMessage());
 
-      // permit user to hash commands
-      jedis.aclSetUser(USER_NAME, "+@hash");
+      // permit user to category commands
+      jedis.aclSetUser(USER_NAME, "+@" + category);
 
-      // user can now execute hash commands
-      client.hgetAll("foo");
+      // user can now execute category commands
+      operation.accept(client);
     }
   }
-
 }

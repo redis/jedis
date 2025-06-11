ee2f87fe623609892bbc8abea1ee46dfa0314228
ggivo <ivo.gaydazhiev@redis.com>
2025-06-11 17:32:39 +0300

    [automatic-failover] Enforce formatting for automatic failover codebase (#4176)
    * enforce formating for mcf
    
    * enforce formating for mcf
    
    * Trigger integration test jobs  feature branches

--- src/main/java/redis/clients/jedis/mcf/MultiClusterPipeline.java
+++ src/main/java/redis/clients/jedis/mcf/MultiClusterPipeline.java
@@ -12,7 +12,8 @@ import redis.clients.jedis.util.KeyValue;
 
 /**
  * This is high memory dependent solution as all the appending commands will be hold in memory until
- * {@link MultiClusterPipeline#sync() SYNC} (or {@link MultiClusterPipeline#close() CLOSE}) gets called.
+ * {@link MultiClusterPipeline#sync() SYNC} (or {@link MultiClusterPipeline#close() CLOSE}) gets
+ * called.
  */
 @Experimental
 public class MultiClusterPipeline extends PipelineBase implements Closeable {
@@ -32,7 +33,8 @@ public class MultiClusterPipeline extends PipelineBase implements Closeable {
     }
   }
 
-  public MultiClusterPipeline(MultiClusterPooledConnectionProvider pooledProvider, CommandObjects commandObjects) {
+  public MultiClusterPipeline(MultiClusterPooledConnectionProvider pooledProvider,
+      CommandObjects commandObjects) {
     super(commandObjects);
     this.failoverProvider = new CircuitBreakerFailoverConnectionProvider(pooledProvider);
   }
@@ -52,8 +54,9 @@ public class MultiClusterPipeline extends PipelineBase implements Closeable {
   }
 
   /**
-   * Synchronize pipeline by reading all responses. This operation close the pipeline. In order to get return values
-   * from pipelined commands, capture the different Response&lt;?&gt; of the commands you execute.
+   * Synchronize pipeline by reading all responses. This operation close the pipeline. In order to
+   * get return values from pipelined commands, capture the different Response&lt;?&gt; of the
+   * commands you execute.
    */
   @Override
   public void sync() {

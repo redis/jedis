4ad3fce612d77c9bb50776320b4af761b7324847
ggivo <ivo.gaydazhiev@redis.com>
2025-07-07 12:14:11 +0300

    [cleanup] Use weak reference in AdaptiveTimeoutHandler to avoid memory leak

--- src/main/java/redis/clients/jedis/AdaptiveTimeoutHandler.java
+++ src/main/java/redis/clients/jedis/AdaptiveTimeoutHandler.java
@@ -1,21 +1,24 @@
 package redis.clients.jedis;
 
+import java.lang.ref.WeakReference;
+import redis.clients.jedis.annots.Experimental;
+
 /**
- * Implementation of MaintenanceListener that manages connection timeout relaxation
+ * Implementation of {@link PushConsumer } that manages connection timeout relaxation
  * during Redis server maintenance events like migration and failover.
  */
+@Experimental
 public class AdaptiveTimeoutHandler implements PushConsumer {
 
-  Connection connection;
-
+  private final WeakReference<Connection> connectionRef;
 
   /**
    * Creates a new maintenance listener for the specified connection.
-   * 
+   *
    * @param connection The connection to manage timeouts for
    */
   public AdaptiveTimeoutHandler(Connection connection) {
-    this.connection = connection;
+    this.connectionRef = new WeakReference<>(connection);
   }
 
   @Override
@@ -39,18 +42,30 @@ public class AdaptiveTimeoutHandler implements PushConsumer {
   }
 
   private void onMigrating() {
-    connection.relaxTimeouts();
+    Connection connection = connectionRef.get();
+    if (connection != null) {
+      connection.relaxTimeouts();
+    }
   }
 
   private void onMigrated() {
-    connection.disableRelaxedTimeout();
+    Connection connection = connectionRef.get();
+    if (connection != null) {
+      connection.disableRelaxedTimeout();
+    }
   }
 
   private void onFailOver() {
-    connection.relaxTimeouts();
+    Connection connection = connectionRef.get();
+    if (connection != null) {
+      connection.relaxTimeouts();
+    }
   }
 
   private void onFailedOver() {
-    connection.disableRelaxedTimeout();
+    Connection connection = connectionRef.get();
+    if (connection != null) {
+      connection.disableRelaxedTimeout();
+    }
   }
 }

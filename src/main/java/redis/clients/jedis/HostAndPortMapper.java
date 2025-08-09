069ea6ffe0d50473f9f25d98378cd3632f8864ac
JuneYub <ajsjdlwj0123@gmail.com>
2025-08-10 04:51:34 +0900

    docs: Improve Javadoc for HostAndPortMapper (#4112)

--- src/main/java/redis/clients/jedis/HostAndPortMapper.java
+++ src/main/java/redis/clients/jedis/HostAndPortMapper.java
@@ -1,6 +1,17 @@
 package redis.clients.jedis;
 
+/**
+ * An interface for mapping Redis node addresses.
+ * <p>
+ * It is used to translate an advertised server address to one that is reachable by the client,
+ * especially in network topologies involving NAT or containerization.
+ */
+@FunctionalInterface
 public interface HostAndPortMapper {
 
+  /**
+   * @param hap The original address from the server.
+   * @return The translated, reachable address.
+   */
   HostAndPort getHostAndPort(HostAndPort hap);
 }

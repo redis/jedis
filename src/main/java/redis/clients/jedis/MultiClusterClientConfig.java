d0ee67ebba46f849f2a7eab1e656e0d0b407c9be
ggivo <ivo.gaydazhiev@redis.com>
2025-08-18 21:50:42 +0300

    Add list constructor

--- src/main/java/redis/clients/jedis/MultiClusterClientConfig.java
+++ src/main/java/redis/clients/jedis/MultiClusterClientConfig.java
@@ -259,6 +259,10 @@ public final class MultiClusterClientConfig {
         return new Builder(clusterConfigs);
     }
 
+    public static Builder builder(List<ClusterConfig> clusterConfigs) {
+        return new Builder(clusterConfigs);
+    }
+
     public static class ClusterConfig {
 
         private HostAndPort hostAndPort;

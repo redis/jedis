2cffb0173f808d3a1b929aff86d5344a4daaf245
atakavci <a_takavci@yahoo.com>
2025-09-05 17:11:00 +0300

    - format

--- src/test/java/redis/clients/jedis/mcf/TestHealthCheckStrategy.java
+++ src/test/java/redis/clients/jedis/mcf/TestHealthCheckStrategy.java
@@ -7,49 +7,49 @@ import redis.clients.jedis.exceptions.JedisException;
 
 public class TestHealthCheckStrategy implements HealthCheckStrategy {
 
-    private int interval;
-    private int timeout;
-    private int retries;
-    private int delay;
-    private Callable<HealthStatus> healthCheck;
+  private int interval;
+  private int timeout;
+  private int retries;
+  private int delay;
+  private Callable<HealthStatus> healthCheck;
 
-    public TestHealthCheckStrategy(int interval, int timeout, int retries, int delay,
-        Callable<HealthStatus> healthCheck) {
-      this.interval = interval;
-      this.timeout = timeout;
-      this.retries = retries;
-      this.delay = delay;
-      this.healthCheck = healthCheck;
-    }
+  public TestHealthCheckStrategy(int interval, int timeout, int retries, int delay,
+      Callable<HealthStatus> healthCheck) {
+    this.interval = interval;
+    this.timeout = timeout;
+    this.retries = retries;
+    this.delay = delay;
+    this.healthCheck = healthCheck;
+  }
 
-    @Override
-    public int getInterval() {
-      return interval;
-    }
+  @Override
+  public int getInterval() {
+    return interval;
+  }
 
-    @Override
-    public int getTimeout() {
-      return timeout;
-    }
+  @Override
+  public int getTimeout() {
+    return timeout;
+  }
 
-    @Override
-    public int getNumberOfRetries() {
-      return retries;
-    }
+  @Override
+  public int getNumberOfRetries() {
+    return retries;
+  }
 
-    @Override
-    public int getDelayInBetweenRetries() {
-      return delay;
-    }
+  @Override
+  public int getDelayInBetweenRetries() {
+    return delay;
+  }
 
-    @Override
-    public HealthStatus doHealthCheck(Endpoint endpoint) {
-      try {
-        return healthCheck.call();
-      } catch (JedisException e) {
-        throw e;
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
+  @Override
+  public HealthStatus doHealthCheck(Endpoint endpoint) {
+    try {
+      return healthCheck.call();
+    } catch (JedisException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
-  };
+  }
+};

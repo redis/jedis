3b91b6c69466e5c1051a9ff83a72870c505245af
李金松 <lijinsong02@58.com>
2025-09-17 15:33:06 +0800

    format file

--- src/main/java/redis/clients/jedis/ReadFrom.java
+++ src/main/java/redis/clients/jedis/ReadFrom.java
@@ -1,12 +1,12 @@
 package redis.clients.jedis;
 
 public enum ReadFrom {
-    // read from the upstream only.
-    UPSTREAM,
-    // read from the replica only.
-    REPLICA,
-    // read preferred from the upstream and fall back to a replica if the upstream is not available.
-    UPSTREAM_PREFERRED,
-    // read preferred from replica and fall back to upstream if no replica is not available.
-    REPLICA_PREFERRED
+  // read from the upstream only.
+  UPSTREAM,
+  // read from the replica only.
+  REPLICA,
+  // read preferred from the upstream and fall back to a replica if the upstream is not available.
+  UPSTREAM_PREFERRED,
+  // read preferred from replica and fall back to upstream if no replica is not available.
+  REPLICA_PREFERRED
 }

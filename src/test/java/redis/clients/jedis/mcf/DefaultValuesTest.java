f622c1ea457216afc585eec6a1775c57e04b5ff1
atakavci <a_takavci@yahoo.com>
2025-09-19 00:14:36 +0300

    - set & test default values

---
+++ src/test/java/redis/clients/jedis/mcf/DefaultValuesTest.java
@@ -0,0 +1,79 @@
+package redis.clients.jedis.mcf;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+import redis.clients.jedis.DefaultJedisClientConfig;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisClientConfig;
+import redis.clients.jedis.MultiClusterClientConfig;
+
+public class DefaultValuesTest {
+
+        HostAndPort fakeEndpoint = new HostAndPort("fake", 6379);
+        JedisClientConfig config = DefaultJedisClientConfig.builder().build();
+
+        @Test
+        void testDefaultValuesInConfig() {
+
+                MultiClusterClientConfig.ClusterConfig clusterConfig = MultiClusterClientConfig.ClusterConfig
+                                .builder(fakeEndpoint, config).build();
+                MultiClusterClientConfig multiConfig = new MultiClusterClientConfig.Builder(
+                                new MultiClusterClientConfig.ClusterConfig[] { clusterConfig })
+                                                .build();
+
+                // check for grace period
+                assertEquals(60000, multiConfig.getGracePeriod());
+
+                // check for cluster config
+                assertEquals(clusterConfig, multiConfig.getClusterConfigs()[0]);
+
+                // check healthchecks enabled
+                assertNotNull(clusterConfig.getHealthCheckStrategySupplier());
+
+                // check default healthcheck strategy is echo
+                assertEquals(EchoStrategy.DEFAULT, clusterConfig.getHealthCheckStrategySupplier());
+
+                // check number of probes
+                assertEquals(3, clusterConfig.getHealthCheckStrategySupplier()
+                                .get(fakeEndpoint, config).getNumProbes());
+
+                assertEquals(500, clusterConfig.getHealthCheckStrategySupplier()
+                                .get(fakeEndpoint, config).getDelayInBetweenProbes());
+
+                assertEquals(ProbingPolicy.BuiltIn.ALL_SUCCESS,
+                        clusterConfig.getHealthCheckStrategySupplier().get(fakeEndpoint, config)
+                                        .getPolicy());
+
+                // check health check interval
+                assertEquals(5000, clusterConfig.getHealthCheckStrategySupplier()
+                                .get(fakeEndpoint, config).getInterval());
+
+                // check lag aware tolerance
+                LagAwareStrategy.Config lagAwareConfig = LagAwareStrategy.Config
+                                .builder(fakeEndpoint, config.getCredentialsProvider()).build();
+                assertEquals(Duration.ofMillis(5000), lagAwareConfig.getAvailabilityLagTolerance());
+
+                // TODO: check CB number of failures threshold -- 1000
+                // assertEquals(1000, multiConfig.circuitBreakerMinNumOfFailures());
+
+                // check CB failure rate threshold
+                assertEquals(10, multiConfig.getCircuitBreakerFailureRateThreshold());
+
+                // check CB sliding window size
+                assertEquals(2, multiConfig.getCircuitBreakerSlidingWindowSize());
+
+                // check failback check interval
+                assertEquals(120000, multiConfig.getFailbackCheckInterval());
+
+                // check failover max attempts before give up
+                assertEquals(10, multiConfig.getMaxNumFailoverAttempts());
+
+                // check delay between failover attempts
+                assertEquals(12000, multiConfig.getDelayInBetweenFailoverAttempts());
+
+        }
+}

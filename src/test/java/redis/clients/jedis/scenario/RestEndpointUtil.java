1846fe6a1e8db8ec79d10334b10fddf2b9a06874
ggivo <ivo.gaydazhiev@redis.com>
2025-09-23 10:56:53 +0300

    format

--- src/test/java/redis/clients/jedis/scenario/RestEndpointUtil.java
+++ src/test/java/redis/clients/jedis/scenario/RestEndpointUtil.java
@@ -5,22 +5,22 @@ import redis.clients.jedis.EndpointConfig;
 
 public class RestEndpointUtil {
 
-    public static Endpoint getRestAPIEndpoint(EndpointConfig config) {
-        return new Endpoint() {
-            @Override
-            public String getHost() {
-                // convert this to Redis FQDN by removing the node prefix
-                // "dns":"redis-10232.c1.taki-active-active-test-c114170a.cto.redislabs.com"
-                String host = config.getHost();
-                // trim until the first dot
-                return host.substring(host.indexOf('.') + 1);
-            }
+  public static Endpoint getRestAPIEndpoint(EndpointConfig config) {
+    return new Endpoint() {
+      @Override
+      public String getHost() {
+        // convert this to Redis FQDN by removing the node prefix
+        // "dns":"redis-10232.c1.taki-active-active-test-c114170a.cto.redislabs.com"
+        String host = config.getHost();
+        // trim until the first dot
+        return host.substring(host.indexOf('.') + 1);
+      }
 
-            @Override
-            public int getPort() {
-                // default port for REST API
-                return 9443;
-            }
-        };
-    }
+      @Override
+      public int getPort() {
+        // default port for REST API
+        return 9443;
+      }
+    };
+  }
 }

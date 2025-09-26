95116bdadd6fd934ccbbfc0e0351c164cc19ba52
atakavci <a_takavci@yahoo.com>
2025-09-26 11:10:48 +0300

    - java doc for exceptions

--- src/main/java/redis/clients/jedis/mcf/JedisFailoverException.java
+++ src/main/java/redis/clients/jedis/mcf/JedisFailoverException.java
@@ -2,6 +2,14 @@ package redis.clients.jedis.mcf;
 
 import redis.clients.jedis.exceptions.JedisConnectionException;
 
+/**
+ * Exception thrown when a failover attempt fails due to lack of available/healthy clusters.
+ * <p>
+ * This exception itself is not thrown, see the child exceptions for more details.
+ * </p>
+ * @see JedisFailoverException.JedisPermanentlyNotAvailableException
+ * @see JedisFailoverException.JedisTemporarilyNotAvailableException
+ */
 public class JedisFailoverException extends JedisConnectionException {
   private static final String MESSAGE = "Cluster/database endpoint could not failover since the MultiClusterClientConfig was not "
       + "provided with an additional cluster/database endpoint according to its prioritized sequence. "
@@ -15,6 +23,14 @@ public class JedisFailoverException extends JedisConnectionException {
     super(MESSAGE);
   }
 
+  /**
+   * Exception thrown when a failover attempt fails due to lack of available/healthy clusters, and
+   * the max number of failover attempts has been exceeded. And there is still no healthy cluster.
+   * See the configuration properties
+   * {@link redis.clients.jedis.MultiClusterClientConfig#maxNumFailoverAttempts} and
+   * {@link redis.clients.jedis.MultiClusterClientConfig#delayInBetweenFailoverAttempts} for more
+   * details.
+   */
   public static class JedisPermanentlyNotAvailableException extends JedisFailoverException {
     public JedisPermanentlyNotAvailableException(String s) {
       super(s);
@@ -25,6 +41,16 @@ public class JedisFailoverException extends JedisConnectionException {
     }
   }
 
+  /**
+   * Exception thrown when a failover attempt fails due to lack of available/healthy clusters, but
+   * the max number of failover attempts has not been exceeded yet. Though there is no healthy
+   * cluster including the selected/current one, given configuration suggests that it should be a
+   * temporary condition and it is possible that there will be a healthy cluster available.
+   * See the configuration properties
+   * {@link redis.clients.jedis.MultiClusterClientConfig#maxNumFailoverAttempts} and
+   * {@link redis.clients.jedis.MultiClusterClientConfig#delayInBetweenFailoverAttempts} for more
+   * details.
+   */
   public static class JedisTemporarilyNotAvailableException extends JedisFailoverException {
 
     public JedisTemporarilyNotAvailableException(String s) {

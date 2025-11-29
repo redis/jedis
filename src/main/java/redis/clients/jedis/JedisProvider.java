37461004e3cbb14beeb1ebb44b1d5b9862ee348e
oscar.besga <oscar.besga@panel.es>
2025-11-29 12:20:54 +0100

    format JedisProvider

--- src/main/java/redis/clients/jedis/JedisProvider.java
+++ src/main/java/redis/clients/jedis/JedisProvider.java
@@ -10,18 +10,21 @@ public interface JedisProvider {
 
     /**
      * Get a Jedis resource from the pool
+     *
      * @return Jedis object
      */
     Jedis getResource();
 
     /**
      * Return a Jedis resource to the pool
+     *
      * @param resource Jedis object
      */
     void returnResource(final Jedis resource);
 
     /**
      * Execute a consumer with a Jedis resource
+     *
      * @param consumer code to execute with the Jedis resource
      */
     default void withResource(Consumer<Jedis> consumer) {
@@ -32,6 +35,7 @@ public interface JedisProvider {
 
     /**
      * Execute a function with a Jedis resource and return a value
+     *
      * @param function code to execute with the Jedis resource
      * @return return value from the function
      */

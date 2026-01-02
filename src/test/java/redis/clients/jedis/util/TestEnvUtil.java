0b79e4c797237632011e0b7462a4418557f79ea0
Igor Malinovskiy <igor.malinovskiy@redis.com>
2026-01-02 18:05:55 +0100

    Add missing ENV_LEGACY after cherry-picks

--- src/test/java/redis/clients/jedis/util/TestEnvUtil.java
+++ src/test/java/redis/clients/jedis/util/TestEnvUtil.java
@@ -6,6 +6,8 @@ public class TestEnvUtil {
     // Redis servers running inside docker
     public static final String ENV_DOCKER = "docker";
 
+    public static final String ENV_LEGACY = "local";
+
     public static final String ENV_REDIS_ENTERPRISE = "re";
 
     private static final String TEST_ENV_PROVIDER = System.getenv().getOrDefault("TEST_ENV_PROVIDER", ENV_DOCKER);

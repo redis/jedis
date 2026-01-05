875c809593016e229aae4a42c6d572d2949c1fb2
李金松 <lijinsong02@58.com>
2026-01-05 17:54:55 +0800

    format

--- src/main/java/redis/clients/jedis/StaticReadOnlyPredicate.java
+++ src/main/java/redis/clients/jedis/StaticReadOnlyPredicate.java
@@ -4,13 +4,15 @@ public class StaticReadOnlyPredicate implements ReadOnlyPredicate {
 
   private static final StaticReadOnlyPredicate REGISTRY = new StaticReadOnlyPredicate();
 
-  private StaticReadOnlyPredicate(){}
+  private StaticReadOnlyPredicate() {
+  }
 
   public static StaticReadOnlyPredicate registry() {
     return REGISTRY;
   }
 
   public boolean isReadOnly(CommandArguments command) {
-    return StaticCommandFlagsRegistry.registry().getFlags(command).contains(CommandFlagsRegistry.CommandFlag.READONLY);
+    return StaticCommandFlagsRegistry.registry().getFlags(command)
+        .contains(CommandFlagsRegistry.CommandFlag.READONLY);
   }
 }

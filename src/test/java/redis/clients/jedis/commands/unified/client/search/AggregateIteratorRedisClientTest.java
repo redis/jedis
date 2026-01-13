6cb1a8ae1abefb97527d76f1518722f3730f1bd7
aleksandar.todorov <a_t_todorov@yahoo.com>
2026-01-13 16:26:39 +0200

    Migrate aggregation tests

---
+++ src/test/java/redis/clients/jedis/commands/unified/client/search/AggregateIteratorRedisClientTest.java
@@ -0,0 +1,26 @@
+package redis.clients.jedis.commands.unified.client.search;
+
+import org.junit.jupiter.params.ParameterizedClass;
+import org.junit.jupiter.params.provider.MethodSource;
+import redis.clients.jedis.RedisProtocol;
+import redis.clients.jedis.UnifiedJedis;
+import redis.clients.jedis.commands.unified.client.RedisClientCommandsTestHelper;
+import redis.clients.jedis.commands.unified.search.AggregateIteratorBaseTest;
+
+/**
+ * Standalone (RedisClient) implementation of AggregateIterator tests. Uses modules-docker endpoint
+ * which has RediSearch modules built-in.
+ */
+@ParameterizedClass
+@MethodSource("redis.clients.jedis.commands.CommandsTestsParameters#respVersions")
+public class AggregateIteratorRedisClientTest extends AggregateIteratorBaseTest {
+
+  public AggregateIteratorRedisClientTest(RedisProtocol protocol) {
+    super(protocol);
+  }
+
+  @Override
+  protected UnifiedJedis createTestClient() {
+    return RedisClientCommandsTestHelper.getClient(protocol);
+  }
+}

62becdf85fad4c8c7fdd89b77f9aa97f39c08b7a
Igor Malinovskiy <igor.malinovskiy@redis.com>
2026-01-21 16:37:15 +0100

    Fix formatting

--- src/main/java/redis/clients/jedis/exceptions/UnsupportedAggregationException.java
+++ src/main/java/redis/clients/jedis/exceptions/UnsupportedAggregationException.java
@@ -3,9 +3,8 @@ package redis.clients.jedis.exceptions;
 /**
  * Exception thrown when an unsupported aggregation operation is attempted.
  * <p>
- * This exception is thrown when the aggregation policy cannot handle the given
- * data types. For example, when trying to sum non-numeric values or compare
- * non-comparable types.
+ * This exception is thrown when the aggregation policy cannot handle the given data types. For
+ * example, when trying to sum non-numeric values or compare non-comparable types.
  * </p>
  */
 public class UnsupportedAggregationException extends ClusterAggregationException {
@@ -24,4 +23,3 @@ public class UnsupportedAggregationException extends ClusterAggregationException
     super(message, cause);
   }
 }
-

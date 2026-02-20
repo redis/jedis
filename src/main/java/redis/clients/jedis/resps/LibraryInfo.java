ab665120f69332509c980d5658f34d1e4dc3f44c
aleksandar.todorov <a_t_todorov@yahoo.com>
2026-02-20 14:18:50 +0200

    Address reviews

--- src/main/java/redis/clients/jedis/resps/LibraryInfo.java
+++ src/main/java/redis/clients/jedis/resps/LibraryInfo.java
@@ -20,6 +20,15 @@ public class LibraryInfo {
   private final List<Map<String, Object>> functions;
   private final String libraryCode;
 
+  /**
+   * @deprecated Since 7.4. This constructor will be removed in the next major release.
+   *             Use {@link #LibraryInfo(String, String, List, String)} instead.
+   */
+  @Deprecated
+  public LibraryInfo(String libraryName, String engineName, List<Map<String, Object>> functions) {
+    this(libraryName, engineName, functions, null);
+  }
+
   public LibraryInfo(String libraryName, String engineName, List<Map<String, Object>> functions, String code) {
     this.libraryName = libraryName;
     this.engine = engineName;
@@ -43,6 +52,13 @@ public class LibraryInfo {
     return libraryCode;
   }
 
+  private static class LibraryInfoHolder {
+    String libraryName;
+    String engineName;
+    String libraryCode;
+    List<Map<String, Object>> functions;
+  }
+
   public static final Builder<LibraryInfo> LIBRARY_INFO = new Builder<LibraryInfo>() {
     @Override
     public LibraryInfo build(Object data) {
@@ -50,42 +66,39 @@ public class LibraryInfo {
       List list = (List) data;
       if (list.isEmpty()) return null;
 
-      String[] libname = { null };
-      String[] enginename = { null };
-      String[] librarycode = { null };
-      List[] functions = { null };
+      LibraryInfoHolder holder = new LibraryInfoHolder();
 
       if (list.get(0) instanceof KeyValue) {
         // RESP3 format: list of KeyValue objects
         for (KeyValue kv : (List<KeyValue>) list) {
-          processField(kv.getKey(), kv.getValue(), libname, enginename, functions, librarycode);
+          processField(kv.getKey(), kv.getValue(), holder);
         }
       } else {
         // RESP2 format: flat list with alternating key-value pairs
         // Note: Redis Enterprise may include extra fields like "consistent"
         for (int i = 0; i + 1 < list.size(); i += 2) {
-          processField(list.get(i), list.get(i + 1), libname, enginename, functions, librarycode);
+          processField(list.get(i), list.get(i + 1), holder);
         }
       }
 
-      return new LibraryInfo(libname[0], enginename[0], functions[0], librarycode[0]);
+      return new LibraryInfo(holder.libraryName, holder.engineName, holder.functions, holder.libraryCode);
     }
 
-    private void processField(Object key, Object value, String[] libname, String[] enginename,
-        List[] functions, String[] librarycode) {
+    private void processField(Object key, Object value, LibraryInfoHolder holder) {
       switch (BuilderFactory.STRING.build(key)) {
         case "library_name":
-          libname[0] = BuilderFactory.STRING.build(value);
+          holder.libraryName = BuilderFactory.STRING.build(value);
           break;
         case "engine":
-          enginename[0] = BuilderFactory.STRING.build(value);
+          holder.engineName = BuilderFactory.STRING.build(value);
           break;
         case "functions":
-          functions[0] = ((List<Object>) value).stream().map(ENCODED_OBJECT_MAP::build)
+          holder.functions = ((List<Object>) value).stream()
+              .map(ENCODED_OBJECT_MAP::build)
               .collect(Collectors.toList());
           break;
         case "library_code":
-          librarycode[0] = BuilderFactory.STRING.build(value);
+          holder.libraryCode = BuilderFactory.STRING.build(value);
           break;
       }
     }

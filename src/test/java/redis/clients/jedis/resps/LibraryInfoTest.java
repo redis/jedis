f30269cf423fb6c15c677bddff7da491220e7253
aleksandar.todorov <a_t_todorov@yahoo.com>
2026-02-20 14:40:30 +0200

    Formatting

--- src/test/java/redis/clients/jedis/resps/LibraryInfoTest.java
+++ src/test/java/redis/clients/jedis/resps/LibraryInfoTest.java
@@ -23,21 +23,14 @@ public class LibraryInfoTest {
   public void buildLibraryInfoResp2Standard() {
     // Simulate standard RESP2 response for FUNCTION LIST (without WITHCODE)
     // Format: [library_name, <value>, engine, <value>, functions, <value>]
-    String respResponse =
-        "*6\r\n" +
-            "$12\r\nlibrary_name\r\n" +
-            "$5\r\nmylib\r\n" +
-            "$6\r\nengine\r\n" +
-            "$3\r\nLUA\r\n" +
-            "$9\r\nfunctions\r\n" +
-            "*1\r\n" +  // functions array with 1 element
-            "*6\r\n" +  // function info with 3 key-value pairs
-            "$4\r\nname\r\n" +
-            "$6\r\nmyfunc\r\n" +
-            "$11\r\ndescription\r\n" +
-            "$-1\r\n" +  // null description
-            "$5\r\nflags\r\n" +
-            "*0\r\n";  // empty flags array
+    String respResponse = "*6\r\n" + "$12\r\nlibrary_name\r\n" + "$5\r\nmylib\r\n"
+        + "$6\r\nengine\r\n" + "$3\r\nLUA\r\n" + "$9\r\nfunctions\r\n" + "*1\r\n" + // functions
+                                                                                    // array with 1
+                                                                                    // element
+        "*6\r\n" + // function info with 3 key-value pairs
+        "$4\r\nname\r\n" + "$6\r\nmyfunc\r\n" + "$11\r\ndescription\r\n" + "$-1\r\n" + // null
+                                                                                       // description
+        "$5\r\nflags\r\n" + "*0\r\n"; // empty flags array
 
     Object data = parseRespResponse(respResponse);
     LibraryInfo result = LibraryInfo.LIBRARY_INFO.build(data);
@@ -55,23 +48,16 @@ public class LibraryInfoTest {
   public void buildLibraryInfoResp2WithCode() {
     // Simulate RESP2 response for FUNCTION LIST WITHCODE
     // Format: [library_name, <value>, engine, <value>, functions, <value>, library_code, <value>]
-    String respResponse =
-        "*8\r\n" +
-            "$12\r\nlibrary_name\r\n" +
-            "$5\r\nmylib\r\n" +
-            "$6\r\nengine\r\n" +
-            "$3\r\nLUA\r\n" +
-            "$9\r\nfunctions\r\n" +
-            "*1\r\n" +  // functions array with 1 element
-            "*6\r\n" +  // function info with 3 key-value pairs
-            "$4\r\nname\r\n" +
-            "$6\r\nmyfunc\r\n" +
-            "$11\r\ndescription\r\n" +
-            "$-1\r\n" +  // null description
-            "$5\r\nflags\r\n" +
-            "*0\r\n" +  // empty flags array
-            "$12\r\nlibrary_code\r\n" +
-            "$50\r\n#!LUA name=mylib\nredis.register_function('myfunc')\r\n";
+    String respResponse = "*8\r\n" + "$12\r\nlibrary_name\r\n" + "$5\r\nmylib\r\n"
+        + "$6\r\nengine\r\n" + "$3\r\nLUA\r\n" + "$9\r\nfunctions\r\n" + "*1\r\n" + // functions
+                                                                                    // array with 1
+                                                                                    // element
+        "*6\r\n" + // function info with 3 key-value pairs
+        "$4\r\nname\r\n" + "$6\r\nmyfunc\r\n" + "$11\r\ndescription\r\n" + "$-1\r\n" + // null
+                                                                                       // description
+        "$5\r\nflags\r\n" + "*0\r\n" + // empty flags array
+        "$12\r\nlibrary_code\r\n"
+        + "$50\r\n#!LUA name=mylib\nredis.register_function('myfunc')\r\n";
 
     Object data = parseRespResponse(respResponse);
     LibraryInfo result = LibraryInfo.LIBRARY_INFO.build(data);
@@ -91,23 +77,15 @@ public class LibraryInfoTest {
     // Simulate Redis Enterprise RESP2 response with extra "consistent" field
     // This is the bug scenario from CAE-2120
     // Format: [library_name, <value>, engine, <value>, consistent, <value>, functions, <value>]
-    String respResponse =
-        "*8\r\n" +
-            "$12\r\nlibrary_name\r\n" +
-            "$5\r\nmylib\r\n" +
-            "$6\r\nengine\r\n" +
-            "$3\r\nLUA\r\n" +
-            "$10\r\nconsistent\r\n" +
-            ":1\r\n" +  // consistent value (integer)
-            "$9\r\nfunctions\r\n" +
-            "*1\r\n" +  // functions array with 1 element
-            "*6\r\n" +  // function info with 3 key-value pairs
-            "$4\r\nname\r\n" +
-            "$6\r\nmyfunc\r\n" +
-            "$11\r\ndescription\r\n" +
-            "$-1\r\n" +  // null description
-            "$5\r\nflags\r\n" +
-            "*0\r\n";  // empty flags array
+    String respResponse = "*8\r\n" + "$12\r\nlibrary_name\r\n" + "$5\r\nmylib\r\n"
+        + "$6\r\nengine\r\n" + "$3\r\nLUA\r\n" + "$10\r\nconsistent\r\n" + ":1\r\n" + // consistent
+                                                                                      // value
+                                                                                      // (integer)
+        "$9\r\nfunctions\r\n" + "*1\r\n" + // functions array with 1 element
+        "*6\r\n" + // function info with 3 key-value pairs
+        "$4\r\nname\r\n" + "$6\r\nmyfunc\r\n" + "$11\r\ndescription\r\n" + "$-1\r\n" + // null
+                                                                                       // description
+        "$5\r\nflags\r\n" + "*0\r\n"; // empty flags array
 
     Object data = parseRespResponse(respResponse);
     LibraryInfo result = LibraryInfo.LIBRARY_INFO.build(data);
@@ -124,26 +102,19 @@ public class LibraryInfoTest {
   @Test
   public void buildLibraryInfoResp2WithExtraConsistentFieldAndCode() {
     // Simulate Redis Enterprise RESP2 response with extra "consistent" field and WITHCODE
-    // Format: [library_name, <value>, engine, <value>, consistent, <value>, functions, <value>, library_code, <value>]
-    String respResponse =
-        "*10\r\n" +
-            "$12\r\nlibrary_name\r\n" +
-            "$5\r\nmylib\r\n" +
-            "$6\r\nengine\r\n" +
-            "$3\r\nLUA\r\n" +
-            "$10\r\nconsistent\r\n" +
-            ":1\r\n" +  // consistent value (integer)
-            "$9\r\nfunctions\r\n" +
-            "*1\r\n" +  // functions array with 1 element
-            "*6\r\n" +  // function info with 3 key-value pairs
-            "$4\r\nname\r\n" +
-            "$6\r\nmyfunc\r\n" +
-            "$11\r\ndescription\r\n" +
-            "$-1\r\n" +  // null description
-            "$5\r\nflags\r\n" +
-            "*0\r\n" +  // empty flags array
-            "$12\r\nlibrary_code\r\n" +
-            "$50\r\n#!LUA name=mylib\nredis.register_function('myfunc')\r\n";
+    // Format: [library_name, <value>, engine, <value>, consistent, <value>, functions, <value>,
+    // library_code, <value>]
+    String respResponse = "*10\r\n" + "$12\r\nlibrary_name\r\n" + "$5\r\nmylib\r\n"
+        + "$6\r\nengine\r\n" + "$3\r\nLUA\r\n" + "$10\r\nconsistent\r\n" + ":1\r\n" + // consistent
+                                                                                      // value
+                                                                                      // (integer)
+        "$9\r\nfunctions\r\n" + "*1\r\n" + // functions array with 1 element
+        "*6\r\n" + // function info with 3 key-value pairs
+        "$4\r\nname\r\n" + "$6\r\nmyfunc\r\n" + "$11\r\ndescription\r\n" + "$-1\r\n" + // null
+                                                                                       // description
+        "$5\r\nflags\r\n" + "*0\r\n" + // empty flags array
+        "$12\r\nlibrary_code\r\n"
+        + "$50\r\n#!LUA name=mylib\nredis.register_function('myfunc')\r\n";
 
     Object data = parseRespResponse(respResponse);
     LibraryInfo result = LibraryInfo.LIBRARY_INFO.build(data);
@@ -158,4 +129,3 @@ public class LibraryInfoTest {
     assertTrue(result.getLibraryCode().contains("#!LUA name=mylib"));
   }
 }
-

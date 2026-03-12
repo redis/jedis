e9ff346b18f09f9c9e1343db51947ff7eadf625d
Igor Malinovskiy <u.glide@gmail.com>
2026-03-12 12:40:46 +0100

    Add mTLS tests (#4458)
    * Add mTLS tests
    
    * Fix sslVerifyMode in ClientAuthRedisClusterClientIT
    
    * Topic/ggivo/mtls auth tests shared (#4459)
    
    * base tests for RedisClient/RedisClusterClient
    
    * move mTLS endpoint initialization to concrete test classes
    
    * format
    
    * discoverClusterNodesWithMtls - init client using single seed node
    
    * address review comments
    
    * Fix formatting
    
    * Add missing negative test
    
    ---------
    
    Co-authored-by: Ivo Gaydazhiev <ivo.gaydazhiev@redis.com>

--- src/test/java/redis/clients/jedis/util/TlsUtil.java
+++ src/test/java/redis/clients/jedis/util/TlsUtil.java
@@ -107,6 +107,21 @@ public class TlsUtil {
         return Paths.get(TEST_WORK_FOLDER, certLocation.toString(), TEST_SERVER_CERT);
     }
 
+    /**
+     * Resolves the path to a pre-generated PKCS12 keystore for mTLS client authentication.
+     * The Docker container generates .p12 files for each TLS_CLIENT_CNS entry.
+     *
+     * @param certLocation the certificate location from EndpointConfig
+     * @param clientName the name of the client certificate (without extension)
+     * @return the absolute path to the .p12 keystore file
+     */
+    public static Path clientKeystorePath(Path certLocation, String clientName) {
+        if (certLocation.isAbsolute()) {
+            return certLocation.resolve(clientName + ".p12");
+        }
+        return Paths.get(TEST_WORK_FOLDER, certLocation.toString(), clientName + ".p12");
+    }
+
     public static Path testTruststorePath(String name) {
         return Paths.get(TEST_WORK_FOLDER, name  + '-' + TEST_TRUSTSTORE);
     }

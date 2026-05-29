9ccfe9fa293f6c66375f621769e2618aa273f256
Igor Malinovskiy <igor.malinovskiy@redis.com>
2026-05-29 11:30:26 +0200

    Fix connection leak in MultiDbConnectionSupplier
    Remove fqn import

--- src/main/java/redis/clients/jedis/mcf/MultiDbConnectionSupplier.java
+++ src/main/java/redis/clients/jedis/mcf/MultiDbConnectionSupplier.java
@@ -7,6 +7,7 @@ import io.github.resilience4j.decorators.Decorators.DecorateSupplier;
 import redis.clients.jedis.Connection;
 import redis.clients.jedis.annots.Experimental;
 import redis.clients.jedis.mcf.MultiDbConnectionProvider.Database;
+import redis.clients.jedis.util.IOUtils;
 
 /**
  * ConnectionProvider with built-in retry, circuit-breaker, and failover to another /database
@@ -46,8 +47,13 @@ public class MultiDbConnectionSupplier extends MultiDbFailoverBase {
    */
   private Connection handleGetConnection(Database database) {
     Connection connection = database.getConnection();
-    connection.ping();
-    return connection;
+    try {
+      connection.ping();
+      return connection;
+    } catch (RuntimeException e) {
+      IOUtils.closeQuietly(connection);
+      throw e;
+    }
   }
 
   /**

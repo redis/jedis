a3dbae946100aaadb18b2440cced49c7c556a350
Hyun Lee <dlwhdugs4147@gmail.com>
2026-09-10 11:37:39 -0500

    Encode infinity arguments with the Redis protocol charset

--- src/main/java/redis/clients/jedis/Protocol.java
+++ src/main/java/redis/clients/jedis/Protocol.java
@@ -48,8 +48,8 @@ public final class Protocol {
   public static final byte[] BYTES_EQUAL = SafeEncoder.encode("=");
   public static final byte[] BYTES_ASTERISK = SafeEncoder.encode("*");
 
-  public static final byte[] POSITIVE_INFINITY_BYTES = "+inf".getBytes();
-  public static final byte[] NEGATIVE_INFINITY_BYTES = "-inf".getBytes();
+  public static final byte[] POSITIVE_INFINITY_BYTES = "+inf".getBytes(CHARSET);
+  public static final byte[] NEGATIVE_INFINITY_BYTES = "-inf".getBytes(CHARSET);
 
   static final List<KeyValue> PROTOCOL_EMPTY_MAP = Collections.unmodifiableList(new ArrayList<>(0));
 
